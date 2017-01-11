137cebb391c034f06f9168b493d4dc2f508f13b2
rsubramonian <rsubramonian@nerdwallet.com>
2017-01-10 22:13:27 -0800

    ok

--- OPERATORS/F1F2OPF3/lua/f1f2opf3_generator.lua
+++ OPERATORS/F1F2OPF3/lua/f1f2opf3_generator.lua
@@ -1,31 +1,29 @@
 #!/usr/bin/env lua
 
-package.path = package.path.. ";../../../UTILS/lua/?.lua"
+package.path = package.path.. ";../?.lua"
 require("aux")
 
-dofile '../../../UTILS/lua/globals.lua' -- do not hard code locations
+dofile '../globals.lua'
 
-local srcdir = "../gen_src/" 
-local incdir = "../gen_inc/" 
+local srcdir = "../../PRIMITIVES/src/" 
+local incdir = "../../PRIMITIVES/inc/" 
 local T = dofile 'f1f2opf3_operators.lua' 
 local tmpl = dofile 'f1f2opf3.tmpl'
--- Improve following
-local types = { 'I1', 'I2', 'I4', 'I8','F4', 'F8' }
+local types = { 'int8_t', 'int16_t', 'int32_t', 'int64_t','float', 'double' }
 
 for i, v in ipairs(T) do
   local base_name = v
   local str = 'require \'' .. base_name .. '_static_checker\''
-  --  require concat_static_checker.lua
+--  require concat_static_checker.lua
   load(str)()
   for i, in1type in ipairs(types) do 
     for j, in2type in ipairs(types) do 
       for k, returntype in ipairs(types) do 
         stat_chk = base_name .. '_static_checker'
         assert(_G[stat_chk], "no checker for " .. base_name)
-        -- subs = substitutions, incs = includes
-        local subs, incs = 
+        local substitutions, includes = 
         _G[base_name .. '_static_checker'](in1type, in2type, returntype)
-        if ( subs ) then
+        if ( substitutions ) then
           local B = nil; local W = nil
           if ( file_exists(base_name .. "_black_list.lua")) then 
             B = dofile(base_name .. "_black_list.lua")
@@ -41,12 +39,11 @@ for i, v in ipairs(T) do
             error("Cannot have both black and white list")
           end
           -- TODO Improve following.
-          tmpl.fn = subs.fn
-          tmpl.in1type = subs.in1type
-          tmpl.in2type = subs.in2type
-          tmpl.returntype = subs.returntype
-          tmpl.scalar_op = subs.scalar_op
-          local fn = tmpl.fn
+          tmpl.name = substitutions.name
+          tmpl.in1type = substitutions.in1type
+          tmpl.in2type = substitutions.in2type
+          tmpl.returntype = substitutions.returntype
+          tmpl.scalar_op = substitutions.scalar_op
           -- process black/white lists
           local skip = false; local decided = false
           if ( ( B == nil ) and ( W == nil ) ) then 
@@ -60,33 +57,32 @@ for i, v in ipairs(T) do
           if ( ( W ~= nil ) and ( decided == false ) ) then
             if W[fn] then skip = false else skip = true end 
             decided = true
+            if ( skip == true ) then print("Skipping ", fn) end
           end
           if not decided then error("Control cannot come here") end
-          if skip then 
-            print("Skipping ", fn) 
-          else
-            -- print(tmpl 'declaration')
-            doth = tmpl 'declaration'
-            -- print("doth = ", doth)
-            local fname = incdir .. "_" .. subs.fn .. ".h", "w"
-            local f = assert(io.open(fname, "w"))
-            f:write(doth)
-            if ( incs ) then 
-              for i, v in ipairs(incs) do
-                f:write("#include <" .. v .. ".h>\n")
-              end
+          if not skip then 
+          -- print(tmpl 'declaration')
+          doth = tmpl 'declaration'
+          -- print("doth = ", doth)
+          local fname = incdir .. "_" .. substitutions.name .. ".h", "w"
+          local f = assert(io.open(fname, "w"))
+          f:write(doth)
+          if ( includes ) then 
+            for i, v in ipairs(includes) do
+              f:write("#include <" .. v .. ".h>\n")
             end
-            f:close()
-            -- print(tmpl 'definition')
-            dotc = tmpl 'definition'
-            -- print("dotc = ", dotc)
-            local fname = srcdir .. "_" .. subs.fn .. ".c", "w"
-            local f = assert(io.open(fname, "w"))
-            f:write(dotc)
-            f:close()
           end
-        else
-          print("Invalid combo ", in1type, " ", in2type, " ", returntype)
+          f:close()
+          -- print(tmpl 'definition')
+          dotc = tmpl 'definition'
+          -- print("dotc = ", dotc)
+          local fname = srcdir .. "_" .. substitutions.name .. ".c", "w"
+          local f = assert(io.open(fname, "w"))
+          f:write(dotc)
+          f:close()
+        end
+      else
+        -- print("Invalid combo ", in1type, " ", in2type, " ", outtype1)
         end
       end
     end
