744849859dab5082b9d8c2a5d834ca4dd78e1934
rsubramonian <rsubramonian@nerdwallet.com>
2017-01-24 20:43:25 -0800

    ok

--- DATA_LOAD/src/README.sh
+++ DATA_LOAD/src/README.sh
@@ -7,5 +7,13 @@ gcc -c -std=gnu99 txt_to_I1.c  -I../../UTILS/inc
 gcc -c -std=gnu99 txt_to_I2.c  -I../../UTILS/inc
 gcc -c -std=gnu99 txt_to_I4.c  -I../../UTILS/inc
 gcc -c -std=gnu99 txt_to_I8.c  -I../../UTILS/inc
+
+lua extract_func_decl.lua txt_to_I1.c > ../inc/txt_to_I1.h
+lua extract_func_decl.lua txt_to_I2.c > ../inc/txt_to_I2.h
+lua extract_func_decl.lua txt_to_I4.c > ../inc/txt_to_I4.h
+lua extract_func_decl.lua txt_to_I8.c > ../inc/txt_to_I8.h
+lua extract_func_decl.lua txt_to_F4.c > ../inc/txt_to_F4.h
+lua extract_func_decl.lua txt_to_F8.c > ../inc/txt_to_F8.h
+
 echo "COMPLETED SUCCESSFULLY"
 # Now generate header files using ../../UTILS/src/extract_func_decl.lua
