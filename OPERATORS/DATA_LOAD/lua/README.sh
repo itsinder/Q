4dca676c0317aa4d01a82f86842ef92002005f21
rsubramonian <rsubramonian@nerdwallet.com>
2017-01-31 11:22:31 -0800

    ok

--- OPERATORS/DATA_LOAD/lua/README.sh
+++ OPERATORS/DATA_LOAD/lua/README.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 set -e 
+# cleanup
 rm -r -f ../gen_inc/*.h
 rm -r -f ../gen_src/*.h
+# generate all primitives
 lua gen_code.lua 
+# iterate over all generated code=> should compile without warnings
 cd ../gen_src/
 ls *c > _x
 while read line; do
   echo $line
   gcc -c $line -I../gen_inc -I../../../UTILS/inc/
 done< _x
+# cleanup
 rm -f *.o
 rm -f _x
 cd -
