d17f4302e81e65291b8c2cabdd98b47cb3ed99e7
PragatiPailwan <pragati.pailwan@gmail.com>
2017-01-31 12:23:35 +0530

    Modified the README_Data_Load.sh script file.

--- OPERATORS/DATA_LOAD/lua/README_Data_Load.sh
+++ OPERATORS/DATA_LOAD/lua/README_Data_Load.sh
@@ -6,12 +6,22 @@
 #2) Compile the C code and create the QFunc.so file, the command is:
 gcc -fPIC -shared -o ../src/QCFunc.so ../src/QCFunc.c
 
-#3) create the directory ./out and ./metadata .. here output file will be stored
+#3) If out and metadata directory doesnot exists then create the directory ./out and ./metadata .. here the output files (binary files and null files) will be stored.
+#If out, metadata directory exists then remove and create new directories:
+if [ -d "out" ]; then 
+  rm -r out
+fi
+mkdir out
 
-#4) In main.lua file set the path of csv file which u will give as the input file:
-#e.g.: local csv_file_path_name = "../test/csv_inputfile1.csv"  
+if [ -d "metadata" ]; then
+  rm -r metadata
+fi
+mkdir metadata
 
-#5) Adjust metadata in main.lua according to the csv file you are reading 
+#4) If you want to use your CSV file then In main.lua file set the path of csv file which u will give as the input file:
+#e.g.: local csv_file_path_name = "../test/csv_inputfile1.csv"
+
+#5) If you are using your CSV file then adjust the metadata M according to the respective CSV file in main.lua
 
 #6) Then run the main.lua file, the command is:
 luajit main.lua
@@ -19,15 +29,13 @@ luajit main.lua
 
 
 
-
-
 ############## REMAINING Things ############
-# - fix size string 
+
 # - Dictionary for varchar -- testing remaining
 # - NULL value handing -- started working on the same
-# - Metadta's ignore column .. i.e. setting the name as "" (empty string) in metadata 
-# - Storing generated file in the directory specified by global variable -- testing remaining
-# - custom datatype (ts example given in csv_load.pdf)
-#
-#
-#
+# - File Name : what pattern to use to make it 16 characters long
+# - If output/metadata directory does not exists, then it should throw error
+# - Deleting null vector file, if null value is not found in input
+# - Integrate this with function created by Ramesh.
+# - Fix size string 
+# - Custom datatype (ts example given in csv_load.pdf)
