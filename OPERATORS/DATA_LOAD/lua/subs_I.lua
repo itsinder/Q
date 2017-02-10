ca74de4f6abc2e6ea542c7b33b16a7210521bfd2
rsubramonian <rsubramonian@nerdwallet.com>
2017-02-10 13:10:54 -0800

    ok

--- OPERATORS/DATA_LOAD/lua/subs_I.lua
+++ OPERATORS/DATA_LOAD/lua/subs_I.lua
@@ -41,26 +41,3 @@ Entry{
   max_val = 'LLONG_MAX',
   converter = 'strtoll',
 }
-    
-Entry{
-  qtype = "F4",
-  fn = 'txt_to_F4',
-  out_type_displ = 'F4',
-  out_type = 'float',
-  big_out_type = 'double',
-  min_val = 'FLT_MIN',
-  max_val = 'FLT_MAX',
-  converter = 'strtold',
-}
-    
-Entry{
-  qtype = "F8",
-  fn = 'txt_to_F8',
-  out_type_displ = 'F8',
-  out_type = 'double',
-  big_out_type = 'double',
-  min_val = 'DBL_MIN',
-  max_val = 'DBL_MAX',
-  converter = 'strtold',
-}
-
