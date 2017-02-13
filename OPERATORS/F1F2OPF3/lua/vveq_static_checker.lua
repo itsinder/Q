dfdc1842ed037074c56a5e0a61b6f3d5c9f76471
rsubramonian <rsubramonian@nerdwallet.com>
2017-02-12 20:06:14 -0800

    ok

--- OPERATORS/F1F2OPF3/lua/vveq_static_checker.lua
+++ OPERATORS/F1F2OPF3/lua/vveq_static_checker.lua
@@ -6,12 +6,11 @@ function vveq_static_checker(
   returntype = "I1"
   local tmpl = 'f1f2opf3.tmpl'
   local subs = {}
-  local incs = {}
   subs.fn = "vveq_" .. f1type .. "_" .. f2type 
   subs.in1type   = assert(g_qtypes[f1type].ctype)
   subs.in2type   = assert(g_qtypes[f2type].ctype)
   subs.returntype = assert(g_qtypes[returntype].ctype)
   subs.argstype = "void *"
   subs.c_code_for_operator = "c = (a == b); "
-  return subs, incs, tmpl
+  return subs, tmpl
 end
