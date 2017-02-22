56da849400eb04c238a7013f6a57702778d41c74
rsubramonian <rsubramonian@nerdwallet.com>
2017-02-22 12:31:11 -0800

    ok

--- OPERATORS/F_TO_S/lua/min_chk.lua
+++ OPERATORS/F_TO_S/lua/min_chk.lua
@@ -5,8 +5,6 @@ function min_chk(
 
     local tmpl = 'reduce.tmpl'
     local subs = {}
-    -- This includes is just as a demo. Not really needed
-    subs.includes = "#include <math.h>\n#include <curl/curl.h>"
     subs.fn = "min_" .. intype 
     subs.intype = g_qtypes[intype].ctype
     subs.reducer = "mcr_min"
