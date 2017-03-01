faaa84705962760e0db7a2cea4faf4a249280c26
PragatiPailwan <pragati.pailwan@gmail.com>
2017-03-01 19:25:10 +0530

    Implemented unique string approach for varchar columns. Done with internal code review changes.

--- OPERATORS/DATA_LOAD/test/gen_csv_metadata_file.lua
+++ OPERATORS/DATA_LOAD/test/gen_csv_metadata_file.lua
@@ -1,23 +1,14 @@
 package.path = package.path .. ";../../../Q2/code/?.lua;../../../UTILS/lua/?.lua"
-
 require 'globals'
 require 'pl'
-
---Used to escape "'s , so that string can be inserted in csv line
-local function escape_csv (s)
-  if string.find(s, '[,"]') then
-    s = '"' .. string.gsub(s, '"', '""') .. '"'
-  end
-  return s
-end
 
 --to convert row (table) into comma separated line
 local function to_csv (tt)
   local s = ""
-  for _,p in ipairs(tt) do  
-    s = s .. "," .. escape_csv(p)
+  for _,p in ipairs(tt) do 
+    s = s .. "," .. p
   end
-    return(string.sub(s, 2))
+    return(string.sub(s, 2))    
 end
 
 --placing random seed once at start for generating random no. each time
@@ -62,14 +53,14 @@ end
 --generating maximum specified unique strings
 local function dict_size_unique_string(max_str_size, max_idx)
   local str
-  local unique_strings_table = { } --for storing unique strings in table
-  local reverse_storing = { } --for searching string in 0(n) time
-  local idx = 1
+  local unique_strings_table = {'""'} --for storing unique strings in table
+  local reverse_storing = {'""'} --for searching string in 0(n) time
+  local idx = 2
 
   repeat  
     str = random_SV(max_str_size-1)
     if(reverse_storing[str] == nil) then 
-      --generated string is not in the table then insert in both the tables 
+      --IF generated string is not in the table then insert in both the tables 
       unique_strings_table[idx] = str
       reverse_storing[str] = idx
       idx = idx + 1
@@ -90,29 +81,19 @@ function generate_unique_varchar_strings(meta_info)
       
       --calculating possible unique string limit
       local exp_unique_strings= 0
-      for i=1, meta_info[i]['size']-1 do
-        exp_unique_strings = exp_unique_strings + math.pow(#charset, i)
+      for j=1, meta_info[i]['size']-1 do
+        exp_unique_strings = exp_unique_strings + math.pow(#charset, j)
       end
-      print("For string length ",meta_info[i]['size']-1," value is ",exp_unique_strings)
-      
+      --print("For string length ",meta_info[i]['size']-1," value is ",exp_unique_strings)
       -- before gen unique strings checking if max_unique_value is within possible limit
-      if meta_info[i]['max_unique_values'] <= exp_unique_strings then
-        unique_string_tables[i] = {}
-        unique_string_tables[i] = dict_size_unique_string(meta_info[i]['size'],meta_info[i]['max_unique_values'])
-      else
-        print(meta_info[i]['max_unique_values'],"is beyond possible limit value...")
-        os.exit()
-      end
+      assert(meta_info[i]['max_unique_values'] <= exp_unique_strings,"Specified Unique string limit is beyond possible limit value...")
+      unique_string_tables[i] = dict_size_unique_string(meta_info[i]['size'],meta_info[i]['max_unique_values'])
     end
   end  
-  
   if is_varchar_col == false then
-    return false
-  else
-    --pretty.dump(unique_string_tables[2])
-    --print("table size",#unique_string_tables[2])
-    return unique_string_tables
+    return nil 
   end
+  return unique_string_tables
 end
 
 
@@ -126,36 +107,28 @@ function generate_metadata(meta_info)
   for i=1, #meta_info do
     for k = 1 , meta_info[i]['column_count'] do
       metadata_table[idx] = {}
+      metadata_table[idx]['name'] = col_name ..idx
+      metadata_table[idx]['type'] = meta_info[i]['type']
+      metadata_table[idx]['null'] = meta_info[i]['null']
+      
       if meta_info[i]['type']== 'SC' then
-        metadata_table[idx]['name'] = col_name ..idx
-        metadata_table[idx]['type'] = meta_info[i]['type']
         metadata_table[idx]['size'] = meta_info[i]['size']
-        metadata_table[idx]['null'] = meta_info[i]['null']
-        idx = idx +1
       elseif meta_info[i]['type']== 'SV' then
-        metadata_table[idx]['name'] = col_name ..idx
-        metadata_table[idx]['type'] = meta_info[i]['type']
         metadata_table[idx]['size'] = meta_info[i]['size']
-        metadata_table[idx]['null'] = meta_info[i]['null']
         metadata_table[idx]['add'] = meta_info[i]['add']
         metadata_table[idx]['dict'] = "D"..i
         metadata_table[idx]['unique_table_id']= i
         metadata_table[idx]['max_unique_values'] = meta_info[i]['max_unique_values']
         if k==1 then
-          metadata_table[idx]['is_dict'] = false
+          metadata_table[idx]['dict_exists'] = false
         else
-          metadata_table[idx]['is_dict'] = true
+          metadata_table[idx]['dict_exists'] = true
         end
-        idx = idx +1
-      else
-        metadata_table[idx]['name'] = col_name ..idx
-        metadata_table[idx]['type'] = meta_info[i]['type']
-        metadata_table[idx]['null'] = meta_info[i]['null']
-        idx = idx +1
       end
+      idx = idx +1
     end
   end
-  pretty.dump(metadata_table)
+  --pretty.dump(metadata_table)
   return metadata_table
 end
 
@@ -169,7 +142,6 @@ local function fill_table(column_list, chunk_print_size,unique_string_tables)
     --fill table data
     file_data[ind] = {}
     for j=1, col_length do
-      --if col is other than SC and SV
       if column_list[j]['type']=='SC' then
         func ='random_'..column_list[j]['type']
         loadstring("value = " .. func)()
@@ -178,7 +150,7 @@ local function fill_table(column_list, chunk_print_size,unique_string_tables)
       elseif column_list[j]['type']=='SV' then
         local random_no = math.random(1,column_list[j]['max_unique_values'])
         local dict_no = column_list[j]['unique_table_id']
-        --print("col no:",j,"dict no:",dict_no)
+        assert(unique_string_tables~=nil,"Attempting to use unique string table which doesnt exists")
         table.insert(file_data[ind],unique_string_tables[dict_no][random_no])
       else
         local data_type_short_code = column_list[j]['type']
@@ -206,31 +178,25 @@ end
 
 --generating csv file based on metadata (this function returns no of rows generated)
 function generate_csv_file(csv_file_name, metadata_table, row_count, chunk_print_size,unique_string_tables)
-  
   local file = assert(io.open(csv_file_name, 'w')) 
   local no_of_chunks = math.floor(row_count/chunk_print_size)
   local chunks = 1
   local table_data = { }
-  local rows = 0 
   
   while( chunks <= no_of_chunks) do
     table_data = fill_table(metadata_table, chunk_print_size, unique_string_tables)
     write_and_empty_table(chunk_print_size, table_data, file)
     chunks = chunks + 1
-    rows = rows + chunk_print_size
   end
   
   --for the last chunk
   local last_chunk_rows = no_of_chunks*chunk_print_size
   local last_chunk_print_size = 0
   
-  if(last_chunk_rows< row_count)then
+  if(last_chunk_rows < row_count)then
     last_chunk_print_size = row_count - last_chunk_rows
     table_data = fill_table(metadata_table, last_chunk_print_size, unique_string_tables)
     write_and_empty_table(last_chunk_print_size, table_data, file)
   end
-  
-  rows = rows + last_chunk_print_size
-  io.close(file)
-  return rows
+  file:close() --closing csv file
 end
--- OPERATORS/DATA_LOAD/test/test_performance.lua
+++ OPERATORS/DATA_LOAD/test/test_performance.lua
@@ -1,13 +1,12 @@
 package.path = package.path .. ";../lua/?.lua"
-
 local lu = require 'luaunit'
 require 'pl'
 require 'gen_csv_metadata_file'
 require 'load_csv'
+
 local performance_file ="./performance_results/performance_measures.txt"
 
 test_performance = {}
-
 local filep 
   
 function test_performance:setUp()
@@ -19,7 +18,7 @@ function test_performance:setUp()
   
   dir.makepath(_G["Q_DATA_DIR"])
   dir.makepath(_G["Q_META_DATA_DIR"])
-  filep = assert(io.open(performance_file, 'a')) 
+  filep = assert(io.open(performance_file, 'a')) -- append mode so that all testcases writes their result in this file
 end
 
 function test_performance:tearDown()
@@ -29,16 +28,15 @@ function test_performance:tearDown()
   filep:close()
 end
 
---testcase for 100000 rows and 256 cols with varchar
+--testcase for 100000 rows and 259 cols with varchar
 function test_performance:test_rows100000_cols259_var()
   --meta information
   local meta_info= {{type= 'SC', size= 16, column_count= 1, null= false }, 
-    { type= "SV", size= 3, column_count= 3, max_unique_values = 1000, null= false, is_dict= false, add= true },
-    { type= "SV", size= 20, column_count= 1, max_unique_values = 500, null= false, is_dict= false, add= true },
+    { type= "SV", size= 3, column_count= 3, max_unique_values = 1000, null= true, dict_exists= false, add= true },
+    { type= "SV", size= 20, column_count= 1, max_unique_values = 200, null= true, dict_exists= false, add= true },
     { type= 'I1', column_count= 65, null= false }, {type= 'I2', column_count= 63, null= false },
     { type= 'I4', column_count= 63, null= false }, {type= 'F4', column_count= 63, null= false }
   }
-
   --generating maximum dictionary size unique strings
   local unique_string_tables = generate_unique_varchar_strings(meta_info)
   
@@ -46,101 +44,81 @@ function test_performance:test_rows100000_cols259_var()
   local metadata_table = generate_metadata(meta_info)
     
   local csv_file_path ="./_csv_1l_256v.csv"
-  local row_count = 128 -- no of rows you wish to enter
-  local chunk_print_size = 10 -- writing data into files as chunks(i.e. chunk size)
-  local ret_count
- 
+  local row_count = 100000  -- no of rows you wish to enter
+  local chunk_print_size = 1000   -- writing data into files as chunks(i.e. chunk size)
+
   --need to provide csv_file_name, columns_list, no_of_rows, chunk_print_size and unique_str_table(if varchar column exists)
-  if unique_string_tables == false then
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size)
-  else
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
-  end
-  
-  assert(ret_count == row_count, "Error while generating csv file") 
+  generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
   
   local start_time = os.time()
   load(csv_file_path, metadata_table)
   local end_time = os.time()
   local date = (os.date ("%c")) 
   filep:write(string.format("\nRows: %d \t Columns: %d \t Execution time: %.2f seconds \t Date:%s ",
-              ret_count,#metadata_table,end_time-start_time,date))
-  --file.delete(csv_file_path)  
+              row_count,#metadata_table,end_time-start_time,date))
+  file.delete(csv_file_path)  -- deleting generated csv file after getting results
 end
 
---[[
+
 --testcase for 200000 rows and 256 cols with varchar
 function test_performance:test_rows200000_cols256_var()
 
   --meta information
    local meta_info= {{type= 'SC', size= 16, column_count= 1, null= false }, 
-    { type= "SV", size= 20, column_count= 1, max_unique_values = 200, null= false, is_dict= false, add= true },
+    { type= "SV", size= 20, column_count= 1, max_unique_values = 200, null= true, dict_exists= false, add= true },
     { type= 'I1', column_count= 65, null= false }, {type= 'I2', column_count= 63, null= false },
     { type= 'I4', column_count= 63, null= false }, {type= 'F4', column_count= 63, null= false }
   }
-  
   --generating maximum dictionary size unique strings
   local unique_string_tables = generate_unique_varchar_strings(meta_info)
   
-  --generate metadata table
+  --generate metadata table which is to be passed to load csv
   local metadata_table = generate_metadata(meta_info)
-  
+    
   local csv_file_path ="./_csv_2l_256v.csv"
-  local row_count = 295
-  local chunk_print_size = 100
- 
+  local row_count = 200000 -- no of rows you wish to enter
+  local chunk_print_size = 1000 -- writing data into files as chunks(i.e. chunk size)
+
   --need to provide csv_file_name, columns_list, no_of_rows, chunk_print_size and unique_str_table(if varchar column exists)
-  if unique_string_tables == false then
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size)
-  else
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
-  end
-  
-  assert(ret_count == row_count, "Error while generating csv file") 
+  generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
   
   local start_time = os.time()
   load(csv_file_path, metadata_table)
   local end_time = os.time()
   local date = (os.date ("%c")) 
   filep:write(string.format("\nRows: %d \t Columns: %d \t Execution time: %.2f seconds \t Date:%s ",
-              ret_count,#metadata_table,end_time-start_time,date))
-  --file.delete(csv_file_path)  
+              row_count,#metadata_table,end_time-start_time,date))
+  file.delete(csv_file_path) 
 end
 
---testcase for 1000000 rows and 256 cols without varchar
+
+--testcase for 1000000 rows and 256 cols with no varchar
 function test_performance:test_rows1000000_cols256()
   
    local meta_info= {{type= 'SC', size= 16, column_count= 1, null= false }, 
     { type= 'I1', column_count= 66, null= false }, {type= 'I2', column_count= 63, null= false },
     { type= 'I4', column_count= 63, null= false }, {type= 'F4', column_count= 63, null= false }
   }
-
   --generating maximum dictionary size unique strings
   local unique_string_tables = generate_unique_varchar_strings(meta_info)
   
-  --generate metadata table
+  --generate metadata table which is to be passed to load csv
   local metadata_table = generate_metadata(meta_info)
-  
+    
   local csv_file_path ="./_csv_1m_256.csv"
-  local row_count = 150
-  local chunk_print_size = 100
- 
+  local row_count = 1000000 -- no of rows you wish to enter
+  local chunk_print_size = 1000 -- writing data into files as chunks(i.e. chunk size)
+
   --need to provide csv_file_name, columns_list, no_of_rows, chunk_print_size and unique_str_table(if varchar column exists)
-  if unique_string_tables == false then
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size)
-  else
-    ret_count = generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
-  end
-  
-  assert(ret_count == row_count, "Error while generating csv file") 
-  
+  generate_csv_file(csv_file_path, metadata_table, row_count, chunk_print_size,unique_string_tables)
+
   local start_time = os.time()
   load(csv_file_path, metadata_table)
   local end_time = os.time()
   local date = (os.date ("%c")) 
   filep:write(string.format("\nRows: %d \t Columns: %d \t Execution time: %.2f seconds \t Date:%s ",
-              ret_count,#metadata_table,end_time-start_time,date))
-  --file.delete(csv_file_path)  
+              row_count,#metadata_table,end_time-start_time,date))
+  file.delete(csv_file_path)
 end
---]]
+
 os.exit( lu.LuaUnit.run() )
