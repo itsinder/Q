67da77b4f114d12547c04012c37649b9343e5fe7
Pranav Maniar <pranav9428@gmail.com>
2017-03-01 19:33:53 +0530

    - Updated RUN_TEST.sh to set LD_LIBRARY_PATH - Removing the README_DATA_LOAD_Test.txt as it is no longer valid

--- OPERATORS/DATA_LOAD/test/RUN_TEST.sh
+++ OPERATORS/DATA_LOAD/test/RUN_TEST.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 set -e
+
+ld_library_add() {
+    if [ -d "$1" ] && [[ ":$LD_LIBRARY_PATH:" != *":$1:"* ]]; then
+       LD_LIBRARY_PATH="${LD_LIBRARY_PATH:+"$LD_LIBRARY_PATH:"}$1"
+       export LD_LIBRARY_PATH
+    fi
+}
+
+ld_library_add ../../../Q2/code/
+ld_library_add ../obj/
+ld_library_add ../../PRINT/obj/
+
+
 echo "-----------------------------"
 echo "Running Dictionary Test Cases"
 echo "-----------------------------"
