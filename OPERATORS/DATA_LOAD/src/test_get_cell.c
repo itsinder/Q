7c109eb02719fe020587fd109ae7a3335d473ae2
rsubramonian <rsubramonian@nerdwallet.com>
2017-03-06 16:15:08 -0800

    ok

---
+++ OPERATORS/DATA_LOAD/src/test_get_cell.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "q_macros.h"
+#include "q_types.h"
+#include "get_cell.h"
+
+int
+main(
+    int argc,
+    char **argv
+    )
+{
+  int status = 0;
+  char *X = "abc,def,123,456\n"; size_t nX = strlen(X);
+  size_t xidx = 0;
+  int bufsz = 32;
+  char buf[bufssz]; 
+  for ( int i = 0; i < 4; i++ ) {
+  }
+BYE:
+  return status;
+}
