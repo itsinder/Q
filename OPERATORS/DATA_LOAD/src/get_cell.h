e348dcdfd6837fab52563daada9e6ce3e798059f
rsubramonian <rsubramonian@nerdwallet.com>
2017-03-21 06:27:22 -0700

    fixed get-cell test case

--- OPERATORS/DATA_LOAD/src/get_cell.h
+++
@@ -1,9 +0,0 @@
-extern size_t
-get_cell(
-    char *X,
-    size_t nX,
-    size_t xidx,
-    bool is_last_col,
-    char *buf,
-    size_t bufsz
-    );
--- UTILS/build/gen.lua
+++ UTILS/build/gen.lua
@@ -3,6 +3,7 @@ local T = {}
 T[#T+1] = { dir = "/UTILS/src/", scripts = { "gen_files.sh" } }
 T[#T+1] = { dir = "/OPERATORS/DATA_LOAD/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/DATA_LOAD/src/", scripts = { "gen_files.sh" }}
+T[#T+1] = { dir = "/OPERATORS/DATA_LOAD/test/", scripts = { "test_get_cell.sh" }}
 T[#T+1] = { dir = "/OPERATORS/F1F2OPF3/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/PRINT/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/PRINT/src/", scripts = { "gen_files.sh" }}
