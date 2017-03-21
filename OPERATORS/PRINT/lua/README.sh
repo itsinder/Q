b9665867de56f51f4dedf5c6c080da0b3c84c5b0
vijay19482 <vijaykumar.patel@gslab.com>
2017-03-21 15:09:36 +0530

    added latest working files in PRINT

--- OPERATORS/PRINT/lua/README.sh
+++ OPERATORS/PRINT/lua/README.sh
@@ -1,12 +1,21 @@
 #!/bin/bash 
 set -e 
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPT_PATH=$(dirname "$SCRIPT")
+echo $SCRIPT_PATH
+
 #Clean all directory first
 rm -rf ../gen_inc ../gen_src ../obj
+rm -rf bin
 
 #create all the required directory
 mkdir ../gen_src ../gen_inc ../obj
+mkdir bin
 
+export Q_SRC_ROOT=../../../
 
 bash gen_files.sh
 
@@ -23,5 +32,19 @@ gcc -std=gnu99 \
   -fPIC -shared \
   -I../../../UTILS/inc/ \
   -I../gen_inc/
+
+gcc -std=gnu99 -shared \
+  -o ../obj/libinitialize_vector.so \
+  ../src/initialize_vector.c
+
+
+#generate vector_map.so
+cd ../../../Q2/code
+make
+
+export LD_LIBRARY_PATH=../../../Q2/code/:../obj/
+
+cd $SCRIPT_PATH
+luajit main.lua
 
 echo "Completed $0 in $PWD" # TODO
