0e5fd81bca03b9d2543c886f592f18c5266e8a52
rsubramonian <rsubramonian@nerdwallet.com>
2017-03-22 13:21:04 -0700

    ok

--- UTILS/lua/dictionary.lua
+++ UTILS/lua/dictionary.lua
@@ -1,3 +1,4 @@
+--== START: Following is standard stuff for creating a class 
 local Dictionary = {}
 Dictionary.__index = Dictionary
 
@@ -16,30 +17,33 @@ type = function( obj )
     end
     return otype
 end
+--== STOP: Following is standard stuff for creating a class 
 
-function Dictionary.get_instance(dict_name)
+function Dictionary.get_instance(
+  dict_name
+  )
     local dict = _G["Q_DICTIONARIES"][dict_name]
     if not dict then
         local dict = setmetatable({}, Dictionary)
         _G["Q_DICTIONARIES"][dict_name] = dict
+        -- Create a forward map and a reverse map
         dict.string_to_index = {}
         dict.index_to_string = {}
     end
     return dict
 end
 
--- Given a index, if that index exists in dictionary then the string corresponding to that index is returned, null otherwise
 function Dictionary:get_string_by_index(index)
     return self.index_to_string[index]
 end
 
--- Given a string, if that string  exists in dictionary then the corresponding index to that string, null otherwise
 function Dictionary:get_index_by_string(text)
     return self.string_to_index[text]
 end
 
 function Dictionary:add(text)
-    assert(text and text ~= "", "Cannot add nil or empty string in dictionary")
+    assert(text and text ~= "", 
+    "Cannot add nil or empty string in dictionary")
     local index = self:get_index_by_string(text)
     if not index then
         index = #self.index_to_string + 1
@@ -47,19 +51,6 @@ function Dictionary:add(text)
         self.index_to_string[index] = text
     end
     return index
-end
-
-function Dictionary:conditional_add(text, condition)
-    if condition then
-        return self:add(text)
-    else
-        local index = self:get_index_by_string(text)
-        if index then 
-            return index
-        else
-            error("Text does not exist in dictionary")
-        end
-    end
 end
 
 function Dictionary:get_size()
