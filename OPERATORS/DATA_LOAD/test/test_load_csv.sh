57c382661bcdec130964027c14734b94c8faad95
vijay19482 <vijaykumar.patel@gslab.com>
2017-03-29 20:16:18 +0530

    modified test_load_csv.sh

--- OPERATORS/DATA_LOAD/test/test_load_csv.sh
+++ OPERATORS/DATA_LOAD/test/test_load_csv.sh
@@ -1,8 +1,21 @@
 #!/bin/bash 
 set -e 
 
+# get the current directory in $SCRIPT_PATH
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+echo $SCRIPT_PATH
+
 export Q_SRC_ROOT=../../../
-export LD_LIBRARY_PATH=../../../Q2/code/:../obj/:../../PRINT/obj/
+export LD_LIBRARY_PATH=../../../Q2/code/:../obj/:../../PRINT/obj
+
+cd $SCRIPT_PATH
+bash test_load.sh
+
+cd $SCRIPT_PATH/../../PRINT/test
+bash test_print.sh
+
+cd $SCRIPT_PATH
 
 #run test_load_csv
 luajit test_load_csv.lua
