6335cffe79d2bec5a29d4de62e9aaf101aae2617
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-05 18:48:21 +0530

    ok

--- OPERATORS/PRINT/test/test_print.sh
+++ OPERATORS/PRINT/test/test_print.sh
@@ -7,23 +7,23 @@ SCRIPT_PATH=$(dirname "$SCRIPT")
 echo $SCRIPT_PATH
 
 #Clean all directory first
-#rm -rf ../gen_inc ../gen_src ../obj
-rm -rf ../obj
+rm -rf ../gen_inc ../gen_src ../obj
+#rm -rf ../obj
 
 #create all the required directory
-#mkdir ../gen_src ../gen_inc ../obj
-mkdir ../obj
+mkdir ../gen_src ../gen_inc ../obj
+#mkdir ../obj
 
 export Q_SRC_ROOT=../../../
 export LD_LIBRARY_PATH=../../../Q2/code/:../obj/
 
-#cd $SCRIPT_PATH/../lua
+cd $SCRIPT_PATH/../lua
 # generate _xxx_to_txt.c files
 # it is done in this script, because C files are not getting from build.lua from UTILS now
 # will remove this once build.lua is working
-#bash gen_files.sh
+bash gen_files.sh
 
-#cd $SCRIPT_PATH
+cd $SCRIPT_PATH
 
 # simple test to print 5 I4 values = [1,2,3,4,5] and SV values mapped to I4 values
 # from the bin file I4.bin in bin folder.
