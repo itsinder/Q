d4bb774bcab69c6cc0f6883c65043224dd97ba69
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-06 13:07:09 +0530

    revert dictionary class to dev version

--- OPERATORS/PRINT/test/test_print.lua
+++ OPERATORS/PRINT/test/test_print.lua
@@ -30,29 +30,16 @@ assert(status, "compile of .so failed")
 -- require print_csv added here because so file needs to be created beforehand
 require 'print_csv'
 
--- testcase for SV data type
--- e1 mapped to 1 , e2 mapped to 2 and so on
-_G["Q_DICTIONARIES"] = {} 
-local dictionary = Dictionary({dict = "D1", is_dict = false, add=true})
-dictionary:add_with_condition("e1")
-dictionary:add_with_condition("e2")
-dictionary:add_with_condition("e3")
-dictionary:add_with_condition("e4")
-dictionary:add_with_condition("e5")
-
-
 -- I4.bin is a binary file contain 5 I4 digits - 1,2,3,4,5
 local column_I4 = Column{field_type="I4", field_size = 4,chunk_size = 5,filename="./bin/I4.bin"}
-local column_SV = Column{field_type="SV", field_size = 4,chunk_size = 5, filename="./bin/I4.bin"}
-column_SV:set_meta("dir", dictionary)
 
-local arr = {column_I4, column_SV}  
+local arr = {column_I4}  
 local status,err = pcall(print_csv, arr)
 print(err)
 assert(status == true,"Error in print_csv")
 log.info("All is well")
 
 -- output will be in the format
--- 1,e1
--- 2,e2
+-- 1
+-- 2
 -- and so on
