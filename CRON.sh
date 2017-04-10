cdfa58cc3ff55eca449e2688a7fbf23e7be52189
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-10 14:46:03 +0530

    modified CRON.sh . use build.lua for print testcases

--- CRON.sh
+++ CRON.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-set -e
+set +e
 export Q_SRC_ROOT=/home/ubuntu/Q
 cd $Q_SRC_ROOT
 git pull
 
-cd $Q_SRC_ROOT/UTILS/src/
-bash gen_files.sh
-
-cd $Q_SRC_ROOT/OPERATORS/LOAD_CSV/lua/
-bash gen_files.sh
-
-cd $Q_SRC_ROOT/OPERATORS/LOAD_CSV/src/
-bash gen_files.sh
-
-cd $Q_SRC_ROOT/OPERATORS/LOAD_CSV/test/
-bash test_get_cell.sh
-
-cd $Q_SRC_ROOT/OPERATORS/LOAD_CSV/test/testcases/
-bash test_load_csv.sh
+cd $Q_SRC_ROOT/UTILS/build/
+var=$(luajit build.lua)
 
 #check whether night build txt file is present in LOAD_CSV/test/testcases folder
 nightly_file=$Q_SRC_ROOT/OPERATORS/LOAD_CSV/test/testcases/nightly_build.txt
@@ -28,7 +16,7 @@ then
 else
  var1="Error in Creating LOAD TEST CASES"
 fi
-
+rm $nightly_file
 echo "$var1" | /usr/bin/mail -s "Q Unit Tests" vijaykumar.patel@gslab.com 
 
 
