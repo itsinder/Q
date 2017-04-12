2ff0864eacfe733c483a7eab573a3438034cff9a
PragatiPailwan <pragati.pailwan@gmail.com>
2017-04-12 15:49:03 +0530

    fizxed

--- OPERATORS/PRINT/test/test_metadata/gm_print_SV.lua
+++ OPERATORS/PRINT/test/test_metadata/gm_print_SV.lua
@@ -1,4 +1,4 @@
 -- valid SV column contents
 return {
-  { name = "col1", qtype ="SV", dict="D1", is_dict=false, add=true} 
+  { name = "col1", qtype ="SV", dict="D1", is_dict=false, add=true, max_width=1024 } 
 }
