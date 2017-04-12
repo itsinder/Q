c2546c9d1dbd92fe146c7a9fffeddcc4a8691906
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-12 15:38:56 +0530

    modified test_load.sh for creating and removing directory

--- OPERATORS/LOAD_CSV/test/test_load.sh
+++ OPERATORS/LOAD_CSV/test/test_load.sh
@@ -13,8 +13,9 @@ make
 export Q_SRC_ROOT=../../../
 export LD_LIBRARY_PATH=../../../Q2/code/:../obj/
 
-rm -rf ../gen_inc ../gen_src ../obj
-mkdir ../gen_inc ../gen_src ../obj
+cd $SCRIPT_PATH/
+rm -rf ../gen_inc ../gen_src 
+mkdir ../gen_inc ../gen_src 
 
 cd $SCRIPT_PATH/../lua
 bash gen_files.sh
@@ -23,6 +24,8 @@ cd $SCRIPT_PATH/../src
 bash gen_files.sh
 
 cd $SCRIPT_PATH/
+rm -rf ../obj
+mkdir ../obj
 
 luajit test_load.lua meta.lua test.csv
 
