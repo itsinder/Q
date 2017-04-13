6a4f846bd0c7c4d811bfb70da8a215bac4993c79
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-13 23:55:40 +0530

    put comments for fixing performance testing

--- OPERATORS/LOAD_CSV/lua/load_csv.lua
+++ OPERATORS/LOAD_CSV/lua/load_csv.lua
@@ -55,7 +55,8 @@ function mk_out_buf(
   out_buf,
   out_buf_len,
   err_loc
-  )
+)
+    -- commented the below line to fix the performance testing
     --ffi.cdef("size_t strlen(const char *);")
     local in_buf_len = assert( tonumber(cee.strlen(in_buf)))
 
@@ -172,6 +173,7 @@ function load_csv(
 
       local x_idx = 0
       local out_buf_sz = 1024 -- TODO FIX 
+      -- replace the below 3 lines, to fix for performance testing
       local in_buf  = ffi.gc(cee.malloc(max_txt_width), cee.free)      
       local out_buf = ffi.gc(cee.malloc(out_buf_sz), cee.free)      
       local is_nn   = ffi.gc(cee.malloc(1), cee.free)
