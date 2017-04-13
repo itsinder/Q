7d899d8a51d9b3135884cbdd5f3dda987e0d7a52
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-13 22:10:05 +0530

    appended ../../../Q2/code/:../obj/:../../DATA_LOAD/obj in export LD_LIBRARY_PATH

--- OPERATORS/PRINT/test/test_meta_data.sh
+++ OPERATORS/PRINT/test/test_meta_data.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 set -e 
 
 # get the current directory in $SCRIPT_PATH
@@ -6,21 +6,18 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 echo $SCRIPT_PATH
 
-cd $SCRIPT_PATH
-cd ../../../../
+
+cd $SCRIPT_PATH/
+cd ../../../
 export Q_SRC_ROOT="`pwd`"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Q_SRC_ROOT/Q2/code:$Q_SRC_ROOT/OPERATORS/LOAD_CSV/obj:$Q_SRC_ROOT/OPERATORS/PRINT/obj
-# echo $LD_LIBRARY_PATH
 export LUA_INIT="@$Q_SRC_ROOT/init.lua"
 
-cd $SCRIPT_PATH/../
-bash test_load.sh
+cd $SCRIPT_PATH/
+rm -rf metadata/
+mkdir metadata
 
-cd $SCRIPT_PATH/../../../PRINT/test
-bash test_print.sh
+luajit test_meta_data.lua $1
 
-cd $SCRIPT_PATH
-
-#run test_load_csv
-luajit test_load_csv.lua $1
+echo "Completed $0 in $PWD"
 
