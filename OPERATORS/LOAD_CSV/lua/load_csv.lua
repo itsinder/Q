b643f6cc92bf3898710039746e867f2f446fe4d5
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-13 23:49:56 +0530

    fixed performance testing in LOAD_CSV

--- OPERATORS/LOAD_CSV/lua/load_csv.lua
+++ OPERATORS/LOAD_CSV/lua/load_csv.lua
@@ -56,7 +56,7 @@ function mk_out_buf(
   out_buf_len,
   err_loc
   )
-    ffi.cdef("size_t strlen(const char *);")
+    --ffi.cdef("size_t strlen(const char *);")
     local in_buf_len = assert( tonumber(cee.strlen(in_buf)))
 
     if m.qtype == "SV" then 
@@ -172,9 +172,9 @@ function load_csv(
 
       local x_idx = 0
       local out_buf_sz = 1024 -- TODO FIX 
-      local in_buf  = ffi.cast("char  *", ffi.gc(cee.malloc(max_txt_width), ffi.C.free))
-      local out_buf = ffi.cast("char *", ffi.gc(cee.malloc(out_buf_sz), ffi.C.free))
-      local is_nn   = ffi.cast("char *", ffi.gc(cee.malloc(1), ffi.C.free))
+      local in_buf  = ffi.gc(cee.malloc(max_txt_width), cee.free)      
+      local out_buf = ffi.gc(cee.malloc(out_buf_sz), cee.free)      
+      local is_nn   = ffi.gc(cee.malloc(1), cee.free)
       local ncols = #M
       local row_idx = 1
       local col_idx = 1
---
+++ OPERATORS/LOAD_CSV/test/performance_testing/RUN_PERFOMANCE_TEST.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+# get the current directory in $SCRIPT_PATH
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+echo $SCRIPT_PATH
+
+
+cd $SCRIPT_PATH
+cd ../../../../
+export Q_SRC_ROOT="`pwd`"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Q_SRC_ROOT/Q2/code:$Q_SRC_ROOT/OPERATORS/LOAD_CSV/obj
+# echo $LD_LIBRARY_PATH
+export LUA_INIT="@$Q_SRC_ROOT/init.lua"
+
+cd $SCRIPT_PATH/../
+bash test_load.sh
+
+cd $SCRIPT_PATH
+#run test_performance.lua 
+
+echo "-----------------------------"
+echo "Running Perfomance Test Cases"
+echo "-----------------------------"
+luajit test_performance.lua 
+
+echo "DONE"
+
+
