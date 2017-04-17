85be72817765c70d1dce61900c3ae3b3c800f6fb
rsubramonian <rsubramonian@nerdwallet.com>
2017-04-17 14:26:37 -0700

    fixed warnings for idx_sort

--- OPERATORS/IDX_SORT/lua/gen_files.sh
+++ OPERATORS/IDX_SORT/lua/gen_files.sh
@@ -6,12 +6,13 @@ UDIR="$Q_SRC_ROOT/UTILS/lua"
 test -d $UDIR
 unset LUA_INIT
 export LUA_INIT="@linit.lua"
+FLAGS="-std=gnu99 -Wall -fPIC -W -Waggregate-return -Wcast-align -Wmissing-prototypes -Wnested-externs -Wshadow -Wwrite-strings -pedantic -fopenmp"
 lua gen_code.lua 
 cd ../gen_src/
 ls *c > _x
 while read line; do
   echo $line
-  gcc -c $line -I../gen_inc -I../../../UTILS/inc/
+  gcc $FLAGS -c $line -I../gen_inc -I../../../UTILS/inc/
 done< _x
 rm -f *.o
 rm -f _x
