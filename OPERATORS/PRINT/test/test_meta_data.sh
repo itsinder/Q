e8468889baf5891c0ce44357b7beb0b93bf622f0
vijay19482 <vijaykumar.patel@gslab.com>
2017-04-21 14:06:01 +0530

    cleanup of test_meta_data in PRINT

--- OPERATORS/PRINT/test/test_meta_data.sh
+++ OPERATORS/PRINT/test/test_meta_data.sh
@@ -6,15 +6,12 @@ SCRIPT=$(readlink -f "$0")
 SCRIPT_PATH=$(dirname "$SCRIPT")
 echo $SCRIPT_PATH
 
-
 cd $SCRIPT_PATH/
 cd ../../../
 export Q_SRC_ROOT="`pwd`"
 export LUA_INIT="@$Q_SRC_ROOT/init.lua"
 unset LD_LIBRARY_PATH
 `lua | tail -1`
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$Q_SRC_ROOT/Q2/code:$Q_SRC_ROOT/OPERATORS/LOAD_CSV/obj:$Q_SRC_ROOT/OPERATORS/PRINT/obj
 
 cd $SCRIPT_PATH/
 rm -rf metadata/
