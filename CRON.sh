5d8c970da983facd8ba8158b3d5cbe05e55ea888
vijay19482 <vijaykumar.patel@gslab.com>
2017-05-03 16:43:50 +0530

    check luacov file present or not

--- CRON.sh
+++ CRON.sh
@@ -115,7 +115,48 @@ var85=$(lua mk_so.lua /tmp/ 2>&1)
 
 var100="${var100}"$'\n\n'"${var80}"$'\n\n'"${var81}"$'\n\n'"${var82}"$'\n\n'"${var83}"$'\n\n'"${var84}"$'\n\n'"${var85}"
 #echo "$var100" 
-echo "$var100" | /usr/bin/mail -s "Q Unit Tests" projectq@gslab.com,isingh@nerdwallet.com,rsubramonian@nerdwallet.com 
-/usr/bin/mail -s "Lua Coverage Reports" projectq@gslab.com,isingh@nerdwallet.com,rsubramonian@nerdwallet.com -A $Q_SRC_ROOT/Q2/
-code/test_cases/vector.report.txt -A $Q_SRC_ROOT/UTILS/test/dictionary.report.txt -A $Q_SRC_ROOT/OPERATORS/MK_COL/test/testcases/mk_col.report.txt -A $Q_SRC_ROOT/OPERATORS/LOAD_CSV/test/testcases/load_csv.report.txt -A $Q_SRC_ROOT/OPERATORS/DATA_LOAD/test/testcases/data_load.report.txt -A $Q_SRC_ROOT/OPERATORS/PRINT/test/print_csv.report.txt
+
+attach_file=$Q_SRC_ROOT/Q2/code/test_cases/vector.report.txt
+if [ -f $attach_file ] 
+then
+ varattach="-A "$attach_file
+fi
+
+
+attach_file=$Q_SRC_ROOT/UTILS/test/dictionary.report.txt
+if [ -f $attach_file ] 
+then
+ varattach=$varattach" -A "$attach_file
+fi
+
+
+attach_file=$Q_SRC_ROOT/OPERATORS/LOAD_CSV/test/testcases/load_csv.report.txt
+if [ -f $attach_file ] 
+then
+ varattach=$varattach" -A "$attach_file
+fi
+
+
+attach_file=$Q_SRC_ROOT/OPERATORS/DATA_LOAD/test/testcases/data_load.report.txt
+if [ -f $attach_file ] 
+then
+ varattach=$varattach" -A "$attach_file
+fi
+
+
+attach_file=$Q_SRC_ROOT/OPERATORS/PRINT/test/print_csv.report.txt
+if [ -f $attach_file ] 
+then
+ varattach=$varattach" -A "$attach_file
+fi
+
+attach_file=$Q_SRC_ROOT/OPERATORS/MK_COL/test/testcases/mk_col.report.txt
+if [ -f $attach_file ] 
+then
+ varattach=$varattach" -A "$attach_file
+fi
+
+# echo $varattach
+
+echo "$var100" | /usr/bin/mail -s "Q Unit Tests" projectq@gslab.com,isingh@nerdwallet.com,rsubramonian@nerdwallet.com $varattach
 
