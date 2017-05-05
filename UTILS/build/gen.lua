cb2c130645a3da444d6c3b65e889a9833ab6df23
rsubramonian <rsubramonian@nerdwallet.com>
2017-05-05 16:09:49 -0700

    ok

--- UTILS/build/gen.lua
+++ UTILS/build/gen.lua
@@ -5,7 +5,6 @@ T[#T+1] = { dir = "/UTILS/src/", scripts = { "gen_files.sh" } }
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/src/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/F1F2OPF3/lua/", scripts = { "make clean && make" }}
-T[#T+1] = { dir = "/RUNTIME/COLUMN/code/test/", scripts = { "make clean && make" }}
 T[#T+1] = { dir = "/OPERATORS/F1S1OPF2/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/PRINT/lua/", scripts = { "gen_files.sh" }}
 T[#T+1] = { dir = "/OPERATORS/PRINT/src/", scripts = { "gen_files.sh" }}
--- UTILS/build/tests.lua
+++ UTILS/build/tests.lua
@@ -8,6 +8,7 @@ T[#T+1] = { dir = "/OPERATORS/IDX_SORT/test/", scripts = { "run_tests.sh" }}
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/", scripts = { "test_get_cell.sh" }}
 -- RS TODO T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/", scripts = { "run_tests.sh" }}
 -- RS TODO T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/", scripts = { "test_get_cell.sh" }}
+T[#T+1] = { dir = "/RUNTIME/COLUMN/code/test/", scripts = { "make clean && make" }}
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/", scripts = { "test_load.sh" }}
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/testcases/", scripts = { "test_meta_data.sh" }}
 T[#T+1] = { dir = "/OPERATORS/LOAD_CSV/test/testcases/", scripts = { "test_load_csv.sh" }}
