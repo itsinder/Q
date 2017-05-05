bae4d6897a045278224113b827a79ffa148f0fd1
rsubramonian <rsubramonian@nerdwallet.com>
2017-05-05 09:32:16 -0700

    better testing and building for f1f2opf3

--- OPERATORS/F1F2OPF3/test/run_tests.sh
+++ OPERATORS/F1F2OPF3/test/run_tests.sh
@@ -1,11 +1,9 @@
 #!/bibn/bash
 set -e 
-cd ../lua/
-bash gen_files.sh
-cd - 
-gcc -g -std=gnu99 concat.c \
-  -I../gen_inc -I../../../UTILS/inc \
-  ../gen_src/_concat_I1_I2_I4.c
+INCS=" -I.  -I../../../UTILS/inc/ -I../gen_inc/ "
+make -C ../lua/
+#---------------
+gcc -g ${INCS} ${QC_FLAGS} -Werror concat.c ../lua/libf1f2opf3.so
 valgrind ./a.out 2>_x
 set +e
 grep 'ERROR SUMMARY' _x | grep ' 0 errors' 1>/dev/null 2>&1
@@ -13,8 +11,7 @@ status=$?
 if [ $status != 0 ]; then echo VG: FAILURE; else echo VG: SUCCESS; fi 
 set -e 
 #-------------------
-gcc -g -std=gnu99 eq.c \
-  -I../gen_inc -I../../../UTILS/inc \
+gcc -g ${INCS} ${QC_FLAGS} -Werror eq.c \
   ../gen_src/_vveq_F4_F8.c 
 valgrind ./a.out 2>_x
 set +e
@@ -23,5 +20,5 @@ status=$?
 if [ $status != 0 ]; then echo VG: FAILURE; else echo VG: SUCCESS; fi 
 set -e 
 #-------------------
-rm _x
+rm -f _*
 echo "Completed $0 in $PWD"
