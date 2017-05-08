ac7a8d5280c587c2867abd7d9acdd3d6a730e3d5
PragatiPailwan <pragati.pailwan@gmail.com>
2017-05-08 15:04:30 +0530

    cleanup of test_load.lua

--- OPERATORS/DATA_LOAD/test/test_load.lua
+++ OPERATORS/DATA_LOAD/test/test_load.lua
@@ -2,27 +2,7 @@ local log = require 'log'
 local plpath = require 'pl.path'
 local dir = require 'pl.dir'
 local fns = require 'utils'
-local compile_so = require 'compile_so'
-
-local ffi = require 'ffi'
--- Create libload_csv.so
-local incs = {  "../../../UTILS/inc/", "../../../UTILS/gen_inc/", "../gen_inc/"}
-local srcs = {  "../src/get_cell.c",
-          "../src/txt_to_SC.c", 
-          "../gen_src/_txt_to_I1.c",
-          "../gen_src/_txt_to_I2.c",
-          "../gen_src/_txt_to_I4.c",
-          "../gen_src/_txt_to_I8.c",
-          "../gen_src/_txt_to_F4.c",
-          "../gen_src/_txt_to_F8.c",
-          "../../../UTILS/src/is_valid_chars_for_num.c",
-          "../../../UTILS/src/f_mmap.c",
-          "../../../UTILS/src/f_munmap.c"
-       }    
-         
-local tgt = "../obj/libload_csv.so"
-local status = assert(compile_so(incs, srcs, tgt), "compile of .so failed")
-
+local q_core = require 'q_core'
 local load_csv = require 'load_csv_dataload'
 local Column = require 'Column'
 
@@ -33,7 +13,7 @@ assert(plpath.isfile(metadata_file_path), "ERROR: Please check metadata_file_pat
 assert(plpath.isfile(csv_file_path), "ERROR: Please check csv_file_path")
  
 local M = dofile(metadata_file_path)
-fns["preprocess_bool_values"](M, "has_nulls", "dict_exists", "add")
+fns["preprocess_bool_values"](M, "has_nulls", "is_dict", "add")
 
 -- set default values for globals
 _G["Q_DATA_DIR"] = "./out/"     
