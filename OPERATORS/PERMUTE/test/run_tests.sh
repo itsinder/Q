99f69cc23f93ee369b73beaeebcca5b0f22ae7af
srinath <srinath.krishnamurthy@gslab.com>
2017-05-11 14:50:27 +0530

    permute run_tests.sh - validations for env

--- OPERATORS/PERMUTE/test/run_tests.sh
+++ OPERATORS/PERMUTE/test/run_tests.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+if [ -z "$TERRA_HOME" ]  
+then
+  echo "ERR: set TERRA_HOME to point to terra executable"
+  exit -1
+fi
+if [ -z "$Q_SRC_ROOT" ]  
+then
+  echo "ERR: run setup.sh from Q source root dir"
+  exit -1
+fi
+
 export LUA_PATH=";$Q_SRC_ROOT/RUNTIME/COLUMN/code/lua/?.lua;$Q_SRC_ROOT/UTILS/lua/?.lua;$Q_SRC_ROOT/OPERATORS/MK_COL/lua/?.lua;;"
 export LUA_PATH="$LUA_PATH;$Q_SRC_ROOT/OPERATORS/PERMUTE/test/?.lua"
 export LUA_PATH="$LUA_PATH;;"
