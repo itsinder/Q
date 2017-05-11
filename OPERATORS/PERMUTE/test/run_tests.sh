3bb08f2eae43f2df82f8ba76490f15806ec3527e
srinath <srinath.krishnamurthy@gslab.com>
2017-05-11 14:16:43 +0530

    renamed testPermute to testsuite_permute

--- OPERATORS/PERMUTE/test/run_tests.sh
+++ OPERATORS/PERMUTE/test/run_tests.sh
@@ -3,4 +3,4 @@ export LUA_PATH=";$Q_SRC_ROOT/RUNTIME/COLUMN/code/lua/?.lua;$Q_SRC_ROOT/UTILS/lu
 export LUA_PATH="$LUA_PATH;$Q_SRC_ROOT/OPERATORS/PERMUTE/test/?.lua"
 export LUA_PATH="$LUA_PATH;;"
 export TERRA_PATH="$Q_SRC_ROOT/OPERATORS/PERMUTE/terra/?.t;;"
-$TERRA_HOME/terra $Q_SRC_ROOT/UTILS/lua/test_runner.lua permute testPermute
+$TERRA_HOME/terra $Q_SRC_ROOT/UTILS/lua/test_runner.lua permute testsuite_permute
