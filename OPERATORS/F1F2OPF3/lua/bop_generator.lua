5d20163e4617ce9df27753999236ced60f75c8d8
Andrew Winkler <awinkler@nerdwallet.com>
2017-05-18 14:23:11 -0700

    refactor require

--- OPERATORS/F1F2OPF3/lua/bop_generator.lua
+++ OPERATORS/F1F2OPF3/lua/bop_generator.lua
@@ -8,21 +8,8 @@
   local operators = assert(dofile(operator_file))
   local types = { 'B1' }
   for i, operator in ipairs(operators) do
-    -- TODO Fix this 
-    local sp_fn_name = string.format(" require '%s_specialize'", operator)
-    local sp_fn = nil
-    if ( operator == "vvand" ) then 
-      sp_fn = require 'vvand_specialize'
-    elseif ( operator == "vvor" ) then 
-      sp_fn = require 'vvor_specialize'
-    elseif ( operator == "vvxor" ) then 
-      sp_fn = require 'vvxor_specialize'
-    elseif ( operator == "vvandnot" ) then 
-      sp_fn = require 'vvandnot_specialize'
-    else
-      assert(nil, "Bad operator")
-    end
-    -- ==================
+    local sp_fn = require (operator .. '_specialize')
+
     for i, in1type in ipairs(types) do 
       for j, in2type in ipairs(types) do 
         local status, subs, tmpl = pcall(sp_fn, in1type, in2type)
--- OPERATORS/F1F2OPF3/lua/cmp_generator.lua
+++ OPERATORS/F1F2OPF3/lua/cmp_generator.lua
@@ -10,23 +10,8 @@
 
   args = nil -- not being used just yet
   for i, operator in ipairs(operators) do
-    local sp_fn_name = string.format(" require '%s_specialize'", operator)
-    local sp_fn = nil
-    if ( operator == "vveq" ) then 
-      sp_fn = require 'vveq_specialize'
-    elseif ( operator == "vvneq" ) then 
-      sp_fn = require 'vvneq_specialize'
-    elseif ( operator == "vvleq" ) then 
-      sp_fn = require 'vvleq_specialize'
-    elseif ( operator == "vvgeq" ) then 
-      sp_fn = require 'vvgeq_specialize'
-    elseif ( operator == "vvlt" ) then 
-      sp_fn = require 'vvlt_specialize'
-    elseif ( operator == "vvgt" ) then 
-      sp_fn = require 'vvgt_specialize'
-    else
-      assert(nil, "Bad operator")
-    end
+    local sp_fn = require (operator .. '_specialize')
+
     for i, in1type in ipairs(types) do 
       for j, in2type in ipairs(types) do 
         local status, subs, tmpl = pcall(sp_fn, in1type, in2type)
