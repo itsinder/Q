5ec1d94d15e7799a6cc22c7908ba5cb7272a406c
Indrajeet Singh <isingh@nerdwallet.com>
2017-05-18 15:39:38 -0700

    adding an all in one build and basic test script

--- GUIDING_PRINCIPLES/aio.sh
+++ GUIDING_PRINCIPLES/aio.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 ###Install lua , luajit and luarocks
 ### Install lua ####
-sudo apt-get install lua5.1
-sudo apt-get install liblua5.1-dev  
-
-
-######## Lua JIT #########
-wget http://luajit.org/download/LuaJIT-2.0.4.tar.gz
-tar -xvf LuaJIT-2.0.4.tar.gz
-cd LuaJIT-2.0.4/
-make
-sudo make install
-
-wget https://luarocks.org/releases/luarocks-2.4.1.tar.gz
-tar zxpf luarocks-2.4.1.tar.gz
-cd luarocks-2.4.1
-./configure; sudo make bootstrap
-sudo luarocks install penlight
-sudo luarocks install luaposix
+# sudo apt-get install lua5.1 -y
+# sudo apt-get install liblua5.1-dev -y
+# sudo apt-get install unzip -y # for luarocks
+# 
+# ######## Lua JIT #########
+# wget http://luajit.org/download/LuaJIT-2.0.4.tar.gz
+# tar -xvf LuaJIT-2.0.4.tar.gz
+# cd LuaJIT-2.0.4/
+# make
+# udo make install
+# 
+# ######## Luarocks #########
+# wget https://luarocks.org/releases/luarocks-2.4.1.tar.gz
+# tar zxpf luarocks-2.4.1.tar.gz
+# cd luarocks-2.4.1
+# ./configure; sudo make bootstrap
+# sudo luarocks install penlight
+# sudo luarocks install luaposix
+# 
+#  ######## Build Q #########
+# source ../setup.sh -f 
+# cd ../UTILS/build
+# lua build.lua gen.lua
+# lua mk_so.lua /tmp/
+# cd - 
+PROG="
+q_core = require 'q_core'
+q = require 'q'
+require 'globals'
+load_csv = require 'load_csv'
+print_csv = require 'print_csv'
+mk_col = require 'mk_col'
+save = require 'save'
+print('SUCCESS')
+"
+luajit -e "$PROG" &>/dev/null
+RES=$?
+if [[ $RES -eq 0 ]] ; then
+    echo "SUCCESS"
+else
+    echo "FAIL"
+fi
