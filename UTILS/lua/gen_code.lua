2354dfc4466685d469b1a60109b61c56ea3bc481
Indrajeet Singh <isingh@nerdwallet.com>
2017-05-19 13:04:47 -0700

    reverting gen_code

--- UTILS/lua/gen_code.lua
+++ UTILS/lua/gen_code.lua
@@ -1,38 +1,42 @@
 local plpath = require 'pl.path'
 
-local section = { c = 'selection', h = 'definition' }
-
-local function do_replacements(tmpl, subs)
-   local T = dofile(tmpl)
-   for k,v in pairs(subs) do
-      T[k] = v
-   end
-   return T
-end
-
-
-local _dotfile = function(subs, tmpl, opdir, ext)
-  local T = do_replacements(tmpl, subs)
-  local dotfile = T(section[ext])
-  if ( ( not opdir ) or ( opdir == "" ) ) then 
-    return dotfile
-  end
-  assert(plpath.isdir(opdir), "Unable to find opdir " .. opdir)
-  local fname = opdir .. "_" .. subs.fn .. "." .. ext, "w"
-  local f = assert(io.open(fname, "w"))
-  f:write(dotfile)
-  f:close()
-
-end
-
 local fns = {}
+local function do_replacements(tmpl, subs)
+	local T = dofile(tmpl)
+	for k,v in pairs(subs) do
+		T[k] = v
+	end
+	return T
+end
+
 
 fns.dotc = function (subs, tmpl, opdir)
-  _dotfile(subs, tmpl, opdir, 'c')
+	local T = do_replacements(tmpl, subs)
+	local dotc = T 'definition'
+	if ( ( not opdir ) or ( opdir == "" ) ) then
+		return dotc
+	end
+	assert(plpath.isdir(opdir), "Unable to find opdir " .. opdir)
+	local fname = opdir .. "_" .. subs.fn .. ".c", "w"
+	local f = assert(io.open(fname, "w"))
+	f:write(dotc)
+	f:close()
 end
 
 fns.doth = function (subs, tmpl, opdir)
-  _dotfile(subs, tmpl, opdir, 'h')
+	local T = do_replacements(tmpl, subs)
+	local doth = T 'declaration'
+	if ( ( not opdir ) or ( opdir == "" ) ) then
+		return doth
+	end
+	assert(plpath.isdir(opdir), "Unable to find opdir " .. opdir)
+	local fname = opdir .. "_" .. subs.fn .. ".h", "w"
+	local f = assert(io.open(fname, "w"))
+	f:write(doth)
+	f:close()
 end
+
+
+
 
 return fns
