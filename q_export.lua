a15d31662f70287f3a886d186684c2c22d9fa565
ksrinath <srinath.krishnamurthy@gslab.com>
2017-05-27 04:13:24 +0530

    commented logs

--- q_export.lua
+++ q_export.lua
@@ -3,7 +3,7 @@ if not Q_DATA_DIR then Q_DATA_DIR = os.getenv("HOME") .. "/Q/DATA_DIR" end
 local Q_META_DATA_DIR = os.getenv("Q_METADATA_DIR") -- TODO default
 if not Q_META_DATA_DIR then Q_META_DATA_DIR = os.getenv("HOME") .. "/Q/METADATA_DIR" end
 
-print (Q_DATA_DIR)
+--print ("Your Q_DATA_DIR is: " .. Q_DATA_DIR)
 local pldir = require 'pl.dir'
 pldir.makepath(Q_DATA_DIR)
 pldir.makepath(Q_META_DATA_DIR)
