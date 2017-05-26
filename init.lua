9d23928365646ac648748248f5163cdb6344c19c
ksrinath <srinath.krishnamurthy@gslab.com>
2017-05-27 03:55:02 +0530

    removed need for Q_ROOT; include file goes to lua-install dir and so file goes to os library /usr/lib

--- init.lua
+++ init.lua
@@ -1,18 +1,16 @@
 -- requires luaposix have to include in our luarocks def
 -- local stdlib = require("posix.stdlib")
+local function script_path()
+   local str = debug.getinfo(2, "S").source:sub(2)
+   return str:match("(.*/)")
+end
+
+-- needed by q_core; check this init location later.
+require ('Q/q_export').Q_ROOT = script_path()
 
 -- require all the root operator files
 require "Q/OPERATORS/MK_COL/lua/mk_col"
 require "Q/OPERATORS/LOAD_CSV/lua/load_csv"
 require "Q/OPERATORS/PRINT/lua/print_csv"
--------OLD-----------------------------------
-local plpath = require 'pl.path'
 
-local q_root = assert(os.getenv("Q_ROOT"), "Q_ROOT must be set")
-assert(plpath.isdir(q_root), "Q_ROOT not found")
-assert(plpath.isdir(q_root .. "/lib/" ), "Q_ROOT/lib not found")
-assert(plpath.isdir(q_root .. "/include/" ), "Q_ROOT/include not found")
-
--- TODO check
-----------------------------------
 return require 'Q/q_export'
--- q_install.sh
+++ q_install.sh
@@ -5,3 +5,7 @@ cp -r ./UTILS /usr/local/share/lua/5.1/Q
 cp -r ./RUNTIME /usr/local/share/lua/5.1/Q
 cp -r  q_export.lua /usr/local/share/lua/5.1/Q
 cp -r  init.lua /usr/local/share/lua/5.1/Q
+
+# FIX THIS, pick library from build target
+cp /home/srinath/Q/lib/libq_core.so /usr/lib
+cp /home/srinath/Q/include/q_core.h /usr/local/share/lua/5.1/Q
