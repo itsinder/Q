afcd010433062aa8e077551e567b341089d79ddd
ksrinath <srinath.krishnamurthy@gslab.com>
2017-05-28 00:20:37 +0530

    q_install.sh - hack for now for .so and .h; need to use Q_ROOT

--- q_install.sh
+++ q_install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 rm -rf /usr/local/share/lua/5.1/Q
 mkdir /usr/local/share/lua/5.1/Q
 cp -r ./OPERATORS /usr/local/share/lua/5.1/Q
@@ -7,5 +9,5 @@ cp -r  q_export.lua /usr/local/share/lua/5.1/Q
 cp -r  init.lua /usr/local/share/lua/5.1/Q
 
 # FIX THIS, pick library from build target
-cp $Q_ROOT/lib/libq_core.so /usr/lib
-cp $Q_ROOT/include/q_core.h /usr/local/share/lua/5.1/Q
+cp $HOME/Q/lib/libq_core.so /usr/lib
+cp $HOME/Q/include/q_core.h /usr/local/share/lua/5.1/Q
