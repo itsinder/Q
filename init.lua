07987a49f8f7de0ecb313dadaca6482fc75563e3
ksrinath <srinath.krishnamurthy@gslab.com>
2017-05-29 14:54:50 +0530

    moving Q_ROOT to q_core - it belongs there better

--- init.lua
+++ init.lua
@@ -1,13 +1,3 @@
--- requires luaposix have to include in our luarocks def
--- local stdlib = require("posix.stdlib")
-local function script_path()
-   local str = debug.getinfo(2, "S").source:sub(2)
-   return str:match("(.*/)")
-end
-
--- needed by q_core; check this init location later.
-require ('Q/q_export').Q_ROOT = script_path()
-
 -- require all the root operator files
 require "Q/OPERATORS/MK_COL/lua/mk_col"
 require "Q/OPERATORS/LOAD_CSV/lua/load_csv"
