3333981acd73c829e31fecba64388c13ae427999
Garo Brik <garo.brik@gmail.com>
2017-06-02 12:30:25 -0700

    fix logistic regression doc build script

--- RUNTIME/COLUMN/DOC/README.sh
+++ RUNTIME/COLUMN/DOC/README.sh
@@ -5,6 +5,6 @@ filename=$1
 # Sample usage if bash README.sh log_reg
 # Assume that $1 is prefix of filename
 touch .meta
-eval `../../latex/tools/setenv`
-make -f ../../latex/tools/docdir.mk ${filename}.pdf
+eval `../../../latex/tools/setenv`
+make -f ../../../latex/tools/docdir.mk ${filename}.pdf
 echo "Created ${filename}.pdf"
