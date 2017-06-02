7224aa8505f15158215b42503b1425c159bf8b89
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-01 17:31:38 -0700

    ok

--- OPERATORS/F1F2OPF3/test/test_add.lua
+++ OPERATORS/F1F2OPF3/test/test_add.lua
@@ -3,7 +3,8 @@ local c1 = Q.mk_col( {1,2,3,4,5,6,7,8}, "I4")
 local c2 = Q.mk_col( {8,7,6,5,4,3,2,1}, "I4")
 print("---------------------------")
 z = Q.vvadd(c1, c2, "junk")
-local dbg = require 'Q/UTILS/lua/debugger'
+-- local dbg = require 'Q/UTILS/lua/debugger'
+--
 print("xxxxxxxxxxxxxxxxxxxx")
 Q.print_csv(z, nil, "")
 os.exit()
