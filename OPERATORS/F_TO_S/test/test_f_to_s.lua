038a8ae780715d9e2683214e82f30a6e86b2f335
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-05 20:13:31 -0700

    ok

--- OPERATORS/F_TO_S/test/test_f_to_s.lua
+++ OPERATORS/F_TO_S/test/test_f_to_s.lua
@@ -2,6 +2,7 @@ local Q = require 'Q'
 -- local dbg = require 'Q/UTILS/lua/debugger'
 local c1 = Q.mk_col( {1,2,3,4,5,6,7,8}, "I4")
 local z = Q.sum(c1)
+print(z)
 assert(z == 36 )
 local z = Q.min(c1)
 assert(z == 1 )
