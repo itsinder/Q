2718a48a90784b7f309d33c7b81d4c7a31848e68
Indrajeet Singh <isingh@nerdwallet.com>
2017-06-06 07:15:19 -0700

    making c file compilable

--- OPERATORS/custom_lr.c
+++ OPERATORS/custom_lr.c
@@ -1,3 +1,5 @@
+#include <stdint.h>
+
 int
 lr_custom(  
   double *X,
@@ -9,26 +11,32 @@ lr_custom(
   uint32_t cache_size
   )
   {
-    for ( uint32_t i = 0; i < m; i++ ) { 
+    int status = 0; //TODO ramesh please fix
+      for ( uint32_t i = 0; i < m; i++ ) { 
       for ( uint32_t j = 0; j < m; j++ ) { 
         A[i][j] = 0;
       }
     }
+    int num_blocks = 0; //TODO ramesh please fix
     for ( int b = 0; b < num_blocks; b++ ) { 
       for ( int i = 0; i < m; i++ ) { 
         for ( int j = i+1; i < m; i++ ) { 
           double sum = 0;
+          int lb =0 , ub =0 ; //TODO ramesh please fix
+
           for ( int l = lb; l < ub; l++ ) { 
             sum += (X[l] * Y[l] * Z[l]);
           }
+          int k =0; //TODO ramesh please fix
           A[j][k] = sum;
         }
       }
     }
       for ( uint32_t j = 0; j < m; j++ ) { 
-        A[i][j] = 0;
+         int i=0; //TODO ramesh please fix. Just added for q to build
+          A[i][j] = 0;
       }
-    }
+    
 BYE:
     return status;
   }
