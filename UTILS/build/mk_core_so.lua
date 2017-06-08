9b6e5d9b99af570027dc59b9b02cc0debb642b8c
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-08 15:42:39 -0700

    ok

--- UTILS/build/mk_core_so.lua
+++ UTILS/build/mk_core_so.lua
@@ -14,11 +14,24 @@ incs = incs .. " -I" .. src_root .. "/OPERATORS/LOAD_CSV/gen_inc/"
 incs = incs .. " " 
 
 local qcflags = assert(os.getenv("QC_FLAGS"))
+local qlflags = assert(os.getenv("Q_LINK_FLAGS"))
 
+local fn_protos = {}
+-- special case for mmap_struct
+file = io.open(src_root .. "/UTILS/inc/mmap_types.h")
+for line in file:lines() do
+  print(line)
+  start, stop = string.find(line, "#")
+  if ( not start ) then
+    fn_protos[#fn_protos+1] = line
+  end
+end
+--===============================
+io.close()
 local file = io.open(corefiles, "r");
 local arr = {}
 local int n = 0
-local fn_protos = {}
+os.execute("rm -f *.o *.so")
 for line in file:lines() do
   table.insert (arr, line);
   local filename = src_root .. "/" .. line
@@ -28,6 +41,12 @@ for line in file:lines() do
   assert(status == 0, "Failure on " .. command)
   n = n + 1
 end
+
+command = "gcc " .. qlflags .. " *.o -o " .. q_root .. "/libq_core.so"
+print(command)
+status = os.execute(command)
+assert(status == 0, "Failure on " .. command)
+os.execute("rm -f *.o *.so")
 -- TODO Where to create this?
 plfile.write(q_root .. "/include/q_core.h", table.concat(fn_protos, "\n"))
 print("Successfully generated minimal stuff with " .. n .. " files")
