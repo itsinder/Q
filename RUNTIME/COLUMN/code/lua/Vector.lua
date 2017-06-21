271e73e9f97135e0a921ffa818de4ab739a9215e
Indrajeet Singh <isingh@nerdwallet.com>
2017-06-21 16:02:52 -0700

    adding extra check to vector

--- RUNTIME/COLUMN/code/lua/Vector.lua
+++ RUNTIME/COLUMN/code/lua/Vector.lua
@@ -293,6 +293,7 @@ function Vector:eov()
     ffi.C.fflush(self.file)
     self.input_from_file = true
     self.f_map = ffi.gc(qc.f_mmap(self.filename, true), qc.f_munmap)
+    assert(self.f_map ~= ffi.NULL, "Should get back a valid map")
     assert(self.f_map.status == 0, "Mmap failed")
     self.cdata = self.f_map.ptr_mmapped_file
     --mmap the file
