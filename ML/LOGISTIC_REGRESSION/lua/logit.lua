36f01800b61cd232184ff4ba3428494f81854200
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-21 14:04:40 -0700

    ok

--- ML/LOGISTIC_REGRESSION/lua/logit.lua
+++ ML/LOGISTIC_REGRESSION/lua/logit.lua
@@ -5,9 +5,7 @@ return function (x)
   assert(type(x) == "Column", "input must be a column")
 
   local t1 = Q.exp(x)
-  t1:eval()
   local t2 = Q.vsadd(t1, 1)
-  t2:eval()
   local t3 = Q.vvdiv(t1, t2)
   local t4 = Q.vvdiv(t3, t2)
 
@@ -15,8 +13,8 @@ return function (x)
   local nC = math.ceil( x_len / qconsts.chunk_size )
 
   for i = 1, nC do
-    t3:chunk(i)
-    t4:chunk(i)
+    t3:chunk(i-1)
+    t4:chunk(i-1)
   end
 
   return t3, t4
