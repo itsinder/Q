bf8f90ecfa1f3dc8e525237097b0bc5d31f3a1ec
Indrajeet Singh <isingh@nerdwallet.com>
2017-06-24 09:20:50 -0700

    modifying q_install and travis

--- q_install.sh
+++ q_install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
-sudo rm -rf /usr/local/share/lua/5.1/Q
-sudo rm -rf /usr/lib/libq_core.so
-sudo mkdir /usr/local/share/lua/5.1/Q
-sudo cp -r ./OPERATORS /usr/local/share/lua/5.1/Q
-sudo cp -r ./UTILS /usr/local/share/lua/5.1/Q
-sudo cp -r ./RUNTIME /usr/local/share/lua/5.1/Q
-sudo cp -r ./ML /usr/local/share/lua/5.1/Q
-sudo cp -r  q_export.lua /usr/local/share/lua/5.1/Q
-sudo cp -r  init.lua /usr/local/share/lua/5.1/Q
+source ./setup.sh
+rm -rf /usr/local/share/lua/5.1/Q
+rm -rf /usr/lib/libq_core.so
+mkdir /usr/local/share/lua/5.1/Q
+cp -r ./OPERATORS /usr/local/share/lua/5.1/Q
+cp -r ./UTILS /usr/local/share/lua/5.1/Q
+cp -r ./RUNTIME /usr/local/share/lua/5.1/Q
+cp -r ./ML /usr/local/share/lua/5.1/Q
+cp -r  q_export.lua /usr/local/share/lua/5.1/Q
+cp -r  init.lua /usr/local/share/lua/5.1/Q
 
 # FIX THIS, pick library from build target
-sudo cp $Q_ROOT/lib/libq_core.so /usr/lib
-sudo cp $Q_ROOT/include/q_core.h /usr/local/share/lua/5.1/Q
+cp $Q_ROOT/lib/libq_core.so /usr/lib
+cp $Q_ROOT/include/q_core.h /usr/local/share/lua/5.1/Q
