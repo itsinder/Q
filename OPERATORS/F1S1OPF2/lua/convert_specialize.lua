23d5f841d9a0965484063b25e003487968a3e159
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-26 15:29:00 -0700

    ok

--- OPERATORS/F1S1OPF2/lua/convert_specialize.lua
+++ OPERATORS/F1S1OPF2/lua/convert_specialize.lua
@@ -1,37 +1,31 @@
 return function (
   in_qtype,
-  scalar
+  args
   )
-
+  local out_qtype = nil
+  local is_safe   = nil
+  if ( type(args) == "table" ) then
+    out_qtype = assert(args.qtype)
+    is_safe = args.is_safe
+    if ( not is_safe ) then is_safe = false end 
+  else
+    out_qtype = args
+    is_safe = false
+  end
   local is_base_qtype = require 'Q/UTILS/lua/is_base_qtype'
   local qconsts = require 'Q/UTILS/lua/q_consts'
 
-  assert(is_base_qtype(in_qtype))
-  local scalar_val = nil
-  local scalar_qtype = nil
-
-  if ( type(scalar) == "table" ) then
-    scalar_val = scalar.value
-    if ( scalar.qtype ) then
-      scalar_qtype = scalar.qtype
-    else
-      scalar_qtype = in_qtype
-    end
-  else
-    scalar_val = scalar
-    assert( ( type(scalar_val) == "string") or ( type(scalar_val) == "number") )
-    scalar_qtype = in_qtype
-  end
-  local out_qtype = scalar_qtype
-
+  assert(type(out_qtype) == "string")
+  assert(is_base_qtype(out_qtype), "Cannot convert to type " .. out_qtype)
+  local out_ctype = qconsts.qtypes[out_qtype].ctype
+  local in_ctype = qconsts.qtypes[in_qtype].ctype
+  
   local tmpl = 'f1opf2.tmpl'
   local subs = {};
-  subs.fn = "convert_" .. in_qtype .. "_" .. scalar_qtype
-  subs.in_ctype = qconsts.qtypes[in_qtype].ctype
-  subs.out_qtype = out_qtype
-  subs.out_ctype = qconsts.qtypes[subs.out_qtype].ctype
-  subs.c_code_for_operator = "c = (" .. subs.out_ctype .. ") a"
+  subs.fn = "convert_" .. in_qtype .. "_" .. out_qtype
+  subs.c_code_for_operator = "c = (" .. out_ctype .. ") a"
+  subs.in_ctype = in_ctype
+  subs.out_ctype = out_ctype
   subs.c_mem = nil
   return subs, tmpl
-
 end
