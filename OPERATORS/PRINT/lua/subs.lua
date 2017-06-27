7254499fca3cf7c744566d44584d2b484bcd61f0
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-26 21:15:36 -0700

    ok

--- OPERATORS/PRINT/lua/subs.lua
+++ OPERATORS/PRINT/lua/subs.lua
@@ -3,40 +3,35 @@ Entry{
   fn = 'I1_to_txt',
   out_qtype = 'I1',
   out_ctype = 'int8_t',
-  default_fmt = '"%d"',
 }
 Entry{
   qtype = "I2",
   fn = 'I2_to_txt',
   out_qtype = 'I2',
   out_ctype = 'int16_t',
-  default_fmt = '"%d"',
 }
 Entry{
   qtype = "I4",
   fn = 'I4_to_txt',
   out_qtype = 'I4',
   out_ctype = 'int32_t',
-  default_fmt = '"%d"',
 }
 Entry{
   qtype = "I8",
   fn = 'I8_to_txt',
   out_qtype = 'I8',
   out_ctype = 'int64_t',
-  default_fmt = '"%lld"',
 }
 Entry{
   qtype = "F4",
   fn = 'F4_to_txt',
   out_qtype = 'F4',
   out_ctype = 'float',
-  default_fmt = '"%f"',
 }
 Entry{
   qtype = "F8",
   fn = 'F8_to_txt',
   out_qtype = 'F8',
   out_ctype = 'double',
-  default_fmt = '"%lf"'
+  default_fmt = '"%%lf"'
 }
