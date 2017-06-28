0652f83645a56c4401df11aab9e4abfddd367888
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-28 11:44:30 -0700

    code review of Scalar

--- OPERATORS/F1S1OPF2/test/test_convert.lua
+++ OPERATORS/F1S1OPF2/test/test_convert.lua
@@ -6,11 +6,13 @@ local converted_col = Q.convert(input_col, {qtype = "I1"})
 converted_col:eval()
 
 -- Compare converted column with expected column
-local cmp_col = Q.vveq(expected_col, converted_col)
-cmp_col:eval()
+local n = Q.sum(Q.vveq(expected_col, converted_col))
+assert(type(n) == "Scalar")
+len = input_col:length()
+assert(n:eval() == len)
 
 -- Check the compare result
-Q.print_csv(cmp_col, nil, "")
+Q.print_csv(converted_col, nil, "")
 --===========================
-print("Successfully completed" .. arg[0])
+print("Successfully completed " .. arg[0])
 os.exit()
