4fa756ef07462d200584fccfe324d1ec15461104
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-28 12:51:52 -0700

    ok

---
+++ UTILS/src/mix_UI4.c
@@ -0,0 +1,18 @@
+//START_INCLUDES
+#include "mix_UI4.h"
+//STOP_INCLUDES
+//START_FUNC_DECL
+uint32_t
+mix_UI4( 
+    uint32_t a
+    )
+//STOP_FUNC_DECL
+{
+  a = (a+0x7ed55d16) + (a<<12);
+  a = (a^0xc761c23c) ^ (a>>19);
+  a = (a+0x165667b1) + (a<<5);
+  a = (a+0xd3a2646c) ^ (a<<9);
+  a = (a+0xfd7046c5) + (a<<3);
+  a = (a^0xb55a4f09) ^ (a>>16);
+  return a;
+}
