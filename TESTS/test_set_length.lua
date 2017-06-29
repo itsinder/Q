af58c257647175addd5d12119e816256e1be60ad
rsubramonian <rsubramonian@nerdwallet.com>
2017-06-29 11:42:38 -0700

    ok

--- TESTS/test_set_length.lua
+++ TESTS/test_set_length.lua
@@ -3,45 +3,38 @@ local Q = require 'Q'
 local dbg = require 'Q/UTILS/lua/debugger'
 
 
+local x_len = 65537
 for iter = 1, 100 do 
-  local x_len = 65537
   local y = Q.rand({ lb = 0, ub = 1, seed = 1234, qtype = "I1", len = x_len } )
   local c1 = Q.rand({ lb = -1048576, ub = 1048576, seed = 1234, qtype = "F8", len = x_len } )
   local c2 = Q.rand({ lb = -1048576, ub = 1048576, seed = 1234, qtype = "F8", len = x_len } )
   X = {c1, c2}
   local beta = Q.rand({ lb = 0, ub = 1, qtype = "F8", len = 2 } )
-  beta:eval()
 
-  -- dbg()
-  --[[
-  local Xbeta = Q.cmvmul(X, beta)
-  local p = Q.logit(Xbeta)
-  local w = Q.logit2(Xbeta)
-  local ysubp = Q.vvsub(y, p)
-  local temp = ysubp:eval()
-  --]]
   local A = {}
-  local b = {}
-  print('setting up matrices')
   local chk = 0
   local chk2 = 0
+  lengths = {}
+  for i, _ in ipairs(X) do 
+    lengths[i] = 0
+  end
+
   for i, X_i in ipairs(X) do
-    assert( ( X_i:length() == x_len) or ( X_i:length() == 0 ) )
-    print("iter/len Xi = ", iter, i, X_i:length())
+    if ( iter == 1 ) then 
+      assert(X_i:length() == 0 )
+    else
+      assert(X_i:length() > 0 )
+    end
     A[i] = {}
-    -- b[i] = Q.sum(Q.vvmul(X_i, ysubp))
     for j, X_j in ipairs(X) do
       print("iter/i/j/len Xj = ", iter, i, j, X_j:length())
-      -- A[i][j] = Q.sum(Q.vvmul(X_i, Q.vvmul(w, X_j)))
       A[i][j] = Q.sum(Q.vvmul(X_i, X_j))
       chk = A[i][j]:eval()
       if ( i > 1 ) then 
         chk2 = A[i][j]:eval()
         assert ( chk2 == chk )
       end
-
     end
-    -- b[i]:eval()
   end
 end
 print("Completed " .. arg[0])
