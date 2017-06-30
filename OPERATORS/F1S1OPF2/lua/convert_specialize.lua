996d2ddde0dd471628957d8c53bd166a373aa4ad
krushna <krushnakant.mardikar@gslab.com>
2017-06-30 07:33:19 -0700

    Removed the unrequired file c_range

--- OPERATORS/F1S1OPF2/lua/convert_specialize.lua
+++ OPERATORS/F1S1OPF2/lua/convert_specialize.lua
@@ -14,7 +14,6 @@ return function (
   end
   local is_base_qtype = require 'Q/UTILS/lua/is_base_qtype'
   local qconsts = require 'Q/UTILS/lua/q_consts'
-  local crange = require 'Q/OPERATORS/F1S1OPF2/lua/c_range'
 
   assert(type(out_qtype) == "string")
   assert(is_base_qtype(out_qtype), "Cannot convert to type " .. out_qtype)
@@ -27,8 +26,8 @@ return function (
   if is_safe then
     tmpl = 'safe_f1opf2.tmpl'
     --TODO: Write logic to get min_val and max_val according to out_qtype
-    subs.min_val = crange[out_qtype].min_val
-    subs.max_val = crange[out_qtype].max_val
+    subs.min_val = qconsts.qtypes[out_qtype].min
+    subs.max_val = qconsts.qtypes[out_qtype].max
     subs.fn = "safe_convert_" .. in_qtype .. "_" .. out_qtype
   end
   subs.c_code_for_operator = "c = (" .. out_ctype .. ") a; "
