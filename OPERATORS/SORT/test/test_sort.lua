8e9ad6ef998a8e8e28602ac896b332f097beb627
krushna <krushnakant.mardikar@gslab.com>
2017-07-05 03:37:13 -0700

    Updated the SORT test with test-writing convention

--- OPERATORS/SORT/test/test_sort.lua
+++ OPERATORS/SORT/test/test_sort.lua
@@ -1,4 +1,8 @@
+-- FUNCTIONAL
+
 Q = require 'Q'
+require 'Q/UTILS/lua/strict'
+
 x = Q.mk_col({10, 20, 30, 40}, 'I4')
 print(type(x))
 print(x:length())
@@ -8,4 +12,5 @@ Q.print_csv(x, nil, "")
 save = require 'Q/UTILS/lua/save'
 save('tmp.save')
 print("SUCCESS for " .. arg[0])
+require('Q/UTILS/lua/cleanup')()
 os.exit()
