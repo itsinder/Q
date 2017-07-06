27a4579e805a6169b5cfaf4d66d9cd837b7919e4
krushna <krushnakant.mardikar@gslab.com>
2017-07-06 03:47:28 -0700

    Updated the B1 testcase in MK_COL

--- OPERATORS/MK_COL/test/test_mkcol_B1.lua
+++ OPERATORS/MK_COL/test/test_mkcol_B1.lua
@@ -5,11 +5,19 @@ require 'Q/UTILS/lua/strict'
 
 local mk_col = require 'Q/OPERATORS/MK_COL/lua/mk_col'
 -- input table of values 1,2,3 of type I4, given to mk_col
-local status, ret = pcall(mk_col, {1,0,0,0,1,1,0,1,0}, "B1")
+
+local input = {1,0,0,0,1,1,0,1,0}
+local status, ret = pcall(mk_col, input, "B1")
 -- local status, ret = pcall(mk_col, {1.1,5.1,4.5}, "F4")
 assert(status, "Error in mk col ")
 assert(type(ret) == "Column", " Output of mk_col is not Column")
 Q.print_csv(ret, nil, "")
+
+for i=1,ret:length() do
+  local result = ret:get_element(i-1)
+  if result == nil then result = 0 end
+  assert(result == input[i], "Mismatch between input and column values")
+end
 print("SUCCESS")
 
 require('Q/UTILS/lua/cleanup')()
