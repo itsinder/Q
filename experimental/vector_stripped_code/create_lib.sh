6ddf53b53303f046208ed7258a2f14f58f83d876
krushna <krushnakant.mardikar@gslab.com>
2017-07-21 03:25:13 -0700

    Added a script to create the library in vector stripped test, also added required files

---
+++ experimental/vector_stripped_code/create_lib.sh
@@ -0,0 +1,11 @@
+rm -f lib/libq_core.so
+
+gcc -g -std=gnu99 -Wall -fPIC -W -Waggregate-return \
+  -Wcast-align -Wmissing-prototypes -Wnested-externs \
+  -Wshadow -Wwrite-strings -pedantic -fopenmp \
+  c/* \
+  -I./inc/ \
+  -shared -o lib/libq_core.so
+
+echo "================================================"
+echo "Created lib at ./lib/libq_core.so"
