5b56a8c18d5d645e3cb6b8a50f71e5164501bf70
rsubramonian <rsubramonian@nerdwallet.com>
2017-08-10 13:50:01 -0700

    ok

--- experimental/lua-5.1.5/c_modules/expander_gen3.lua
+++ experimental/lua-5.1.5/c_modules/expander_gen3.lua
@@ -3,41 +3,43 @@ local qconsts = require 'Q/UTILS/lua/q_consts'
 local cmem    = require 'libcmem'
 local lVector = require 'lVector'
 
-local counter = 1
-local chunk_size = qconsts.chunk_size
-local field_size = 4
-local base_data = cmem.new(chunk_size * field_size)
-local function expander_my_custom(f1, f2, optargs)
+local function expander_gen3(f1, f2)
+  -- start: hard coding for this test case
+  local counter = 1
+  local chunk_size = qconsts.chunk_size
+  local qtype = "I4"
+  -- stop : hard coding for this test case
+  local field_size = qconsts.qtypes[qtype].width
+  local base_data = assert(cmem.new(chunk_size * field_size))
    -- currently exapnds f1 with the number of times given in f2
-   -- test for valid stuff
    local state = {}
    local function gen3(chunk_idx, col)
-      local f1_len, f1_chunk
-      local f2_len, f2_chunk
-      local start_f1, start_f2, count
-      local f1_chunk_num , f2_chunk_num = chunk_idx, chunk_idx
-      local init_count = 0
+      local start_f1, f1_len, f1_chunk, f1_chunk_num
+      local start_f2, f2_len, f2_chunk, f2_chunk_num
+      local count, init_count
       if state.prev == nil then
-         start_f1 = 1
-         start_f2 = 1
+        f1_chunk_num = chunk_idx
+        f2_chunk_num = chunk_idx
+        start_f1 = 1
+        start_f2 = 1
+        init_count = 0 
       else
-         local f1_chunk_num = state.prev.f1_chunk
-         local f2_chunk_num = state.prev.f2_chunk
-
-         local start_f1 = state.prev.start_f1
-         local start_f2 = state.prev.start_f2
-         init_count = state.prev.count
+        f1_chunk_num = state.prev.f1_chunk_num
+        f2_chunk_num = state.prev.f2_chunk_num
+        start_f1 = state.prev.start_f1
+        start_f2 = state.prev.start_f2
+        init_count = state.prev.count
       end
       local data_size = 0 -- signifying that the chunk is empty
       repeat
-         f1_len, f1_chunk = f1:chunk(f1_chunk_num)
-         f2_len, f2_chunk = f2:chunk(f2_chunk_num)
+         f1_len, f1_chunk = f1:get_chunk(f1_chunk_num)
+         f2_len, f2_chunk = f2:get_chunk(f2_chunk_num)
          if f1_len == 0 or f2_len == 0 then
             break
          end
-         for f1_index=start_f1, f1_chunk do
+         for f1_index=start_f1, f1_chunk_num do
             local f1_val = f1_chunk[f1_index]
-            for f2_index=start_f2, f2_chunk do
+            for f2_index=start_f2, f2_chunk_num do
                local f2_val = f2_chunk[f2_index]
                for iter=init_count, f2_val do
                   base_data[data_size] = f1_val
@@ -61,7 +63,6 @@ local function expander_my_custom(f1, f2, optargs)
       until false
       return data_size, base_data, nil
    end
-   return Column{gen=gen3, nn=false, field_type="I4"}
+   return gen3
 end
-return expander_my_custom
-
+return expander_gen3
