cb7c176433a5230e4bea7d97903c9b002080d58c
Indrajeet Singh <isingh@nerdwallet.com>
2017-08-10 21:25:09 -0700

    fixing gen3

--- experimental/lua-5.1.5/c_modules/expander_gen3.lua
+++ experimental/lua-5.1.5/c_modules/expander_gen3.lua
@@ -10,7 +10,7 @@ local function expander_gen3(f1, f2)
   local qtype = "I4"
   -- stop : hard coding for this test case
   local field_size = qconsts.qtypes[qtype].width
-  local base_data = ffi.cast("int*", assert(cmem.new(chunk_size * field_size)))
+  local base_data = ffi.cast("int*", assert(ffi.C.malloc(chunk_size * field_size)))
    -- currently exapnds f1 with the number of times given in f2
    local state = {}
    local function gen3(chunk_idx, col)
@@ -36,21 +36,23 @@ local function expander_gen3(f1, f2)
          f1_len, f1_chunk = f1:get_chunk(f1_chunk_num)
          f2_len, f2_chunk = f2:get_chunk(f2_chunk_num)
          dbg()
-	 f1_chunk = ffi.cast("int*", f1_chunk)
-	 f2_chunk = ffi.cast("int*", f1_chunk)
-	 if f1_len == 0 or f2_len == 0 then
-            break
+	 if (f1_len == 0 or f2_len == 0)  or (f1_len == nil or f2_len == nil) 
+		or (type(f1) ~= "number" or type(f2) ~= "number") then
+	    return data_size, base_data, nil
          end
+	 f2_chunk = ffi.cast("int*", f1_chunk)
+	 f1_chunk = ffi.cast("int*", f1_chunk)
          for f1_index=start_f1, f1_len do
 	    local f1_val = f1_chunk[f1_index - 1]
             for f2_index=start_f2, f2_len do
                local f2_val = f2_chunk[f2_index - 1]
+               print(f1_index, f1_index,  f1_val, f2_val)
                for iter=init_count, f2_val do
-		  dbg()
-                  base_data[data_size] = f1_val
+		  base_data[data_size] = f1_val
                   -- check if full
                   if data_size + 1 == chunk_size then
-                     local prev = {}
+                     dbg()
+		     local prev = {}
                      prev.start_f1 = f1_index
                      prev.start_f2 = f2_index
                      -- TODO fix so that previous chunk is not asked for aka
