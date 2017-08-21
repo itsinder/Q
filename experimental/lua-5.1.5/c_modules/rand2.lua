98fd4b42f4902061f2c49755096a1e296a98a3b8
krushna <krushnakant.mardikar@gslab.com>
2017-08-21 06:28:24 -0700

    Modified the random test rand1.lua and added rand2.lua which set/unset memo and persist with random boolean value

--- experimental/lua-5.1.5/c_modules/rand2.lua
+++ experimental/lua-5.1.5/c_modules/rand2.lua
@@ -5,11 +5,14 @@ local cmem = require 'libcmem' ;
 local qconsts = require 'Q/UTILS/lua/q_consts'
 local ffi     = require 'Q/UTILS/lua/q_ffi'
 local rand_qtype = require 'rand_qtype'
+local rand_boolean = require 'rand_boolean'
 require 'Q/UTILS/lua/strict'
 local lVector = require 'lVector'
 math.randomseed(os.time())
 
 local xtype = rand_qtype()
+xtype = rand_qtype()
+print(xtype)
 local x = lVector( { qtype = xtype, gen = true, has_nulls = false})
 local base_data = cmem.new(1024)
 ffi.fill(base_data, 1024)
@@ -22,6 +25,15 @@ for i = 1, num_trials do
   local s1 = Scalar.new(i % 127, qtype)
   if ( qtype == xtype ) then counter = counter + 1 end
   pcall(x.put1, x, s1)
+  if ( i % 4 == 0) then
+    local status, ret = pcall(x.memo, x, rand_boolean())
+    if not status then print(ret) end
+    assert(x:check())
+    -- print("+++++++++++++++++++++++++++++++++++")
+    local status, ret = pcall(x.persist, x, rand_boolean())
+    if not status then print(ret) end
+    assert(x:check())
+  end
   -- x:put1(s1)
   assert(x:check())
 end
