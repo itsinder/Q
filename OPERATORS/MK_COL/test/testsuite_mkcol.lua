a8c7887dc6dcc8d4fdf7391114121194b633e0cf
krushna <krushnakant.mardikar@gslab.com>
2017-09-04 05:57:19 -0700

    Added a simple mk_col testcase using test_runner

---
+++ OPERATORS/MK_COL/test/testsuite_mkcol.lua
@@ -0,0 +1,23 @@
+local mk_col = require 'Q/OPERATORS/MK_COL/lua/mk_col'
+local utils = require 'Q/UTILS/lua/test_utils'
+local convert_c_to_txt = require 'Q/UTILS/lua/C_to_txt'
+
+return {
+  tests = 
+    {
+      { 
+        name = "mkcol_I1", 
+        input = { {10, 20, 30, 40, 50, 60}, "I1" },
+        check = function(col)
+              return utils.col_as_str(col) == "10,20,30,40,50,60,"
+          end
+      },
+      { 
+        name = "mkcol_I4", 
+        input = { {10, 20, 30, 40, 50, 60}, "I4" },
+        check = function(col)
+              return utils.col_as_str(col) == "10,20,30,40,50,60,"
+          end
+      },      
+    }
+}
