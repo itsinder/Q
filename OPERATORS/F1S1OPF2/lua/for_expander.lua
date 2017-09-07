58c10095db5510b748375680000ab33bd76ba9e1
krushna <krushnakant.mardikar@gslab.com>
2017-09-07 06:54:11 -0700

    Updated the F1S1OPF2 operator with new vector changes, removed coroutine and used function

--- OPERATORS/F1S1OPF2/lua/for_expander.lua
+++ OPERATORS/F1S1OPF2/lua/for_expander.lua
@@ -1,5 +1,5 @@
 
-    if type(x) == "Column" and ( ( type(y) == "number" ) or ( type(y) == "string" ) ) then
+    if type(x) == "lVector" and ( ( type(y) == "number" ) or ( type(y) == "string" ) ) then
         local status, col = pcall(expander_f1f2opf3, vs<<operator>>, x, y)
         if ( not status ) then print(col) end
         assert(status, "Could not execute vs<<operator>>")
