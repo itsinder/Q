70c188a12feff3de69aab480b142e3ea20006b7c
krushna <krushnakant.mardikar@gslab.com>
2017-09-07 06:54:59 -0700

    Updated the F_TO_S operator with new vector changes, removed coroutine and used function

--- OPERATORS/F_TO_S/lua/expander_f_to_s.lua
+++ OPERATORS/F_TO_S/lua/expander_f_to_s.lua
@@ -1,5 +1,5 @@
 local qconsts = require 'Q/UTILS/lua/q_consts'
-local Scalar  = require 'Q/RUNTIME/SCALAR/lua/Scalar'
+local Scalar  = require 'Q/RUNTIME/lua/Scalar'
 local ffi     = require 'Q/UTILS/lua/q_ffi'
 local qc      = require 'Q/UTILS/lua/q_core'
 -- local dbg = require 'Q/UTILS/lua/debugger'
@@ -8,31 +8,27 @@ return function (a, x )
     local sp_fn_name = "Q/OPERATORS/F_TO_S/lua/" .. a .. "_specialize"
     local spfn = assert(require(sp_fn_name), 
     "Specializer missing " .. sp_fn_name)
-    assert(type(x) == "Column", "input should be a column")
+    assert(type(x) == "lVector", "input should be a column")
     assert(x:has_nulls() == false, "Not set up for null values as yet")
     local x_qtype = assert(x:fldtype())
     local status, subs, tmpl = pcall(spfn, x_qtype)
     assert(status, "Failure of specializer " .. sp_fn_name)
     local func_name = assert(subs.fn)
     assert(qc[func_name], "Function does not exist " .. func_name)
-    local x_coro = assert(x:wrap(), "wrap failed for x")
     local reduce_struct = assert(subs.c_mem)
     local getter = assert(subs.getter)
     assert(type(getter) == "function")
+    local idx = 0
     --==================
-    local lcoro = coroutine.create(function()
-      local x_status, x_len, x_chunk, nn_x_chunk 
-      local idx = 0
-      x_status = true
-      while (x_status) do
-        x_status, x_len, x_chunk, nn_x_chunk = coroutine.resume(x_coro)
-        if x_status and x_len and x_len > 0 then 
-          qc[func_name](x_chunk, x_len, reduce_struct, idx);
-          coroutine.yield(reduce_struct)
-          idx = idx + x_len
-        end -- if
-      end -- while 
-   end)
-    local s =  Scalar ( { coro = lcoro, func = getter } )
+    local gen1 = function(chunk_idx)
+      local x_len, x_chunk, nn_x_chunk = x:chunk(chunk_idx) 
+      if x_len and x_len > 0 then 
+        qc[func_name](x_chunk, x_len, reduce_struct, idx);
+        idx = idx + x_len
+        return reduce_struct
+      end -- if      
+    end
+    
+    local s =  Scalar ( { next = gen1, func = getter } )
     return s
 end
