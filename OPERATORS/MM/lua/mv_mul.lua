2e5ec3abafd00d80e910f3e2b31e1d11ad5336f4
rsubramonian <rsubramonian@nerdwallet.com>
2017-09-21 04:44:44 -0700

    ok

--- OPERATORS/MM/lua/mv_mul.lua
+++ OPERATORS/MM/lua/mv_mul.lua
@@ -35,6 +35,7 @@ local mv_mul = function(X, y)
     -- START: assemble Xptr
     for xidx = 1, #X do
       local x_len, xptr, nn_xptr = X[xidx]:chunk(chunk_idx) 
+      print("x_len = ", x_len)
       assert(nn_xptr == nil, "Don't support null values")
       if ( xidx == 1 ) then
         len = x_len
@@ -50,6 +51,7 @@ local mv_mul = function(X, y)
       -- mv_mul_simple_F4_F4_F4( double ** x, double * y, double * z, int m, int k); 
       local status = qc[func_name](Xptr, yptr, z_buf, len, #X)
       assert(status == 0, "C error in ", func_name)
+      print("len = ", len)
       return len, z_buf, nn_z_buf
     else
       return 0, nil, nil
--- OPERATORS/MM/test/test_mv_mul.lua
+++ OPERATORS/MM/test/test_mv_mul.lua
@@ -1,14 +1,15 @@
 -- FUNCTIONAL 
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
-local x1 = Q.mk_col({10, 20, 30, 40, 50, 60, 70, 80}, 'F8')
+local x1 = Q.mk_col({1, 2, 3, 4, 5, 6, 7, 8}, 'F8')
 local x2 = Q.mk_col({10, 20, 30, 40, 50, 60, 70, 80}, 'F8')
 local X = {x1, x2}
-local Y = Q.mk_col({10, 20}, 'F8')
+local Y = Q.mk_col({100, 200}, 'F8')
 local Z = Q.mv_mul(X, Y)
 Z:eval()
+print(Z:num_elements())
 print("Completed mv_mul")
-Q.print_csv(Z, nil, "")
+Q.print_csv(Z, nil, "_out1.txt")
 print("================")
 print("SUCCESS for ", arg[0])
 require('Q/UTILS/lua/cleanup')()
