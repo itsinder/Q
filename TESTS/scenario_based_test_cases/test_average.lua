78546180a716d7a076e1a0b6e591cb8fbaa3be87
Q <mishra.utpal@gmail.com>
2017-09-29 21:55:47 +0530

    script modified

--- TESTS/scenario_based_test_cases/test_average.lua
+++ TESTS/scenario_based_test_cases/test_average.lua
@@ -1,16 +1,10 @@
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
 
-local metadata = {
- { name = "eng", has_nulls = true, qtype = "I4", is_load = true }, 
- { name = "phy", has_nulls = true, qtype = "I4", is_load = true }
-}
-
 local M = dofile("meta.lua")
-load_csv_result = Q.load_csv("test.csv", metadata)
+load_csv_result = Q.load_csv("test.csv", M)
 
-
-Q.print_csv(load_csv_resut, nil, "")
+--Q.print_csv(load_csv_resut, nil, "")
 
 col_add = Q.vvadd(load_csv_result[1], load_csv_result[2])
 div = Q.mk_col({2, 2, 2, 2}, "I4")
@@ -19,6 +13,6 @@ col_average = Q.vvdiv(col_add, div)
 
 load_csv_result[#load_csv_result + 1] = col_average
 
-Q.print_csv(load_csv_resut, nil, "")
+--Q.print_csv(load_csv_resut, nil, "")
 
 Q.print_csv(load_csv_result, nil, "average.csv")
