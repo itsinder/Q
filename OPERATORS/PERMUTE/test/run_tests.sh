271451e76d0dc25985f97467690e6f907e737ba4
krushna <krushnakant.mardikar@gslab.com>
2017-10-09 02:12:39 -0700

    Changed the interpreter to luajit from terra

--- OPERATORS/PERMUTE/test/run_tests.sh
+++ OPERATORS/PERMUTE/test/run_tests.sh
@@ -6,4 +6,4 @@ then
 fi
 CDIR=`pwd`
 export LUA_PATH="$LUA_PATH;$CDIR/../../../../?.t;;"
-terra $Q_SRC_ROOT/UTILS/lua/test_runner.lua Q/OPERATORS/PERMUTE/terra/permute Q/OPERATORS/PERMUTE/test/testsuite_permute $1
+luajit $Q_SRC_ROOT/UTILS/lua/test_runner.lua Q/OPERATORS/PERMUTE/terra/permute Q/OPERATORS/PERMUTE/test/testsuite_permute $1
