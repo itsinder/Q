398f82245ea364ed0586431798930bdd64808e85
Lua Q Scrabble <mishra.utpal@gmail.com>
2017-10-13 22:26:59 +0530

    corrected the spelling error of use_accelarator as per new code

--- TESTS/scenario_based_test_cases/test_average.lua
+++ TESTS/scenario_based_test_cases/test_average.lua
@@ -2,24 +2,17 @@ local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
 
 local M = dofile("meta.lua")
-load_csv_result = Q.load_csv("test.csv", M, {use_accesslator = false})
-
+load_csv_result = Q.load_csv("test.csv", M, {use_accelerator = false})
 
 Q.print_csv(load_csv_result, nil, "")
 
 col_add = Q.vvadd(load_csv_result[1], load_csv_result[2])
-col_add:eval()
-
-Q.print_csv(col_add, nil, "")
 
 div = Q.mk_col({2, 2, 2, 2}, "I4")
 
 col_average = Q.vvdiv(col_add, div)
-col_average:eval()
 
-Q.print_csv(col_average, nil, "")
-
-load_csv_result[#load_csv_result + 1] = col_average
+load_csv_result[#load_csv_result + 1] = col_average:eval()
 
 Q.print_csv(load_csv_result, nil, "")
 
@@ -27,4 +20,5 @@ Q.print_csv(load_csv_result, nil, "average.csv")
 
 print("SUCCESS for ", arg[0])
 require('Q/UTILS/lua/cleanup')()
+os.execute("rm _*.bin") 
 os.exit()
