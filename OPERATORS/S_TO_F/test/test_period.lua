8c803842d12e0f8b666ea7d38e26658f19ec8a7f
rsubramonian <rsubramonian@nerdwallet.com>
2017-10-18 12:03:17 -0700

    ok

--- OPERATORS/S_TO_F/test/test_period.lua
+++ OPERATORS/S_TO_F/test/test_period.lua
@@ -1,14 +1,20 @@
 -- FUNCTIONAL 
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
-n = 65537
+n = 65536
+print("n = ", n)
 len = n * 3 
 y = Q.period({start = 1, by = 2, period = 3, qtype = "I4", len = len })
-x = Q.sum(y):eval()
-print(x)
-assert(x == (len * (1+3+5)))
+print("about to eval vector")
+y:eval(); print("evaluated vector")
+actual = Q.sum(y):eval()
+expected = (n * (1+3+5))
+if (actual ~= expected ) then 
+  print("FAILURE", actual, expected) 
+else
+  print("SUCCESS for ", arg[0])
+end 
 -- TODO Krushnakant: Q.print_csv(y, nil, "")
-
-print("SUCCESS for ", arg[0])
 require('Q/UTILS/lua/cleanup')()
+os.execute("rm -f _*.bin")
 os.exit()
