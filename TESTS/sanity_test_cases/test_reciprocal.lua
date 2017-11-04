088d74a43cf067137aaadcd2f621798993ae5a65
Lua Q Scrabble <mishra.utpal@gmail.com>
2017-11-04 23:27:27 +0530

    sanity test: reciprocal of reciprocal is number itself

---
+++ TESTS/sanity_test_cases/test_reciprocal.lua
@@ -0,0 +1,25 @@
+-- SANITY TEST
+-- ## Problem: Reciprocal of the reciprocal is the number itself.
+-- ## Using reciprocal to justify the concept.
+
+-- Library Calls
+local Q = require 'Q'
+require 'Q/UTILS/lua/strict'
+
+--local tests = {}
+--tests.t1 = function ()
+
+-- Creating a vector
+local x = Q.rand( { lb = 1, ub = 10000000, qtype = "I4", len = 100000 })
+print(x:eval():length())
+print(Q.sum(Q.vveq(Q.reciprocal(Q.reciprocal(x)), x)):eval())
+
+
+-- Expected Outcome
+--========================================
+assert(Q.sum(Q.vveq(Q.reciprocal(Q.reciprocal(x)), x):eval()) == x:eval():length(), "Vector Mismatch")
+
+--end
+--======================================
+--return tests
+
