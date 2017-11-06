a9ea028d4b1d7f48772785bbcb00ffd7f1882fa1
PragatiPailwan <pragati.pailwan@gmail.com>
2017-11-06 16:50:02 +0530

    minor modifications in the testsuite of load_csv testcases

--- OPERATORS/LOAD_CSV/test/testcases/testsuite_load_csv.lua
+++ OPERATORS/LOAD_CSV/test/testcases/testsuite_load_csv.lua
@@ -2,14 +2,13 @@
 
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
-
 local load_csv = require 'Q/OPERATORS/LOAD_CSV/lua/load_csv'
 local fns = require 'Q/OPERATORS/LOAD_CSV/test/testcases/handle_category'
 local utils = require 'Q/UTILS/lua/utils'
-local dir = require 'pl.dir'
+-- local dir = require 'pl.dir'
 local plpath = require 'pl.path'
 
-local script_dir = "/home/pragati/WORK/Q/OPERATORS/LOAD_CSV/test/testcases/"
+local script_dir = plpath.dirname(plpath.abspath(arg[1]))
 
 local test_input_dir = script_dir .."/test_data/"
 local test_metadata_dir = script_dir .."/test_metadata/"
@@ -17,12 +16,11 @@ local test_metadata_dir = script_dir .."/test_metadata/"
 local T = dofile(script_dir .."/map_metadata_data.lua")
 
 local test_load = {}
---local pretty = require 'pl.pretty'
 
 for i, v in ipairs(T) do
-
-  test_load["execute"..i] = function()
-    
+  -- testcase number which is entered in map file is an index into test_load table 
+  -- which acts as an identifier for the test cases in this test suite.
+  test_load[v.testcase_no] = function()
     print("Running testcase " .. v.testcase_no ..": ".. v.name)
     local M = dofile(test_metadata_dir..v.meta)
     local D = v.data
@@ -30,11 +28,10 @@ for i, v in ipairs(T) do
     local result
     
     local status, ret = pcall(load_csv,test_input_dir..D,  M, opt_args)
-    --local status, ret = load_csv(test_input_dir..D,  M)
+
     local key = "handle_"..v.category
     if fns[key] then
       result = fns[key](i, status, ret, v)
-      -- print("see", result)
     else
       fns["increment_failed_load"](i, v, "Handle function for "..v.category.." is not defined in handle_category.lua")
       result = false
@@ -43,5 +40,5 @@ for i, v in ipairs(T) do
   end
   
 end
--- pretty.dump(test_s)
+
 return test_load
