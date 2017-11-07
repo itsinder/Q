fcf28768c116019636d5a91ef8c5374ef427db67
PragatiPailwan <pragati.pailwan@gmail.com>
2017-11-07 19:26:44 +0530

    'materialized vector .bin file not present' testcase at lVector level writting it in q_testrunner way

--- RUNTIME/test/lVector_test/test_material_vector_file_not_found.lua
+++ RUNTIME/test/lVector_test/test_material_vector_file_not_found.lua
@@ -13,26 +13,33 @@ local M = {
   is_memo = true,
 }
 
-
+local tests = {} 
 local all_qtypes = { "I1", "I2", "I4", "I8", "F4", "F8" }
 
 for i in pairs(all_qtypes) do
-  M.qtype = all_qtypes[i]
-  M.file_name = script_dir .. "/" .. M.file_name
-  print("----------------------------------------------------")
-  print("Running testcase: file_not_found ".. M.qtype)
-  print("START: Deliberate error attempt")
-  local lvec_status, ret = pcall(lVector,  M)
-  print("STOP : Deliberate error attempt")
-  print("lVector return status:",status)
-  if not status then
-    print(ret)
-    ret = nil
-  end
   
-  -- this testcase is a negative testcase 
-  -- if it fails then testcase is passed
-  local status
-  if lvec_status == false then status = true else status = false end
-  utils["testcase_results"]({ name = "file_not_found ".. M.qtype}, "lVector", "Unit Test", status, "")
+  tests[i] = function()
+    M.qtype = all_qtypes[i]
+    M.file_name = script_dir .. "/" .. M.file_name
+    print("----------------------------------------------------")
+    print("Running testcase: file_not_found ".. M.qtype)
+    print("START: Deliberate error attempt")
+    local lvec_status, ret = pcall(lVector,  M)
+    print("STOP : Deliberate error attempt")
+    print("lVector return status:",lvec_status)
+    assert(lvec_status == false)
+    --[[
+    if not status then
+      print(ret)
+      ret = nil
+    end
+     this testcase is a negative testcase 
+    -- if it fails then testcase is passed
+    local status
+    if lvec_status == false then status = true else status = false end
+    ]]
+    --utils["testcase_results"]({ name = "file_not_found ".. M.qtype}, "lVector", "Unit Test", status, "")
+  end
 end
+
+return tests
