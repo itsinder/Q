1fdcc59ea5c8277990346b6ce95b029b75dee77e
Lua Q Scrabble <mishra.utpal@gmail.com>
2017-11-09 20:20:03 +0530

    Modified sanity test scripts for Q Test Runner

--- TESTS/sanity_test_cases/test_sanity_div.lua
+++ TESTS/sanity_test_cases/test_sanity_div.lua
@@ -2,19 +2,15 @@
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
 
--- SANITY TEST
-a = Q.mk_col({1, 2, 3}, "I4")
-b = Q.mk_col({0, 0, 0}, "I4")
+local tests = {}
+tests.t1 = function ()
+  -- SANITY TEST
+  local a = Q.mk_col({1, 2, 3}, "I4")
+  local b = Q.mk_col({0, 0, 0}, "I4")
+  local c = Q.vvdiv(a, b)
+  assert(type(c) == "lVector")
+end
 
-div_result = Q.vvdiv(a, b, { junk = "junk" })
-div_result:eval()
-assert(type(div_result) == "Column")
---Q.print_csv(div_result, nil, "")
---print("##########")
+--=======================================
 
-print("Sanity div Test DONE !!")
-print("------------------------------------------")
-
-print("SUCCESS for " .. arg[0])
-require('Q/UTILS/lua/cleanup')()
-os.exit()
+return tests
--- TESTS/sanity_test_cases/test_sum_reverse.lua
+++ TESTS/sanity_test_cases/test_sum_reverse.lua
@@ -6,24 +6,20 @@
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
 
--- Original Series
-a = Q.seq( {start = 1, by = 1, qtype = "I4", len = 1000} )
-
--- Reverse Series
-b = Q.seq( {start = 999, by = -1, qtype = "I4", len = 1000} )
-
--- Sum of series
-c = Q.vvadd(a, b)
-
--- Expected Outcome
-d = Q.const( { val = 1000, qtype = "I4", len = 1000 })
-
---========================================
-local m = Q.sum(Q.vveq(d, c)):eval()
-print(m)
-assert(m == 1000)
---=======================================
-
-print("SUCCESS for " .. arg[0])
-require('Q/UTILS/lua/cleanup')()
-os.exit()
+local tests = {}
+tests.t1 = function ()
+  -- Original Series
+  local a = Q.seq( {start = 1, by = 1, qtype = "I4", len = 1000} )
+  -- Reverse Series
+  local b = Q.seq( {start = 999, by = -1, qtype = "I4", len = 1000} )
+  -- Sum of series
+  local c = Q.vvadd(a, b)
+  -- Expected Outcome
+  local d = Q.const( { val = 1000, qtype = "I4", len = 1000 })
+  -- Expected Outcome
+  --========================================
+  assert(Q.sum(Q.vveq(d, c)):eval():to_num() == 1000)
+  --=======================================
+end
+--======================================
+return tests
