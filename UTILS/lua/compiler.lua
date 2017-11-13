f659d665366d10ac11b6aa6d46c8d7ad4f494216
Indrajeet Singh <isingh@nerdwallet.com>
2017-11-13 15:50:25 -0800

    adding updated compiler and tests

--- UTILS/lua/compiler.lua
+++ UTILS/lua/compiler.lua
@@ -1,70 +1,17 @@
 local plpath = require 'pl.path'
 local plfile = require 'pl.file'
+local qconsts = require 'Q/UTILS/lua/q_consts'
 local QC_FLAGS= assert(os.getenv("QC_FLAGS"), "QC_FLAGS not provided")
+local LD_LIBRARY_PATH = assert(os.getenv("LD_LIBRARY_PATH"), "LD_LIBRARY_PATH not provided")
+local Q_PATH = LD_LIBRARY_PATH:sub(LD_LIBRARY_PATH:find('[^:]*$'))
+local Q_ROOT = os.getenv("Q_ROOT") 
+local H_DIR = Q_ROOT .. "/include/" 
 local Q_LINK_FLAGS = assert(os.getenv("Q_LINK_FLAGS"), "Q_LINK_FLAGS not provided")
-local tmp_c, tmp_h = "/tmp/dc.c", "/tmp/dc.h"
-
--- local function cleaned_h_file(file)
---   local cmd = string.format("cat %s | grep -v '#include'| cpp | grep -v '^#'", file)
---   local handle = io.popen(cmd)
---   local res = handle:read("*a")
---   handle:close()
---   return res
--- end
---
--- local function is_struct_file(file)
---   assert(plpath.isfile(file), "Could not find file " .. file)
---   if string.match(plfile.read(file), "struct ") then
---     -- TODO Now we need to get the name of the struct and compare it with what we
---     -- have in qc already, for now skipped
---     return true
---   else
---     return false
---   end
---   return nil
--- end
---
--- local function add_h_files_to_list(list, file_list)
---   for i=1,#file_list do
---     list[#list + 1] = cleaned_h_file(file_list[i])
---   end
---   return list
--- end
---
--- local function assemble_cdef(doth)
---   local struct_files = {}
---   local other_h_files = {}
---   for i=1,#doth do
---     local file = doth[i]
---     local contents = cleaned_h_file(file)
---     if is_struct_file(file) == true then
---       struct_files[#struct_files + 1] = contents
---     else
---       other_h_files[#other_h_files + 1] = contents
---     end
---   end
---   local all_files = struct_files
---   for i=1,#other_h_files do
---     all_files[#all_files + 1] = other_h_files[i]
---   end
---   return table.concat(all_files, "\n")
--- end
---
---
--- local function compile(doth, dotc, libname)
---   assert(dotc ~= nil and type(dotc) == "table" and #dotc >= 1, "need a valid table with at least one dot c file")
---   assert(doth ~= nil and type(doth) == "table" and #doth >= 1, "need a valid table of dot h files")
---   local q_cmd = string.format("gcc %s %s -I %s %s -o %s",
---   QC_FLAGS, table.concat(dotc, " "), table.concat(doth, " "), Q_LINK_FLAGS, libname)
---   local status = os.execute(q_cmd)
---   assert(status, "gcc failed for command: " .. q_cmd)
---   assert(plpath.isfile(libname), "Target " .. libname .. " not created")
---   print("Successfully created " .. libname)
---   return assemble_cdef(doth)
--- end
+-- local tmp_c, tmp_h = "/tmp/dc.c", "/tmp/dc.h"
 
 local function cleaned_h_file(h_file)
-  local cmd = string.format("cat \"%s\" | grep -v '#include'| cpp | grep -v '^#'", h_file)
+  -- local cmd = string.format([[cat %s | sed 's/\\n/\n/g'| grep -v '#include'| cpp | grep -v '^#']], h_file)
+  local cmd = string.format([[cat %s | grep -v '#include'| cpp | grep -v '^#']], h_file)
   local handle = io.popen(cmd)
   local res = handle:read("*a")
   handle:close()
@@ -73,10 +20,8 @@ end
 
 local function write_to_file(content, fname)
   local file = assert(io.open(fname, "w+"), "unable to create " .. fname)
-  print(content)
-  print(content:gsub('\n', [[\n]]))
-  local str = content:gsub('\n', [[\n]])
-  file:write(str)
+  -- local str = content:gsub('\n', [[\n]])
+  file:write(content)
   file:close()
 
 end
@@ -84,13 +29,23 @@ end
 local function compile(doth, dotc, libname)
   assert(dotc ~= nil and type(dotc) == "string", "need a valid string that is the dot c file")
   assert(doth ~= nil and type(doth) == "string", "need a valid string that is the  dot h file")
+  local lib_path = string.format("%s/lib%s.so", Q_PATH, libname)
+  local tmp_c, tmp_h = string.format("/tmp/%s.c", libname), string.format("/tmp/%s.h", libname)
   write_to_file(dotc, tmp_c)
   write_to_file(doth, tmp_h)
-  local q_cmd = string.format("gcc %s %s -I %s %s -o lib%s.so", QC_FLAGS, tmp_c, tmp_h, Q_LINK_FLAGS, libname)
+  local q_cmd = string.format("gcc %s %s -I %s %s -o %s", QC_FLAGS, tmp_c,
+    '/tmp/', Q_LINK_FLAGS, lib_path)
   local status = os.execute(q_cmd)
-  assert(status, "gcc failed for command: " .. q_cmd)
-  assert(plpath.isfile(libname), "Target " .. libname .. " not created")
-  print("Successfully created " .. libname)
-  return cleaned_h_file(tmp_h)
+  assert(status == 0, "gcc failed for command: " .. q_cmd)
+  assert(plpath.isfile(lib_path), "Target " .. libname .. " not created")
+  -- print("Successfully created " .. libname)
+  -- if qconsts.debug ~= true then
+  --   plfile.delete(tmp_c)
+  --   plfile.delete(tmp_h)
+  -- end
+  local h_file = cleaned_h_file(tmp_h)
+  local h_file_path = H_DIR ..  libname .. ".h"
+  plfile.write(h_file_path, h_file)
 end
+
 return compile
--- UTILS/test/test_compiler.lua
+++ UTILS/test/test_compiler.lua
@@ -1,3 +1,19 @@
 require 'strict'
+local ffi = require 'ffi'
 x = require 'compiler'
-x('#include <stdio.h>', 'int main(){printf("hello\n");}', 't')
+local dotc = [[ #include "./boom.h"
+int boom(){
+printf("hello\n");
+return 0;
+}
+]]
+local doth = [[ #include <stdio.h>
+int boom();
+]]
+-- local cdef = x('#include <stdio.h>\nint boom(void);', '#include <stdio.h>\nint boom(void){printf("hello\n"); return 0;}', 't')
+local cdef = x(doth, dotc, 'boom')
+print(cdef)
+ffi.cdef(cdef)
+local X = ffi.load('libboom.so')
+print(X.boom())
+
