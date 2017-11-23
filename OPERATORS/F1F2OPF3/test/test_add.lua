2f5455003fe86ea48515650827d52cf1c9448656
krushna <krushnakant.mardikar@gslab.com>
2017-11-23 08:13:28 -0800

    Updated the F1F2OPF3 expander to make chunk_idx local, also added test

--- OPERATORS/F1F2OPF3/test/test_add.lua
+++ OPERATORS/F1F2OPF3/test/test_add.lua
@@ -1,9 +1,10 @@
 -- FUNCTIONAL
 local Q = require 'Q'
+local c_to_txt = require 'Q/UTILS/lua/C_to_txt'
 require 'Q/UTILS/lua/strict'
 
 local tests = {}
-  tests.t1 = function()
+tests.t1 = function()
   local c1 = Q.mk_col( {1,2,3,4,5,6,7,8}, "I4")
   local c3 = c1
   c1 = 10
@@ -14,4 +15,27 @@ local tests = {}
   assert(Q.sum(Q.vvneq(z, Q.mk_col({81,72,63,54,45,36,27,18}, "I4"))):eval():to_num() == 0 )
   print("Test t1 succeeded")
 end
+
+tests.t2 = function()
+  local input_table1 = {}
+  local input_table2 = {}
+  local expected_table = {}
+  for i = 1, 65540 do
+    input_table1[i] = i
+    input_table2[i] = i * 10
+    expected_table[i] = i + (i * 10)
+  end
+  local c1 = Q.mk_col(input_table1, "I4")
+  local c2 = Q.mk_col(input_table2, "I4")
+  local expected_col = Q.mk_col(expected_table, "I4")
+  
+  -- Perform vvadd
+  local res = Q.vvadd(c1, c2)
+  res:eval()
+  
+  -- Verification
+  assert(Q.sum(Q.vvneq(res, expected_col)):eval():to_num() == 0)
+  print("Test t2 succeeded")
+end
+
 return tests
