466f1a11264ffea358c4fb0c252a5aa90ca4ee8a
Lua Q Scrabble <mishra.utpal@gmail.com>
2017-11-23 12:55:01 +0530

    MODIFIED: TESTS/test_cast.lua:os.exit() 	MODIFIED: TESTS/test_x.lua:os.exit() 	MODIFIED: TESTS/dotprod_spec.lua:os.exit() 	MODIFIED: TESTS/sample_load_mk-col_print_test/sample_load_mk-col_print_test.lua:os.exit()

--- TESTS/test_cast.lua
+++ TESTS/test_cast.lua
@@ -8,7 +8,9 @@ tests.t1 = function ()
   local c1 = Q.mk_col( {1,1,1,1}, "I4")
   local c2 = Q.cast(c1, "B1")
   local n1, n2 = Q.sum(c2):eval()
-  assert(n2:to_num() == 4)
+  assert(n1:to_num() == 4)
+  assert(n2:to_num() == 4*32)
+  print("test t1 succeeded")
 end
 
 tests.t2 = function ()
@@ -16,15 +18,19 @@ tests.t2 = function ()
 	local c1 = Q.mk_col( {2,2,2,2}, "I4")
 	local c2 = Q.cast(c1, "B1")
 	local n1, n2 = Q.sum(c2):eval()
-	assert(n2:to_num() == 4)
+  assert(n1:to_num() == 4)
+  assert(n2:to_num() == 4*32)
+  print("test t2 succeeded")
 end
 
 tests.t3 = function ()
 --============================= Now twice as many bits set 
-	local c1 = Q.mk_col( {3,3,3,3}, "I4")
+	local c1 = Q.mk_col( {3,3,3,3}, "I8")
 	local c2 = Q.cast(c1, "B1")
 	local n1, n2 = Q.sum(c2):eval()
-	assert(n2:to_num() == 8)
+  assert(n1:to_num() == 8)
+  assert(n2:to_num() == 8*32)
+  print("test t3 succeeded")
 end
 
 tests.t4 = function ()
@@ -35,6 +41,7 @@ tests.t4 = function ()
 	assert(c2:num_elements() == 16)
 	local sum2 = Q.sum(c2):eval():to_num()
 	assert(sum1 == sum2)
+  print("test t4 succeeded")
 end
 
 tests.t5 = function ()
@@ -43,6 +50,7 @@ tests.t5 = function ()
 	local c1 = Q.mk_col( {1,2,3}, "I4")
 	local status, c2 = pcall(Q.cast, c1, "F8")
 	assert(not status)
+  print("test t5 succeeded")
 end
 
 tests.t6 = function ()
@@ -50,65 +58,7 @@ tests.t6 = function ()
 	local c1 = Q.mk_col( {1,2,3}, "I4")
 	local status, c2 = pcall(Q.cast, c1, "B1")
 	assert(not status)
+  print("test t6 succeeded")
 end
-
-=======
-local tests ={}
-tests.t1 = function() 
-  -- this must work because B1 must be multiple of 8 bytes
-  c1 = Q.mk_col( {1,1,1,1}, "I4")
-  c2 = Q.cast(c1, "B1")
-  assert(Q.sum(c2):eval():to_num() == 4)
-end
---=============================
-tests.t2 = function()
-  -- Note that since we are counting number of bits, same rslt
-  c1 = Q.mk_col( {2,2,2,2}, "I4")
-  c2 = Q.cast(c1, "B1")
-  assert(Q.sum(c2):eval():to_num() == 4)
-end
---=============================
-tests.t3 = function()
-  -- Now twice as many bits set 
-  c1 = Q.mk_col( {3,3,3,3}, "I4")
-  c2 = Q.cast(c1, "B1")
-  assert(Q.sum(c2):eval():to_num() == 8)
-end
---=============================
-tests.t4 = function()
-  -- can convert 8 I*s into 16 I4's or 32 I2's or 64 I1's
-  c1 = Q.mk_col( {1,2,3,4,5,6,7,8}, "I8")
-  local num_elems_I8 = c1:length()
-  sum1 = Q.sum(c1):eval():to_num()
-  c2 = Q.cast(c1, "I4")
-  assert(c2:num_elements() == 2*num_elems_I8)
-  sum2 = Q.sum(c2):eval():to_num()
-  assert(sum1 == sum2)
-
-  c2 = Q.cast(c1, "I2")
-  assert(c2:num_elements() == 4*num_elems_I8)
-
-  c2 = Q.cast(c1, "I1")
-  assert(c2:num_elements() == 8*num_elems_I8)
-end
---=============================
-tests.t5 = function()
-  -- let's do some things that should not work
-  -- this will not work because file size not multiple of element size
-  c1 = Q.mk_col( {1,2,3}, "I4")
-  print("START DELIBERATE FAILURE")
-  status, c2 = pcall(Q.cast, c1, "F8")
-  print("STOP  DELIBERATE FAILURE")
-  assert(not status)
-end
---===========================
-tests.t6 = function() 
-  -- this will not work because B1 must be multiple of 8 bytes
-  c1 = Q.mk_col( {1,2,3}, "I4")
-  print("START DELIBERATE FAILURE")
-  status, c2 = pcall(Q.cast, c1, "B1")
-  print("STOP  DELIBERATE FAILURE")
-  assert(not status)
-end
-
+--=======================================
 return tests
