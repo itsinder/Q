3facd0f6bea9de218d604f11d02b03b998ebf4aa
PragatiPailwan <pragati.pailwan@gmail.com>
2017-12-19 15:52:32 +0530

    JIRA: QQ-50: Written test_lVector into smaller tests, test_lVector_nascent and test_lVector_materialized

--- RUNTIME/test/test_lVector_materialized.lua
+++ RUNTIME/test/test_lVector_materialized.lua
@@ -5,7 +5,6 @@ local Scalar  = require 'libsclr'
 local cmem    = require 'libcmem'  
 local lVector = require 'Q/RUNTIME/lua/lVector'
 local fns = require 'Q/RUNTIME/test/generate_csv'
-local qconsts = require 'Q/UTILS/lua/q_consts'
 local ffi     = require 'Q/UTILS/lua/q_ffi'
 local path_to_here = os.getenv("Q_SRC_ROOT") .. "/RUNTIME/test/"
 assert(plpath.isdir(path_to_here))
@@ -16,11 +15,13 @@ local ival
 local databuf
 local x
 local T
-local md -- meta data 
+local md -- meta data
 
+-- testcases for lVector ( materialized vector )
 local tests = {} 
+
 --===================
-function pr_meta(x, file_name)
+local function pr_meta(x, file_name)
   local T = x:meta()
   local temp = io.output() -- this is for debugger to work 
   io.output(file_name)
@@ -37,13 +38,14 @@ function pr_meta(x, file_name)
   return T
 end
 --=========================
-function compare(f1, f2)
+local function compare(f1, f2)
   local s1 = plfile.read(f1)
   local s2 = plfile.read(f2)
   assert(s1 == s2, "mismatch in " .. f1 .. " and " .. f2)
 end
 --=========================
---
+
+--====== Testing materialized vector
 tests.t1 = function()
   -- deleting previous .csv file if present
   --os.execute("rm -f in1_I4.csv")
@@ -74,6 +76,7 @@ tests.t1 = function()
 end
 --=========
 
+
 tests.t2 = function()
   -- deleting previous .csv file if present
   --os.execute("rm -f in2_I4.csv")
@@ -100,157 +103,14 @@ tests.t2 = function()
   len, base_data, nn_data = x:chunk(100)
   assert(not base_data)
   assert(not nn_data)
+  print("Successfully completed test t2")
   plfile.delete(path_to_here .. "/in2_I4.csv")
   plfile.delete(path_to_here .. "/_in2_I4.bin")
   --=========
 end
 
---====== Testing nascent vector
-tests.t3 = function()
-  print("Creating nascent vector")
-  x = lVector( { qtype = "I4", gen = true, has_nulls = false})
-  local num_elements = 1024
-  local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  local iptr = ffi.cast("int32_t *", base_data)
-  for i = 1, num_elements do
-    iptr[i-1] = i*10
-  end
-  x:put_chunk(base_data, nil, num_elements)
-  assert(x:check())
-  x:eov()
-  assert(x:check())
-  pr_meta(x, "_xxx")
-  print("Successfully completed test t3")
-end
-
---====== Testing nascent vector with scalars
-tests.t4 = function()
-  x = lVector( { qtype = "I4", gen = true, has_nulls = false})
-  local num_elements = 1024
-  local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  local iptr = ffi.cast("int32_t *", base_data)
-  for i = 1, num_elements do
-    local s1 = Scalar.new(i*11, "I4")
-    x:put1(s1)
-    assert(x:check())
-  end
-  x:eov()
-  assert(x:check())
-  md = pr_meta(x, "_meta_data")
-  -- print(">>>> ", md.base.file_name)
-  assert(plpath.getsize(md.base.file_name) == num_elements * field_size)
-  -- Check that nn_file_name does not exist
-  local s = plfile.read("_meta_data")
-  local x, y = string.find(s, "nn_file_name")
-  assert(not x)
-  print("Successfully completed test t4")
-end
-
-tests.t5 = function()
-  x = lVector( { qtype = "I4", gen = true})
-  local num_elements = 1024
-  local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  local status = pcall(x.put_chunk, base_data, nil, num_elements)
-  assert(not status)
-  print("Successfully completed test t5")
-end
-
-tests.t6 = function()
-  print("Testing nascent vector with scalars and nulls")
-  x = lVector( { qtype = "I4", gen = true})
-  local num_elements = 1024
-  local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  print("----------------------_XXX----------")
-  local iptr = ffi.cast("int32_t *", base_data)
-  for i = 1, num_elements do
-    local s1 = Scalar.new(i*11, "I4")
-    local s2
-    if ( ( i % 2 ) == 0 ) then
-      s2 = Scalar.new(true, "B1")
-    else
-      s2 = Scalar.new(false, "B1")
-    end
-    x:put1(s1, s2)
-    assert(x:check())
-  end
-  x:eov()
-  assert(x:check())
-  md = pr_meta(x, "_meta_data")
-  assert(plpath.getsize(md.base.file_name) == num_elements * field_size)
-  assert(plpath.getsize(md.nn.file_name) == num_elements / 8)
-  -- Check that nn_file_name exists
-  local s = plfile.read("_meta_data")
-  local x, y = string.find(s, "nn_file_name")
-  assert(x)
-  print("Successfully completed test t6")
-end
---===========================================
-
---====== Testing nascent vector with generator
-tests.t7 = function()
-  print("Creating nascent vector with generator")
-  local gen1 = require 'Q/RUNTIME/test/gen1'
-  x = lVector( { qtype = "I4", gen = gen1, has_nulls = false} )
-  x:persist(true)
-
-  local x_num_chunks = 10
-  local num_chunks = 0
-  local chunk_size = qconsts.chunk_size
-  for chunk_num = 1, x_num_chunks do 
-    print("XX: ", chunk_num, x_num_chunks)
-    local a, b, c = x:chunk(chunk_num-1)
-    if ( a < chunk_size ) then 
-      print("Breaking on chunk", chunk_num); 
-      assert(x:is_eov() == true)
-      break 
-    end
-    num_chunks = num_chunks + 1
-    assert(a == chunk_size)
-    x:check()
-    print("XX: ", chunk_num, x_num_chunks)
-  end
-  local status = pcall(x.eov)
-  assert(not status)
-  local T = x:meta()
-  assert(plpath.getsize(T.base.file_name) == (num_chunks * chunk_size * 4))
-  print("Successfully completed test t7")
-end
---===========================================
-
---====== Testing nascent vector with generator and Vector's buffer
-tests.t8 = function()
-  print("Creating nascent vector with generator using Vector buffer")
-  local gen2 = require 'Q/RUNTIME/test/gen2'
-  x = lVector( { qtype = "I4", gen = gen2, has_nulls = false})
-
-  local num_chunks = 2
-  local chunk_size = qconsts.chunk_size
-  print("===================")
-  for chunk_num = 1, num_chunks do 
-    local a, b, c = x:chunk(chunk_num-1)
-    x:check()
-  end
-  assert(x:is_eov() == true)
-  local status = pcall(x.eov)
-  assert(not status)
-  local len, base_data, nn_data = x:chunk(0)
-  assert(base_data)
-  assert(not nn_data)
-  assert(len == chunk_size )
-
-  local iptr = ffi.cast("int32_t *", base_data)
-  for i = 1, len do
-    -- TODO P1 assert(iptr[i-1] == i)
-  end
-  print("Successfully completed test t8")
-end
-
 --====== Testing materialized vector for SC
-tests.t9 = function()
+tests.t3 = function()
   print("Testing materialized vector for SC")
   os.execute(" cp SC1.bin _SC1.bin " )
   x = lVector( { qtype = "SC", width = 8, file_name = '_SC1.bin' } )
@@ -263,10 +123,10 @@ tests.t9 = function()
   assert(not T.nn) 
   assert(num_aux == 0) -- TODO WHY DO WE HAVE AUX DATA HERE?
   --===========================================
-  print("Successfully completed test t9")
+  print("Successfully completed test t3")
 end
 
-tests.t10 = function()
+tests.t4 = function()
   -- deleting previous .csv file if present
   --os.execute("rm -f in2_I4.csv")
   -- generating .csv files required for generating bin file
@@ -287,12 +147,13 @@ tests.t10 = function()
   pr_meta(x, "_meta_data.csv")
   compare("_meta_data.csv", "in2_meta_data.csv")
 
-  print("Successfully completed test t10")
+  print("Successfully completed test t4")
   plfile.delete(path_to_here .. "/in3_I4.csv")
   plfile.delete(path_to_here .. "/_in3_I4.bin")
 end
+
 --==============================================
-tests.t11 = function()
+tests.t5 = function()
   -- deleting previous .csv file if present
   --os.execute("rm -f in2_I4.csv")
   -- generating .csv files required for generating bin file
@@ -307,7 +168,7 @@ tests.t11 = function()
   x:set_meta("rand scalar key", s)
   v = x:get_meta("rand scalar key")
   assert(v == s)
-  print("Successfully completed test t11")
+  print("Successfully completed test t5")
   plfile.delete(path_to_here .. "/in4_I4.csv")
   plfile.delete(path_to_here .. "/_in4_I4.bin")
 end
