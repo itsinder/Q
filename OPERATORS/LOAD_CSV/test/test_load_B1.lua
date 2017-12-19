bab0e6945da7fe1588db3a839705bb8a9ff87937
PragatiPailwan <pragati.pailwan@gmail.com>
2017-12-19 11:10:32 +0530

    Modified load_csv testcase for B1 qtype and generating required csv using generate_csv function, so deleleting csv.

--- OPERATORS/LOAD_CSV/test/test_load_B1.lua
+++ OPERATORS/LOAD_CSV/test/test_load_B1.lua
@@ -1,15 +1,11 @@
 -- FUNCTIONAL
 local Q = require 'Q'
 require 'Q/UTILS/lua/strict'
-
 local log = require 'Q/UTILS/lua/log'
-local plpath = require 'pl.path'
 local plfile = require 'pl.file'
-local dir = require 'pl.dir'
-local fns = require 'Q/UTILS/lua/utils'
 local load_csv = require 'Q/OPERATORS/LOAD_CSV/lua/load_csv'
 local diff = require 'Q/UTILS/lua/diff'
---local gen = require 'Q/RUNTIME/test/generate_csv'
+local gen = require 'Q/RUNTIME/test/generate_csv'
 
 local Q_SRC_ROOT = os.getenv("Q_SRC_ROOT")
 local script_dir = Q_SRC_ROOT .. "/OPERATORS/LOAD_CSV/test"
@@ -20,7 +16,7 @@ tests.t1 = function ()
   local metadata_file_path = script_dir .."/meta_B1.lua"
   -- no of rows in csv file are 65536 (i.e. equal to chunk_size)
   -- which are of pattern 010101..'s
-  --gen.generate_csv("input_file_B1.csv", "B1", 70)
+  gen.generate_csv("input_file_B1.csv", "B1", 65540)
   local csv_file_path = script_dir .."/input_file_B1.csv"
    
   local M = dofile(metadata_file_path)
@@ -28,15 +24,16 @@ tests.t1 = function ()
   -- call load function to load the data
   local status, ret = pcall(load_csv, csv_file_path, M )
   assert( status == true, "Error: " .. tostring(ret) .. "   : Loading Aborted ")
-  -- printing the values on terminal
-  -- after each 32 bits (elements) it sets bits to 1's
-  Q.print_csv(ret[1], nil, "output_file_B1.csv")
-  print("Num of elements = "..ret[1]:num_elements())
+  assert(ret[1]:num_elements()==65540, "Incorrect number of elements in vector")
   
+  Q.print_csv(ret[1], nil, "output_file_B1.csv")
+
   local diff_status = diff(script_dir .. "/input_file_B1.csv", script_dir .. "/output_file_B1.csv")
   assert(diff_status, "Input and Output csv file not matched")
-  
   log.info("All is well")
+  
+  plfile.delete(script_dir .. "/input_file_B1.csv")
+  plfile.delete(script_dir .. "/output_file_B1.csv") 
 end
 
 return tests
