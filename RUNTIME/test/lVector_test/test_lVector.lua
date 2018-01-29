4a870bd5215c2135a30cda9e99af6d575f20b80f
krushna <krushnakant.mardikar@gslab.com>
2018-01-29 02:51:34 -0800

    Some cosmetic changes in lVector testcases

--- RUNTIME/test/lVector_test/test_lVector.lua
+++ RUNTIME/test/lVector_test/test_lVector.lua
@@ -1,11 +1,8 @@
-local plpath  = require 'pl.path'
 local dir = require 'pl.dir'
-local qconsts = require 'Q/UTILS/lua/q_consts'
 local fns =  require 'Q/RUNTIME/test/lVector_test/assert_valid'
 local genbin = require 'Q/RUNTIME/test/generate_bin'
-local l_Vector = require 'Q/RUNTIME/test/lVector_test/l_Vector'
+local create_vector = require 'Q/RUNTIME/test/lVector_test/create_vector'
 local utils = require 'Q/UTILS/lua/utils'
-local plfile  = require 'pl.file'
 
 local Q_SRC_ROOT = os.getenv("Q_SRC_ROOT")
 local script_dir = Q_SRC_ROOT .. "/RUNTIME/test/lVector_test"
@@ -14,32 +11,32 @@ dir.makepath(script_dir .."/bin/")
 local all_qtype = { 'I1', 'I2', 'I4', 'I8', 'F4', 'F8', 'SC', 'SV' }
 
 -- for calling respective validating functions for each testcase
-local assert_valid = function(res, map_index, qtype)
+local assert_valid = function(res, map_value, qtype)
   -- calling the assert function based on type of vector
-  local function_name = "assert_" .. map_index.assert_fns
-  local status, fail_msg = pcall(fns[function_name], res, map_index.name .. qtype, map_index.num_elements, map_index.gen_method)
+  local function_name = "assert_" .. map_value.assert_fns
+  local status, fail_msg = pcall(fns[function_name], res, map_value.name .. qtype, map_value.num_elements, map_value.gen_method)
   if not status then 
     return status, fail_msg
   end
-  if map_index.test_category == "error_testcase_1" then 
+  if map_value.test_category == "error_testcase_1" then 
     print("STOP : Deliberate error attempt")
   end
   return status
 end
 
 
-local gather_test_meta_data = function(map_index, qtype)
-  assert(map_index.meta, "Provide metadata filename required for vector in map_file ")
-  local M = dofile(script_dir .."/meta_data/"..map_index.meta)
-  
-  if map_index.test_type == "materialized_vector" then
+local gather_test_meta_data = function(map_value, qtype)
+  assert(map_value.num_elements, "Provide number of elements in map_file")
+  assert(map_value.meta, "Provide metadata filename required for vector in map_file ")  
+  local M = dofile(script_dir .."/meta_data/"..map_value.meta)
+  if map_value.test_type == "materialized_vector" then
     local bin_file_name
     -- if "${q_type}" = positive testcase: they want input .bin file to be generated
     -- else negative testcase: dont want input .bin file to be generated
     if string.match( M.file_name,"${q_type}" ) then
       bin_file_name = script_dir.."/bin/in_" .. qtype .. ".bin"
       -- generating .bin files required for materialized vector
-      genbin.generate_bin(map_index.num_elements, qtype, bin_file_name, "random" )
+      genbin.generate_bin(map_value.num_elements, qtype, bin_file_name, "random" )
       -- for .bin file
       M.file_name = bin_file_name
       -- for .nn bin file
@@ -48,57 +45,52 @@ local gather_test_meta_data = function(map_index, qtype)
       end
     end
   end
-    
   M.qtype = qtype
-  assert(map_index.num_elements, "Provide number of elements in map_file")
-  M.num_elements = map_index.num_elements
-return M
+  M.num_elements = map_value.num_elements
+  return M
 end
 
-local test_lVector = {}
-
+local lVector_tests = {}
 local T = dofile(script_dir .."/map_lVector.lua")
-local testcase_no = 1
 
-for i, map_index in ipairs(T) do
+for i, map_value in ipairs(T) do
   local qtype
-  if map_index.qtype then qtype = map_index.qtype else qtype = all_qtype end
+  if map_value.qtype then qtype = map_value.qtype else qtype = all_qtype end
   for j in pairs(qtype) do
-    -- creating individual test-case as an entry in test_lVector 
-    test_lVector[testcase_no] = function()
-      map_index.name = map_index.name .. "_" .. qtype[j]
-      local M = gather_test_meta_data(map_index, qtype[j])
+    -- creating individual test-case as an entry in lVector_tests 
+    lVector_tests[#lVector_tests + 1] = function()
+      map_value.name = map_value.name .. "_" .. qtype[j]
+      local M = gather_test_meta_data(map_value, qtype[j])
      
       -- print messages for negative testcases
-      if map_index.test_category == "error_testcase_1" or map_index.test_category == "error_testcase_2" then 
+      if map_value.test_category == "error_testcase_1" or map_value.test_category == "error_testcase_2" then 
         print("START: Deliberate error attempt")
       end
       
-      local status, res = pcall(l_Vector, M)
-      if map_index.test_category == "error_testcase_2" then
+      local status, res = pcall(create_vector, M)
+      if map_value.test_category == "error_testcase_2" then
         print("STOP : Deliberate error attempt")
       end
       local result, reason
       
       if status then
-        result, reason = assert_valid(res, map_index, qtype[j])
+        result, reason = assert_valid(res, map_value, qtype[j])
         -- preamble
-        utils["testcase_results"](map_index, "lVector", "Unit Test", result, "")
+        utils["testcase_results"](map_value, "lVector", "Unit Test", result, "")
         if reason ~= nil then
-          assert(result,"test name:" .. map_index.name .. ":: Reason: " .. reason)
+          assert(result,"test name:" .. map_value.name .. ":: Reason: " .. reason)
         end
-        assert(result,"test name:" .. map_index.name)
+        assert(result,"test name:" .. map_value.name)
       else      
         -- preamble
         utils["testcase_results"](v, "lVector", "Unit Test", status, "")
         if res ~= nil then
-          assert(status,"test name:" .. map_index.name .. ":: Reason: " .. res)
+          assert(status,"test name:" .. map_value.name .. ":: Reason: " .. res)
         end
-        assert(status,"test name:" .. map_index.name)
+        assert(status,"test name:" .. map_value.name)
       end
-    end 
-    testcase_no = testcase_no + 1
+    end
   end
 end
 
-return test_lVector
+return lVector_tests
