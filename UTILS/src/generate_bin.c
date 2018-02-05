31102823ae581effd503cd4ac6381adb1addf042
krushna <krushnakant.mardikar@gslab.com>
2018-02-05 03:20:19 -0800

    Updated generate bin code, also modified make file to consider generate bin utility

--- UTILS/src/generate_bin.c
+++ UTILS/src/generate_bin.c
@@ -2,48 +2,75 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdlib.h>
+//START_INCLUDES
+#include <limits.h>
+#include <float.h>
+//STOP_INCLUDES
 #include "q_macros.h"
 
-typedef enum _qtype_type { undef_qtype, B1, I1, I2, I4, I8, F4, F8, SC, TM } qtype_type;
+typedef enum _qtype_type { undef_qtype, B1, I1, I2, I4, I8, F4, F8 } qtype_type;
 
-int 
+/*
+
+Convention:-
+
+If gen_type = "linear" then generated_values = 10, 20, 30 ... num_values * 10
+
+if gen_type = "seq" then generated_values = ( i * 10 ) % max_value_of_qtype
+
+if gen_type = "const" then generated_values = const value
+
+*/
+
+//START_FUNC_DECL
+int
 generate_bin(
-    int num_values, 
-    char* fldtype, 
-    char* outfile, 
-    char* gen_type
-    ) 
+    int num_values,
+    const char* fldtype,
+    const char* outfile,
+    const char* gen_type
+    )
+//STOP_FUNC_DECL
 {
     int status = 0;
     FILE *ofp = NULL;
-    
+
     if ( *outfile == '\0' ) { go_BYE(-1); }
     if ( *fldtype == '\0' ) { go_BYE(-1); }
     if ( num_values < 1 ) { go_BYE(-1); }
-    
+    if ( ( strcmp( gen_type, "linear" ) == 0 ) || 
+    ( strcmp( gen_type, "linear" ) == 0 ) || 
+    ( strcmp( gen_type, "linear" ) == 0 )) {
+        // Everything is ok
+    }
+    else {
+        printf("Please provide proper gen_type, available values : { 'seq', 'linear', 'const' }\n");
+        go_BYE(-1);
+    }
+
     qtype_type qtype = undef_qtype;
     ofp = fopen(outfile, "wb");
     return_if_fopen_failed(ofp, outfile, "wb");
-    
-    if ( strcmp(fldtype, "B1") == 0 ) {
+
+    if ( strcmp( fldtype, "B1" ) == 0 ) {
         qtype = B1;
     }
-    else if ( strcmp(fldtype, "I1") == 0 ) {
-        qtype = I1;    
+    else if ( strcmp( fldtype, "I1" ) == 0 ) {
+        qtype = I1;
     }
-    else if ( strcmp(fldtype, "I2") == 0 ) {
+    else if ( strcmp( fldtype, "I2" ) == 0 ) {
         qtype = I2;
     }
-    else if ( strcmp(fldtype, "I4") == 0 ) {
+    else if ( strcmp( fldtype, "I4" ) == 0 ) {
         qtype = I4;
     }
-    else if ( strcmp(fldtype, "I8") == 0 ) {
+    else if ( strcmp( fldtype, "I8" ) == 0 ) {
         qtype = I8;
     }
-    else if ( strcmp(fldtype, "F4") == 0 ) {
+    else if ( strcmp( fldtype, "F4" ) == 0 ) {
         qtype = F4;
     }
-    else if ( strcmp(fldtype, "F8") == 0 ) {
+    else if ( strcmp( fldtype, "F8" ) == 0 ) {
         qtype = F8;
     }
     else {
@@ -57,33 +84,50 @@ generate_bin(
     float tempF4;
     double tempF8;
     for ( uint64_t i = 1; i <= num_values; i++ ) {
-        uint64_t val = i;
-        // Calculate value of 'val' depending on 'gen_type'
+        uint64_t val = i * 10;
         switch ( qtype ) {
             case B1 :
                 // Not Implemented
                 break;
             case I1 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    val %= CHAR_MAX;
+                }
                 tempI1 = (int8_t) val;
                 fwrite(&tempI1, 1, sizeof(int8_t), ofp);
                 break;
             case I2 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    val %= SHRT_MAX;
+                }
                 tempI2 = (int16_t) val;
                 fwrite(&tempI2, 1, sizeof(int16_t), ofp);
                 break;
             case I4 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    val %= INT_MAX;
+                }
                 tempI4 = (int32_t) val;
                 fwrite(&tempI4, 1, sizeof(int32_t), ofp);
                 break;
             case I8 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    val %= LONG_MAX;
+                }
                 tempI8 = (int64_t) val;
                 fwrite(&tempI8, 1, sizeof(int64_t), ofp);
                 break;
             case F4 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    //val %= FLT_MAX;
+                }
                 tempF4 = (float) val;
                 fwrite(&tempF4, 1, sizeof(float), ofp);
                 break;
             case F8 :
+                if ( strcmp( gen_type, "seq" ) == 0 ) {
+                    //val %= DBL_MAX;
+                }
                 tempF8 = (double) val;
                 fwrite(&tempF8, 1, sizeof(double), ofp);
                 break;
@@ -105,11 +149,15 @@ int main(
     )
 {
     int status = 0;
+    if ( argc < 4 ) {
+        printf("Please provide proper argument\nUsage: <executable> num_values fldtype outfile gen_type");
+        cBYE(-1);
+    }
     int num_values = atoi(argv[1]);
     char *fldtype = argv[2];
     char *outfile = argv[3];
     char *gen_type = argv[4];
-   
+
     status = generate_bin(num_values, fldtype, outfile, gen_type); cBYE(status);
     printf("Generate bin status is %d\n", status);
 BYE:
