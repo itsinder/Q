4ed79760c7ae81443e508e1d59aa9f6ed669365f
Indrajeet Singh <isingh@nerdwallet.com>
2018-02-12 15:30:17 -0800

    adding changes to ffi and vagrantfile

--- UTILS/lua/q_ffi.lua
+++ UTILS/lua/q_ffi.lua
@@ -19,37 +19,49 @@ FILE *fopen(const char *path, const char *mode);
 int fclose(FILE *stream);
 int fwrite(void *Buffer,int Size,int Count,FILE *ptr);
 int fflush(FILE *stream);
-]])
+   ]])
 
--- TODO: Put this back later ffi.new = nil 
-
-ffi.malloc = function(n, free_func)
+-- TODO: Put this back later ffi.new = nil
+local t_ffi = {}
+t_ffi.malloc = function(n, free_func)
    assert(n > 0, "Cannot malloc 0 or less bytes")
    local c_mem = nil
    local old = false -- TODO P0 Fix this the right way
-   if old then 
-     if free_func == nil then
-        c_mem = assert(ffi.gc(ffi.C.malloc(n), ffi.C.free))
-     else -- TODO Review with Indrajeet
-        c_mem = assert(ffi.gc(ffi.C.malloc(n), free_func))
-     end
-   else 
-     c_mem = cmem.new(n)
-    end
+   if old then
+      if free_func == nil then
+         c_mem = assert(ffi.gc(ffi.C.malloc(n), ffi.C.free))
+      else -- TODO Review with Indrajeet
+         c_mem = assert(ffi.gc(ffi.C.malloc(n), free_func))
+      end
+   else
+      c_mem = cmem.new(n)
+   end
    return c_mem
 end
 
-ffi.memset = function(buffer, value, size)
+t_ffi.memset = function(buffer, value, size)
    assert( buffer ~= nil, "Buffer cannot be nil")
    assert(size > 0, "Cannot memset 0 or less bytes")
    assert(ffi.C.memset(buffer, value, size), "ffi memset failed")
 end
 
-ffi.memcpy = function(dest, src, size)
+t_ffi.memcpy = function(dest, src, size)
    assert( dest ~= nil, " destination buffer cannot be nil")
    assert( src ~= nil, " source buffer cannot be nil")
    assert(size > 0, "Cannot memset 0 or less bytes")
    assert(ffi.C.memcpy(dest, src, size), "ffi memcpy failed")
 end
-
-return ffi
+local ffi_mt = {
+   __newindex = function(self, key, value)
+      error("Cannot set new value in ffi")
+   end,
+   __index = function(self, key)
+      if t_ffi[key] ~= nil then
+         return t_ffi[key]
+      end
+      return ffi[key]
+   end
+}
+local t = {}
+setmetatable(t,ffi_mt)
+return t
--- Vagrantfile
+++ Vagrantfile
@@ -5,14 +5,14 @@
 # configures the configuration version (we support older styles for
 # backwards compatibility). Please don't change it unless you know what
 # you're doing.
-Vagrant.configure("2") do |config|
+Vagrant.configure('2') do |config|
   # The most common configuration options are documented and commented below.
   # For a complete reference, please see the online documentation at
   # https://docs.vagrantup.com.
 
   # Every Vagrant development environment requires a box. You can search for
   # boxes at https://atlas.hashicorp.com/search.
-  config.vm.box = "ubuntu/trusty64"
+  config.vm.box = 'ubuntu/trusty64'
 
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
@@ -68,8 +68,8 @@ Vagrant.configure("2") do |config|
   #   apt-get update
   #   apt-get install -y apache2
   # SHELL
-  config.vm.provision "shell", inline: <<-SHELL
-    chmod 1777 /tmp    
+  config.vm.provision 'shell', inline: <<-SHELL
+    chmod -R 1777 /tmp
     ln -s /vagrant /Q
     echo 'source /Q/setup.sh' >> /home/vagrant/.bashrc
     # Do stuff to customize your shell e.g.,
@@ -78,6 +78,7 @@ Vagrant.configure("2") do |config|
     # cp /Q/GUIDING_PRINCIPLES/bashrc /home/vagrant/.bashrc
     cd /Q/GUIDING_PRINCIPLES
     bash aio.sh
-  SHELL
+    chmod -R 1777 /tmp
+ SHELL
   # config.vm.provision :shell, path: "GUIDING_PRINCIPLES/aio.sh"
 end
