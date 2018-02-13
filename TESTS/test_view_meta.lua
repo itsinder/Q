91ad6db6c15cb1af6a76f84d081bf4c9157cc7ca
krushna <krushnakant.mardikar@gslab.com>
2018-02-13 08:02:02 -0800

    Updated the TESTS/test_view_meta.lua with sort operator's current implementation

--- TESTS/test_view_meta.lua
+++ TESTS/test_view_meta.lua
@@ -10,10 +10,10 @@ tests.t1 = function()
   c1 = Q.mk_col( {1,2,3,4,5,6,7,8}, "I4")
   c2 = Q.mk_col( {20,35,26,50,11,30,45,17}, "I4")
   z = Q.vvadd(c1, c2)
-  print("START Deliberate error")
+  -- Below case supported as sort performs eval if vector is not eval'ed
+  -- So commenting below assert
   local status = pcall(Q.sort, z, "asc")
-  print("STOP  Deliberate error")
-  assert(not status )
+  --assert(not status )
   z:eval()
   Q.sort(z, "asc")
   z:set_meta("max", Q.max(c2):eval())
