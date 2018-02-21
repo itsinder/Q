bc82efba4fce3039c408d807ed68ca2ea51b4254
rsubramonian <rsubramonian@nerdwallet.com>
2018-02-21 12:02:13 -0800

    ok

--- RUNTIME/test/test_lVector_nascent.lua
+++ RUNTIME/test/test_lVector_nascent.lua
@@ -9,6 +9,20 @@ local ffi     = require 'Q/UTILS/lua/q_ffi'
 local path_to_here = os.getenv("Q_SRC_ROOT") .. "/RUNTIME/test/"
 assert(plpath.isdir(path_to_here))
 require 'Q/UTILS/lua/strict'
+local ffi = require 'ffi'
+-- TODO How to prevent hard coding below?
+ffi.cdef([[
+extern char *strcpy(char *dest, const char *src);
+extern char *strncpy(char *dest, const char *src, size_t n);
+
+typedef struct _cmem_rec_type {
+  void *data;
+  int64_t size;
+  char field_type[4]; // MAX_LEN_FIELD_TYPE TODO Fix hard coding
+  char cell_name[16]; // 15 chaarcters + 1 for nullc, mainly for debugging
+} CMEM_REC_TYPE;
+]]
+)
 
 local v
 local ival
@@ -52,10 +66,11 @@ tests.t1 = function()
   x = lVector( { qtype = "I4", gen = true, has_nulls = false})
   local num_elements = 1024
   local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  local iptr = ffi.cast("int32_t *", base_data)
+  local base_data = cmem.new(num_elements * field_size, "I4", "base")
+  local b2 = ffi.cast("CMEM_REC_TYPE *", base_data)
+  local b3 = ffi.cast("int32_t *", b2[0].data)
   for i = 1, num_elements do
-    iptr[i-1] = i*10
+    b3[i-1] = i*10
   end
   x:put_chunk(base_data, nil, num_elements)
   assert(x:check())
