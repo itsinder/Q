0a2aed91561dc31b93f39876382d7578ef8e539c
krushna <krushnakant.mardikar@gslab.com>
2018-02-22 05:44:40 -0800

    Minor cosmetic changes in load_csv

--- OPERATORS/LOAD_CSV/lua/update_out_buf.lua
+++ OPERATORS/LOAD_CSV/lua/update_out_buf.lua
@@ -34,7 +34,9 @@ local function update_out_buf(
   elseif m.qtype == "SC" then
     local in_buf_len = ffi.C.strlen(in_buf)
     assert(in_buf_len <= m.width, err.STRING_TOO_LONG)
-    out_buf = ffi.cast(qconsts.qtypes.SC.ctype .. " *", out_buf) + num_in_out_buf * m.width
+    local ctype = assert(qconsts.qtypes[m.qtype]["ctype"])
+    out_buf = ffi.cast(ctype .. " *", out_buf)
+    out_buf = out_buf + num_in_out_buf * m.width
     ffi.copy(out_buf, in_buf, in_buf_len)
     --=================================================
   elseif is_base_qtype(m.qtype) then
