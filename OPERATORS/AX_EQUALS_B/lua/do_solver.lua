2458d810984d860208e7d3afb064595724451520
krushna <krushnakant.mardikar@gslab.com>
2018-03-06 02:37:58 -0800

    Updated AX_EQUALS_B and MM to use cmem.new instead of ffi.malloc

--- OPERATORS/AX_EQUALS_B/lua/do_solver.lua
+++ OPERATORS/AX_EQUALS_B/lua/do_solver.lua
@@ -3,6 +3,8 @@ local qc      = require 'Q/UTILS/lua/q_core'
 local qconsts = require 'Q/UTILS/lua/q_consts'
 local ffi     = require 'Q/UTILS/lua/q_ffi'
 local lVector = require 'Q/RUNTIME/lua/lVector'
+local cmem    = require 'libcmem'
+local get_ptr = require 'Q/UTILS/lua/get_ptr'
 
 return function(func_name, A, b)
   -- TODO change positive_solver to to general_linear_solver
@@ -35,26 +37,26 @@ return function(func_name, A, b)
   assert(nn_bptr == nil, "b should have no nil elements")
 
   assert(#A == n, "A should have same width as b")
-  local Aptr = assert(ffi.malloc(n * ffi.sizeof(b_ctype .. " *")))
-  local xptr = assert(ffi.malloc(n * ffi.sizeof(b_ctype)))
+  local Aptr = assert(get_ptr(cmem.new(n * ffi.sizeof(b_ctype .. " *"))))
+  local xptr = assert(cmem.new(n * ffi.sizeof(b_ctype)))
   Aptr = ffi.cast(b_ctype .. " **", Aptr)
   -- Creating separate pointer copy 'copy_xptr' because if we 
   -- modify 'xptr' as below
   -- xptr = ffi.cast('double *', xptr)
   -- then vec:put_chunk() operation fails saying 
   -- "NOT CMEM" type, Line 329 of File vector.c
-  local copy_xptr = ffi.cast(b_ctype .. " *", xptr)
+  local copy_xptr = ffi.cast(b_ctype .. " *", get_ptr(xptr))
   for i = 1, n do
     local Ai_len, Ai_chunk, nn_Ai_chunk = A[i]:get_all()
     assert(Ai_len == n, "A["..i.."] should have same height as b")
     assert(nn_Ai_chunk == nil, "A["..i.."] should have no nil elements")
-    Aptr[i-1] = Ai_chunk
+    Aptr[i-1] = get_ptr(Ai_chunk)
   end
 
   assert(qc[func_name], "Symbol not found " .. func_name)
-  local status = qc[func_name](Aptr, copy_xptr, bptr, n)
+  local status = qc[func_name](Aptr, copy_xptr, get_ptr(bptr), n)
   assert(status == 0, "solver failed")
-  assert(qc["full_positive_solver_check"](Aptr, copy_xptr, bptr, n, 0),
+  assert(qc["full_positive_solver_check"](Aptr, copy_xptr, get_ptr(bptr), n, 0),
          "solution returned by solver "..func_name.." is invalid")
 
   local x_col = lVector({qtype = b_qtype, gen = true, has_nulls=false})
