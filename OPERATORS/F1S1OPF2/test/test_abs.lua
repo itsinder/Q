c430aa0cfa4f39d47f083a26fb74b62446038b8c
PragatiPailwan <pragati.pailwan@gmail.com>
2018-03-07 18:46:45 +0530

    restoring the commented lines

--- OPERATORS/F1S1OPF2/test/test_abs.lua
+++ OPERATORS/F1S1OPF2/test/test_abs.lua
@@ -5,9 +5,9 @@ local tests = {}
 tests.t1 = function()
   local c1 = Q.rand({lb = -10, ub = 10, qtype = "I4", len = 10})
   local opt_args = { opfile = "" }
-  c1:eval(); Q.print_csv(c1, opt_args)
+  -- c1:eval(); Q.print_csv(c1, opt_args)
   local c1 = Q.abs(c1)
-  c1:eval() Q.print_csv(c1, opt_args)
+  -- c1:eval() Q.print_csv(c1, opt_args)
   
   local num_lt_0 = Q.sum(Q.vslt(c1, 0)):eval():to_num()
   assert(num_lt_0 == 0, "FAILURE")
