0269e5a25bd872242a5cbd593e1f8f512725f949
PragatiPailwan <pragati.pailwan@gmail.com>
2018-03-14 17:50:42 +0530

    load_csv currently does not support B1 qtype at lua side, commenting the respective testcase from load_csv testsuite.

--- OPERATORS/LOAD_CSV/test/performance_testing/map_meta_info_data.lua
+++ OPERATORS/LOAD_CSV/test/performance_testing/map_meta_info_data.lua
@@ -1,7 +1,7 @@
 -- mapping of meta_info and data file
 return { 
-    { testcase_no = 1, meta_info= "meta_info_1mr_256c.lua", data= "_csv_1mr_256c.csv", row_count = 1000, chunk_print_size = 1000,
-      name="1mr_256c" },
+    { testcase_no = 1, meta_info= "meta_info_1mr_256c.lua", data= "_csv_1mr_256c.csv", row_count = 100, chunk_print_size = 10,
+      name="1mr_256c", opt_args = { use_accelerator = false } },
     
 --    { testcase_no = 2, meta_info= "meta_info_1lr_259c_var.lua", data= "_csv_1lr_259c_var.csv", row_count = 100000, chunk_print_size = 1000,
 --      name="1lr_259c_var" },
