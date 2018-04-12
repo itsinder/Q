518ce46a42896a7d08acc07bc47e6010d109c12d
krushna <krushnakant.mardikar@gslab.com>
2018-04-12 02:36:22 -0700

    Converted to q_testrunner format

--- ML/LOGISTIC_REGRESSION/test/test_simple.lua
+++ ML/LOGISTIC_REGRESSION/test/test_simple.lua
@@ -1,3 +1,9 @@
-local test = require 'Q/ML/LOGISTIC_REGRESSION/test/test_logistic_regression'
 
-test('simple', os.getenv('Q_SRC_ROOT')..'/DATA_SETS/SIMPLE/simple.tar.gz', 2, 3)
+local tests = {}
+
+tests.t1 = function()
+  local test = require 'Q/ML/LOGISTIC_REGRESSION/test/test_logistic_regression'
+  test('simple', os.getenv('Q_SRC_ROOT')..'/DATA_SETS/SIMPLE/simple.tar.gz', 2, 3)
+end
+
+return tests
