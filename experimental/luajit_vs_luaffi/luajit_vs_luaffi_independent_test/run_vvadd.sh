2451d354f40388fe5a58958b7f24ece5d7e5a9cd
PragatiPailwan <pragati.pailwan@gmail.com>
2018-04-17 15:50:02 +0530

    Added a NOTE in luajit_vs_luaffi README. Minor modifications in test case .sh script related to its usage.

--- experimental/luajit_vs_luaffi/luajit_vs_luaffi_independent_test/run_vvadd.sh
+++ experimental/luajit_vs_luaffi/luajit_vs_luaffi_independent_test/run_vvadd.sh
@@ -6,6 +6,11 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:."
 cd $W_DIR
 
 gcc -O4 -fPIC -std=gnu99 -shared  vvadd_I4_I4_I4.c -o libvvadd_I4_I4_I4.so
+if [ $# -ne 1 ] 
+then
+ echo -e "Usage:\nbash run_vvadd.sh <luajit/lua>"
+ exit
+fi
 
 if [ $1 == "luajit" ]
 then
