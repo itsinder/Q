3bf7ded3ca473d33291ce691ccef0822686f5919
rsubramonian <rsubramonian@nerdwallet.com>
2018-04-24 19:59:18 -0700

    ok

--- ML/DT/doc/README.sh
+++ ML/DT/doc/README.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 touch .meta
-eval ` ../../doc/latex/tools/setenv`
-make -f ../../doc/latex/tools/docdir.mk dt.pdf
+eval ` ../../../DOC/latex/tools/setenv`
+make -f ../../../DOC/latex/tools/docdir.mk dt.pdf
