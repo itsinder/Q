e605242d914112327e01932f7bacc4fb38571164
rsubramonian <rsubramonian@nerdwallet.com>
2018-04-28 17:12:57 -0700

    ok

--- ML/KNN/lua/classify.lua
+++ ML/KNN/lua/classify.lua
@@ -5,27 +5,13 @@ local chk_params = require 'Q/ML/KNN/lua/chk_params'
 local function classify(
   T, -- table of m lvectors of length n
   g, -- lVector of length n
-<<<<<<< HEAD:ML/knn/lua/classify.lua
-  x, -- table of m Scalars 
-=======
   x, -- table of m Scalars
->>>>>>> dev:ML/KNN/lua/classify.lua
   exponent, -- Scalar
   alpha, -- table of m Scalars (scale for different attributes)
   cnts -- lVector of length m (optional)
   )
   local sone = Scalar.new(1, "F4")
   --==============================================
-<<<<<<< HEAD:ML/knn/lua/classify.lua
-  local nT, n, ng = chk_params(T, g, x, alpha)
-  dk = {}
-  for i = 1, nT do 
-    dk[i] = Q.vsmax(Q.abs(Q.vsmul(Q.pow(Q.vssub(T[i], x[i]), exponent), alpha[i])), sone)
-  end
-  local d = Q.const({ val = sone, qtype = "F4", len = n})
-  for i = 1, nx do 
-    d = Q.vvsum(dk[i], d):eval()
-=======
   local nT, n, ng = chk_params(T, g, x, exponent, alpha)
   dk = {}
   local i = 1
@@ -36,7 +22,6 @@ local function classify(
   local d = Q.const({ val = sone, qtype = "F4", len = n})
   for i = 1, nT do 
     d = Q.vvadd(dk[i], d):eval()
->>>>>>> dev:ML/KNN/lua/classify.lua
   end
   d = Q.reciprocal(d):eval()
   -- Now, we need to sum d grouped by value of goal attribute
