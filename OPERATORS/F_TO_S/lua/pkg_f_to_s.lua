0626062ca4e8b38a31513c21fdaac805cf29dfc3
krushna <krushnakant.mardikar@gslab.com>
2018-05-01 13:41:59 -0700

    Added avg (average) operator in F_TO_S, it can be used as Q.avg(<vec>)

--- OPERATORS/F_TO_S/lua/pkg_f_to_s.lua
+++ OPERATORS/F_TO_S/lua/pkg_f_to_s.lua
@@ -13,6 +13,20 @@ T.<<operator>> = <<operator>>
 require('Q/q_export').export('<<operator>>', <<operator>>)
     ]===]
 
+local avg_string = [[
+local function avg(x, y)
+  local Q = require 'Q'
+  local Scalar = require 'libsclr'
+  assert(type(x) == "lVector", "input must be of type lVector")
+  local sum, count = Q.sum(x):eval()
+  local avg = sum:to_num() / count:to_num()
+  local z = Scalar.new(avg, "F8")
+  return z
+end
+T.avg = avg
+require('Q/q_export').export('avg', avg)
+]]
+
 io.output("_f_to_s.lua")
 io.write("local T = {} \n")
 local ops = assert(require 'Q/OPERATORS/F_TO_S/lua/operators')
@@ -22,5 +36,6 @@ for i, op in ipairs(ops) do
 end
 local x = table.concat(T, "\n")
 io.write(x)
+io.write("\n" .. avg_string)
 io.write("\nreturn T\n")
 io.close()
