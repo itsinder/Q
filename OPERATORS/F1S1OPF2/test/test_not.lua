78193ec4a9277a398b1a7d1aece3c89681405ec8
krushna <krushnakant.mardikar@gslab.com>
2018-05-01 11:29:35 -0700

    Updated vnot template with small change, also added a test with odd number of bits i.e 66 for vnot

--- OPERATORS/F1S1OPF2/test/test_not.lua
+++ OPERATORS/F1S1OPF2/test/test_not.lua
@@ -27,4 +27,28 @@ tests.t1 = function()
   print("Completed test t1")
 end
 
+tests.t2 = function()
+  local len = 66
+  local in_table = {}
+  local exp_table = {}
+  for i = 1, len do
+    in_table[i] = 0
+    exp_table[i] = 1
+  end
+  local col = Q.mk_col(in_table, "B1")
+  local n_col = Q.vnot(col):eval()
+  local n_sum = Q.sum(n_col):eval()
+  
+  -- TODO: below assert fails, this is because of vec_add_B1 method, 
+  -- it copies extra bits from last byte if len is not multiple of 8, discuss with Ramesh
+  assert(n_sum:to_num() == len)
+
+  local val, nn_val
+  for i = 1, n_col:length() do
+    val, nn_val = n_col:get_one(i-1)
+    assert(val:to_num() == exp_table[i])
+  end
+  print("Completed test t2")
+end
+
 return tests
