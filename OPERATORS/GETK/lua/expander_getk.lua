d970fe4a32dc31b2ae5e54785bf24f2e8a08dc42
krushna <krushnakant.mardikar@gslab.com>
2018-05-07 03:39:04 -0700

    Modified the GETK expander to have a gen function, now expander returns the lVector with specifying the gen function in constructor

--- OPERATORS/GETK/lua/expander_getk.lua
+++ OPERATORS/GETK/lua/expander_getk.lua
@@ -26,56 +26,65 @@ local function expander_getk(a, fval, k, optargs)
   local sort_fn = subs.sort_fn 
 
   --=================================================
-  -- create a buffer to sort each chunk as you get it 
-  local n = qconsts.chunk_size
-  local sort_buf_val = cmem.new(n * width, qtype)
-  sort_buf_val:zero()
-  local casted_sort_buf_val = ffi.cast(ctype .. "*", get_ptr(sort_buf_val))
-
-  --=== create buffers for keeping topk from each chunk
-  local bufX = cmem.new(k * width, qtype)
-  bufX:zero()
-  local casted_bufX = ffi.cast(ctype .. "*", get_ptr(bufX))
-  local nX = 0
-
-  local bufZ = cmem.new(k * width, qtype)
-  bufZ:zero()
-  local casted_bufZ = ffi.cast(ctype .. "*", get_ptr(bufZ))
-  local nZ = k
-
-  local num_in_Z = cmem.new(4, "I4");
-  local ptr_num_in_Z = ffi.cast("uint32_t *",  get_ptr(num_in_Z))
-  --============================================
-  -- create vectors to return 
-  local val_vec = lVector{nn= false, gen = true, qtype = qtype, has_nulls = false}
-  -- TODO Consider case where there are less than k elements to return
-
   local chunk_idx = 0
-  while ( true ) do
-    local len, chunk, nn_chunk, casted_chunk, nY
-    len, chunk, nn_chunk = fval:chunk(chunk_idx)
-    if ( len == 0 ) then break end 
-    -- copy chunk into local buffer and sort it in right order
-    casted_chunk = ffi.cast(ctype .. "*",  get_ptr(chunk))
-    assert(qc[sort_fn], "function not found " .. sort_fn)
-    sort_buf_val:zero()
-    ffi.C.memcpy(casted_sort_buf_val, casted_chunk, len*width)
-    qc[sort_fn](casted_sort_buf_val, len)
-    --================================
-    if ( k < len ) then nY = k else nY = len end
-    if ( chunk_idx == 0 )  then 
-      ffi.C.memcpy(casted_bufX, casted_sort_buf_val, nY*width)
-      nX = nY
-    else
-      qc[subs.merge_fn](casted_bufX, nX, casted_sort_buf_val, nY, casted_bufZ, nZ, ptr_num_in_Z)
-      -- copy from bufZ to bufX
-      local num_in_Z = ptr_num_in_Z[0]
-      ffi.C.memcpy(casted_bufX, casted_bufZ, num_in_Z*width)
-      nX = num_in_Z
+  local first_call = true
+  local n = qconsts.chunk_size
+  local nX, nZ
+  local sort_buf_val, casted_sort_buf_val
+  local bufX, casted_bufX
+  local bufZ, casted_bufZ
+  local num_in_Z, ptr_num_in_Z
+  local len, chunk, nn_chunk, casted_chunk, nY
+  -- TODO Consider case where there are less than k elements to return
+  local function getk_gen(chunk_num)
+    -- Adding assert on chunk_idx to have sync between expected chunk_num and generator's chunk_idx state
+    assert(chunk_num == chunk_idx)
+    if ( first_call ) then
+      -- create a buffer to sort each chunk as you get it
+      sort_buf_val = cmem.new(n * width, qtype)
+      sort_buf_val:zero()
+      casted_sort_buf_val = ffi.cast(ctype .. "*", get_ptr(sort_buf_val))
+
+      -- create buffers for keeping topk from each chunk
+      bufX = cmem.new(k * width, qtype)
+      bufX:zero()
+      casted_bufX = ffi.cast(ctype .. "*", get_ptr(bufX))
+      nX = 0
+
+      bufZ = cmem.new(k * width, qtype)
+      bufZ:zero()
+      casted_bufZ = ffi.cast(ctype .. "*", get_ptr(bufZ))
+      nZ = k
+
+      num_in_Z = cmem.new(4, "I4");
+      ptr_num_in_Z = ffi.cast("uint32_t *",  get_ptr(num_in_Z))
     end
-    chunk_idx = chunk_idx + 1
+
+    while ( true ) do
+      len, chunk, nn_chunk = fval:chunk(chunk_idx)
+      if ( len == 0 ) then break end
+      -- copy chunk into local buffer and sort it in right order
+      casted_chunk = ffi.cast(ctype .. "*",  get_ptr(chunk))
+      assert(qc[sort_fn], "function not found " .. sort_fn)
+      sort_buf_val:zero()
+      ffi.C.memcpy(casted_sort_buf_val, casted_chunk, len*width)
+      qc[sort_fn](casted_sort_buf_val, len)
+      --================================
+      if ( k < len ) then nY = k else nY = len end
+      if ( chunk_idx == 0 )  then
+        ffi.C.memcpy(casted_bufX, casted_sort_buf_val, nY*width)
+        nX = nY
+      else
+        qc[subs.merge_fn](casted_bufX, nX, casted_sort_buf_val, nY, casted_bufZ, nZ, ptr_num_in_Z)
+        -- copy from bufZ to bufX
+        local num_in_Z = ptr_num_in_Z[0]
+        ffi.C.memcpy(casted_bufX, casted_bufZ, num_in_Z*width)
+        nX = num_in_Z
+      end
+      chunk_idx = chunk_idx + 1
+    end
+    return nX, bufX, nil
   end
-  val_vec:put_chunk(bufX, nil, nX)
-  return val_vec
+  return lVector( { gen = getk_gen, qtype = qtype, has_nulls = false } ) 
 end
 return expander_getk
