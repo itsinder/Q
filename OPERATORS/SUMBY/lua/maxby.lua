51b2de5e3d4484ac954fb6d9cbe5aee781e2bbc5
rsubramonian <rsubramonian@nerdwallet.com>
2018-05-08 18:33:39 -0700

    please review changes

--- OPERATORS/SUMBY/lua/maxby.lua
+++ OPERATORS/SUMBY/lua/maxby.lua
@@ -15,5 +15,21 @@ end
 T.maxby = maxby
 require('Q/q_export').export('maxby', maxby)
 
+-- TODO P4 consider auto-generating following 
+local function minby(x, g, ng, optargs)
+  local expander = require 'Q/OPERATORS/SUMBY/lua/expander_minby_minby'
+  assert(x, "no arg x to minby")
+  assert(g, "no arg y to minby")
+  assert(type(x) == "lVector",  "x is not lVector")
+  assert(type(g) == "lVector",  "g is not lVector")
+  assert(ng > 0)
+  assert(type(ng) == "number")
+  local status, col = pcall(expander, "minby", x, g, ng, optargs)
+  if not status then print(col) end
+  assert(status, "Could not execute MINBY")
+  return col
+end
+T.minby = minby
+require('Q/q_export').export('minby', minby)
 return T
 
