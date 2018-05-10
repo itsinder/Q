40721416b082100c5dd5324e4734295aacfe3e21
krushna <krushnakant.mardikar@gslab.com>
2018-05-10 01:31:04 -0700

    Updated the test_sumby.lua according to the is_safe convention (default value is true)

--- OPERATORS/SUMBY/test/test_sumby.lua
+++ OPERATORS/SUMBY/test/test_sumby.lua
@@ -23,12 +23,12 @@ tests.t1 = function()
 end
 
 tests.t2 = function()
-  -- sumby test in safe mode by setting is_safe to true
+  -- sumby test in safe mode ( default is safe mode )
   -- group by column exceeds limit
   local a = Q.mk_col({1, 2, 4, 5, 6, 7, 8, 9}, "I4")
   local b = Q.mk_col({0, 1, 4, 1, 1, 2, 0, 2}, "I2")
   local nb = 3
-  local res = Q.sumby(a, b, nb, {is_safe = true})
+  local res = Q.sumby(a, b, nb)
   local status = pcall(res.eval, res)
   assert(status == false)
   print("Test t2 completed")
@@ -37,8 +37,8 @@ end
 tests.t3 = function()
   -- Values of b, not having 0
   local a = Q.mk_col({1, 2, 4, 5, 6, 7, 8, 9}, "I4")
-  local b = Q.mk_col({1, 1, 3, 1, 1, 2, 1, 2}, "I2")
-  local exp_val = {0, 22, 16}
+  local b = Q.mk_col({1, 1, 2, 1, 1, 2, 1, 2}, "I2")
+  local exp_val = {0, 22, 20}
   local nb = 3
   local res = Q.sumby(a, b, nb)
   res:eval()
@@ -57,22 +57,22 @@ end
 
 
 tests.t4 = function()
-  -- Length of output vector more than chunk size
+  -- Length of input vector more than chunk size
   local len = qconsts.chunk_size * 2 + 655
   local a = Q.seq( {start = 1, by = 1, qtype = "I4", len = len} )
-  local b = Q.seq( {start = 0, by = 1, qtype = "I4", len = len} )
-  local nb = len
+  local b = Q.period({ len = len, start = 0, by = 1, period = 3, qtype = "I4"})
+  local exp_val = {2891978467, 2892022376, 2892066285}
+  local nb = 3
 
   local res = Q.sumby(a, b, nb)
   res:eval()
 
-  assert(res:length() == len)
+  assert(res:length() == nb)
   local val, nn_val
   for i = 1, res:length() do
     val, nn_val = res:get_one(i-1)
-    assert(val:to_num() == i, tostring(val:to_num()) .. " " .. i)
+    assert(val:to_num() == exp_val[i])
   end
-
   print("Test t4 completed")
 end
 
