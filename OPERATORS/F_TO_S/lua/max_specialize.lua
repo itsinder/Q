38414e1179b20ce30b4143c1f9d5e6ea0b98bce4
krushna <krushnakant.mardikar@gslab.com>
2018-05-16 03:46:20 -0700

    Updated the MAX specialize to have proper initial value for F4 and F8 qtype

--- OPERATORS/F_TO_S/lua/max_specialize.lua
+++ OPERATORS/F_TO_S/lua/max_specialize.lua
@@ -35,8 +35,11 @@ typedef struct _reduce_max_<<qtype>>_args {
       if ( qtype == "I2" ) then subs.initial_val = "INT16_MIN" end
       if ( qtype == "I4" ) then subs.initial_val = "INT32_MIN" end
       if ( qtype == "I8" ) then subs.initial_val = "INT64_MIN" end
-      if ( qtype == "F4" ) then subs.initial_val = "FLT_MIN" end
-      if ( qtype == "F8" ) then subs.initial_val = "DBL_MIN" end
+      -- Updated the initial val for F4 and F8
+      -- FLT_MIN and DBL_MIN have minimum, normalized, positive value of float and double
+      -- so for negative values in input vector, these are not appropriate initial values
+      if ( qtype == "F4" ) then subs.initial_val = "-FLT_MAX-1" end
+      if ( qtype == "F8" ) then subs.initial_val = "-DBL_MAX-1" end
       assert(subs.initial_val)
       subs.reducer = "mcr_max"
       subs.t_reducer = subs.reducer
