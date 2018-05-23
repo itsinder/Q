f2e7e24aefacc1044e35e5c6d6fe3cd7acc680d8
rsubramonian <rsubramonian@nerdwallet.com>
2018-05-22 17:45:20 -0700

    new data set for knn testing

--- ML/KNN/lua/alt_voting.lua
+++ ML/KNN/lua/alt_voting.lua
@@ -2,6 +2,17 @@ local Q = require 'Q'
 local Scalar = require 'libsclr'
 local chk_params = require 'Q/ML/KNN/lua/chk_params'
 
+--=======================================
+local function mk_ptrs(T, m)
+  local c_T = CMEM.new(m * ffi.sizeof("float *"))
+  i = 0
+  for k, v in pairs(T) do
+    local x_len, x_chunk, nn_x_chunk = x:get_all()
+    c_T[i] = x_chunk
+  end
+  return c_T
+end
+--=======================================
 local function chk_data(T)
   assert(type(T) == "table")
   local m = 0
@@ -20,7 +31,7 @@ local function chk_data(T)
   assert(m > 0)
   return m, n
 end
-
+--=======================================
 local function chk_params(
     T_train, 
     T_test, 
@@ -33,18 +44,23 @@ local function chk_params(
   local m_test,  n_test  = chk_data(T_test)
   return m_train, n_train, n_test
 end
-
+--=======================================
 local function voting(
   T_train, -- table of m lVectors of length n_train
   T_test, -- table of m lVectors of length n_test
   alpha, -- lVector of length m
   exponent,  -- Scalar
+  rslt_g, -- lVector of length n_test
   optargs
   )
-  m_train, n_train, n_test = chk_params(
+  m, n_train, n_test = chk_params(
     T_train, T_test, alpha, exponent)
   
-  c_T_train = CMEM.new()
-  -- calc_vote_per_g(....)
+  local x_len, c_alpha, nn_x_chunk = alpha:get_all()
+  local x_len, c_output, nn_x_chunk = alpha:get_all()
+  c_train = mk_ptrs(T_train, m)
+  c_test  = mk_ptrs(T_test, m)
+  qc["calc_vote_per_g"](
+    c_train, m, n_train, c_alpha, c_test, n_test, c_output)
   end
 return alt_voting
