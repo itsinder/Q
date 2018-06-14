83fa2851cb8ee5fda1295b032ea6a1fe40eacdf2
PragatiPailwan <pragati.pailwan@gmail.com>
2018-06-14 18:22:39 +0530

    modified the unique.c to now look like template file which vector will be using. Also added modified the lua and C test file accordingly.

--- OPERATORS/UNIQUE/src/run_unique.sh
+++
@@ -1,6 +0,0 @@
-gcc -O4 -std=c99 unique.c -o unique.o
-gcc -O4 -fPIC -std=gnu99 -shared  unique.c -o unique.so
-
-./unique.o
-
-rm unique.o
--- OPERATORS/UNIQUE/test/test_unique_I4.lua
+++ OPERATORS/UNIQUE/test/test_unique_I4.lua
@@ -10,37 +10,53 @@ local q_src_root = os.getenv("Q_SRC_ROOT")
 local so_dir_path = q_src_root .. "/OPERATORS/UNIQUE/src/"
 
 ffi.cdef([[
-int unique(int32_t *in_buf, int in_size, int32_t *out_buf, int out_size, int *num_in_out);  
+int
+unique_I4(
+      const int32_t * restrict A,
+      uint64_t nA,
+      uint64_t *ptr_aidx,
+      int32_t *C,
+      uint64_t nC,
+      uint64_t *ptr_num_in_C
+      );  
 ]])
 
 -- NOTE: Need to run Q/OPERATORS/UNIQUE/src/run_unique.sh to create unique.so file
-local qc = ffi.load(so_dir_path .. 'unique.so')
+local qc = ffi.load(so_dir_path .. 'unique_I4.so')
 
 -- lua test to check the working of UNIQUE operator only for I4 qtype
 local tests = {}
 tests.t1 = function ()
   local expected_result = {1, 2, 3, 4, 5}
   
-  local num_elements = 100
+  local num_elements = 10
   local qtype = "I4"
   
-  local input = Q.period( {start = 1, by = 1, len = num_elements, period = 5, qtype = qtype} ):eval()
+  local input = Q.period( {start = 1, by = 1, len = num_elements, period = 5, qtype = qtype} ):persist(true)
+  input:eval()
   -- unique operator assumes that the input vector is sorted
   local input_col = Q.sort(input, "asc")
 
-  local sz_out_in_bytes = num_elements * qconsts.qtypes[input:fldtype()].width
-  local out_buf = assert(cmem.new(sz_out_in_bytes))
+  local sz_out_in_bytes = num_elements * qconsts.qtypes[qtype].width
+  local out_buf = nil
+  local n_out = nil
+  local aidx  = nil
 
-  local n_out = assert(get_ptr(cmem.new(ffi.sizeof("int"))))
-  n_out = ffi.cast("int *", n_out)
+  out_buf = assert(cmem.new(sz_out_in_bytes))
+
+  n_out = assert(get_ptr(cmem.new(ffi.sizeof("uint64_t"))))
+  n_out = ffi.cast("uint64_t *", n_out)
+
+  aidx = assert(get_ptr(cmem.new(ffi.sizeof("uint64_t"))))
+  aidx = ffi.cast("uint64_t *", aidx)
+  aidx[0] = 0
       
   local a_len, a_chunk, a_nn_chunk = input:chunk(0)
   
   local casted_a_chunk = ffi.cast( "int32_t *",  get_ptr(a_chunk))
   local casted_out_buf = ffi.cast( "int32_t *",  get_ptr(out_buf))
-  local status = qc["unique"](casted_a_chunk, a_len, casted_out_buf, a_len, n_out)
-  
-  assert(status)
+  local status = qc["unique_I4"](casted_a_chunk, a_len, aidx, casted_out_buf, num_elements, n_out)
+  assert(status == 0, "C error in UNIQUE")
   assert(tonumber(n_out[0]) == #expected_result)
   -- Validate the result
   out_buf = ffi.cast("int32_t *", casted_out_buf)
