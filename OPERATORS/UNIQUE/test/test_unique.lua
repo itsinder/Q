68b8e2e0c80d520ab4a94fa683608cda0edc9d41
PragatiPailwan <pragati.pailwan@gmail.com>
2018-06-25 20:26:03 +0530

    added few more corner test cases for unique operator under Q/OPERATORS/UNIQUE/test directory.

--- OPERATORS/UNIQUE/test/test_unique.lua
+++ OPERATORS/UNIQUE/test/test_unique.lua
@@ -3,6 +3,12 @@ require 'Q/UTILS/lua/strict'
 local Q = require 'Q'
 local c_to_txt = require 'Q/UTILS/lua/C_to_txt'
 local qconsts = require 'Q/UTILS/lua/q_consts'
+local plpath  = require 'pl.path'
+local plfile  = require 'pl.file'
+local path_to_here = os.getenv("Q_SRC_ROOT") .. "/OPERATORS/UNIQUE/test/"
+assert(plpath.isdir(path_to_here))
+
+local chunk_size = qconsts.chunk_size
 
 -- validating unique operator to return unique values from input vector
 -- where num_elements are less than chunk_size
@@ -26,7 +32,7 @@ end
 -- where num_elements are greater than chunk_size 
 tests.t2 = function ()
   local out_table = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-  local input = Q.period({ len = 65536*4+2, start = 1, by = 1, period = 10, qtype = "I4"}):persist(true):eval()
+  local input = Q.period({ len = chunk_size*4+2, start = 1, by = 1, period = 10, qtype = "I4"}):persist(true):eval()
   
   local input_col = Q.sort(input, "asc")
   -- Q.print_csv(input_col, {opfile = "input_file.csv"})
@@ -37,6 +43,78 @@ tests.t2 = function ()
     assert(value == out_table[i])
   end
   print("Test t2 succeeded")
+end
+
+-- validating unique to return unique values from input vector
+-- where num_elements are greater than chunk_size
+-- [ 1 ... chunk_size ] [ chunk_size+1 ... chunk_size*2 ]
+-- [ 1, 1, .. 2, 2, 3 ] [ 3, 3, 3, 3, 3, 3, 3, 3, ... 3 ]
+tests.t3 = function ()
+  local expected_values = {1, 2, 3}
+  local chunk_size = qconsts.chunk_size
+  
+  local input_tbl = {}
+  for i = 1, chunk_size-1 do
+    if i % 2 == 0 then
+      input_tbl[i] = 1
+    else
+      input_tbl[i] = 2
+    end
+  end
+
+  for i = chunk_size+1, chunk_size*2 do
+    input_tbl[i] = 3
+  end
+  input_tbl[chunk_size] = 3
+  local input_col = Q.mk_col(input_tbl, "I1")
+  input_col = Q.sort(input_col, "asc"):eval()
+  Q.print_csv(input_col, {opfile = path_to_here .. "input_file_in.csv"})
+  local c = Q.unique(input_col):eval()
+  assert(c:length() == #expected_values)
+  for i = 1, c:length() do
+    local value = c_to_txt(c, i)
+    -- print(value, expected_values[i])
+    assert(value == expected_values[i])
+  end
+  Q.print_csv(c)
+  plfile.delete(path_to_here .. "/input_file_in.csv") 
+  print("Test t3 succeeded")
+end
+
+-- validating unique to return unique values from input vector
+-- where num_elements are greater than chunk_size 
+-- [ 1 ... chunk_size ] [ chunk_size+1 ... (chunk_size*2-chunk_size/2) ]
+-- [ 1, 1, .. 2, 2, 3 ] [ 3, 3 ... 3 (half the length of second chunk)]
+tests.t4 = function ()
+  local expected_values = {1, 2, 3}
+  local chunk_size = qconsts.chunk_size
+  
+  local input_tbl = {}
+  for i = 1, chunk_size-1 do
+    if i % 2 == 0 then
+      input_tbl[i] = 1
+    else
+      input_tbl[i] = 2
+    end
+  end
+
+  for i = chunk_size+1, (chunk_size*2)-(chunk_size/2) do
+    input_tbl[i] = 3
+  end
+  input_tbl[chunk_size] = 3
+  local input_col = Q.mk_col(input_tbl, "I1")
+  input_col = Q.sort(input_col, "asc"):eval()
+  Q.print_csv(input_col, {opfile = path_to_here .. "input_file_in.csv"})
+  local c = Q.unique(input_col):eval()
+  assert(c:length() == #expected_values)
+  for i = 1, c:length() do
+    local value = c_to_txt(c, i)
+    -- print(value, expected_values[i])
+    assert(value == expected_values[i])
+  end
+  Q.print_csv(c)
+  plfile.delete(path_to_here .. "/input_file_in.csv") 
+  print("Test t4 succeeded")
 end
  
 return tests
