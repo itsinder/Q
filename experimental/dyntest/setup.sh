cf21c1621c178c025c435e315611a11855238ed0
Indrajeet Singh <isingh@nerdwallet.com>
2018-07-16 10:45:20 -0700

    making script run all

--- experimental/dyntest/setup.sh
+++ experimental/dyntest/setup.sh
@@ -10,12 +10,45 @@ cd ./dyncall-0.9/
 make
 sudo make install
 cd ../
- tar -xvzf bindings.tar.gz
+tar -xvzf luaffi
+cd luaffi
+make
+cd ../
+tar -xvzf bindings.tar.gz
 cd ./bindings/lua/luadc/
 make
 cd testFuncs/
 gcc -shared -fPIC -o libtest.so test.c
+echo "*********Lua with dyncall*****"
 ../../../../lua-5.3.3/src/lua dctest.lua
 ../../../../lua-5.3.3/src/lua dctest2.lua
 ../../../../lua-5.3.3/src/lua dctest3.lua
 ../../../../lua-5.3.3/src/lua dctest4.lua
+
+cp ../../../../luaffi/ffi.so ./
+echo "*****lua + ffi *****"
+lua ffitest.lua
+lua ffitest2.lua
+lua ffitest3.lua
+lua ffitest4.lua
+
+
+cd ../../../
+# Note that the make here is modified to have fPIC in luajit a dyncall
+# requirement
+tar -xvzf LuaJIT-2.1.0-beta3.tar.gz
+cd LuaJIT-2.1.0-beta3
+make
+cd testFuncs
+echo "*********Luajit with dyncall*****"
+../../../../LuaJIT-2.1.0-beta3/src/luajit dctest.lua
+../../../../LuaJIT-2.1.0-beta3/src/luajit dctest2.lua
+../../../../LuaJIT-2.1.0-beta3/src/luajit dctest3.lua
+../../../../LuaJIT-2.1.0-beta3/src/luajit dctest4.lua
+
+echo "*****luajit *****"
+luajit ffitest.lua
+luajit ffitest2.lua
+luajit ffitest3.lua
+luajit ffitest4.lua
+
