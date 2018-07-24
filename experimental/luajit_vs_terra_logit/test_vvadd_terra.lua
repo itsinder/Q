ef565821424b12285fae5dccc4b6277cfd84c511
PragatiPailwan <pragati.pailwan@gmail.com>
2018-07-24 20:03:57 +0530

    adding a bash script to run test_logit_terra.lua test under Q/experimental/luajit_vs_terra_logit/ directory.

--- experimental/luajit_vs_terra_logit/test_vvadd_terra.lua
+++
@@ -1,9 +0,0 @@
---require 'terra'
-local qc = require 'Q/UTILS/lua/q_core'
-
-local Q = require 'Q'
-local col_1 = Q.const({ val = 1, qtype = "I1", len = 50000000} ):eval()
-local start_timer = qc.RDTSC()
-local res = Q.logit(col_1):eval()
-local stop_timer = qc.RDTSC()
-print("logit required time", stop_timer - start_timer)
