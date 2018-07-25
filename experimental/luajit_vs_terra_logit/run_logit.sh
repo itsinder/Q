ff5297750d8a66046b394ca6477f939b00a25504
krushna <krushnakant.mardikar@gslab.com>
2018-07-25 05:24:47 -0700

    Updated the terra test script with minor modification

--- experimental/luajit_vs_terra_logit/run_logit.sh
+++ experimental/luajit_vs_terra_logit/run_logit.sh
@@ -15,13 +15,13 @@ then
 fi
 
 use_terra=$2
-if [ \( $compiler == "luajit-2.1.0-beta3" \) -a \( $use_terra == "true" \) ]
-then
+#if [ \( $compiler == "luajit-2.1.0-beta3" \) -a \( $use_terra == "true" \) ]
+#then
   #as we are facing issue with luajit-2.1.0-beta3 compiler when introduced the require 'terra' statement
   # Issue is: luajit-2.1.0-beta3: strict.lua: cannot load incompatible bytecode
-  echo "Running test without terra: As luajit-2.1.0-beta3 version is facing issue with terra"
-  use_terra=false
-fi
+#  echo "Running test without terra: As luajit-2.1.0-beta3 version is facing issue with terra"
+#  use_terra=false
+#fi
 
 $compiler test_logit_terra.lua $use_terra
 
