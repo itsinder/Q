bb2ef85f9c20e257891329136df23339067816ca
PragatiPailwan <pragati.pailwan@gmail.com>
2018-07-26 19:26:06 +0530

    Adding logit code in C(without expander), lua(with ffi.cast) and lua(lua tables) under Q/experimental/logit_POC/ directory.

---
+++ experimental/logit_POC/C_code/run_logit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+W_DIR=`pwd`
+cd $SCRIPT_DIR/../../
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:."
+cd $W_DIR
+
+gcc -O4 -fPIC -std=gnu99 -shared  logit_I8.c -o liblogit_I8.so
+
+luajit $SCRIPT_DIR/test_logit_C.lua
+
