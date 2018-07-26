1c1156fb1bea992dd04008d78256b7e91f285e89
krushna <krushnakant.mardikar@gslab.com>
2018-07-26 05:46:06 -0700

    Updated the sum_prod.lua and it's test with few modifications

--- ML/LOGREG/lua/sum_prod.lua
+++ ML/LOGREG/lua/sum_prod.lua
@@ -11,11 +11,10 @@ function sum_prod(X, w)
     SA[i] = {}
     A[i] = {}
     temp[i] = Q.vvmul(X[i], w) -- :memo(false)
-    for j = i, M do
+    for j = 1, M do
       SA[i][j] = Q.sum(Q.vvmul(X[j], temp[i])) -- :memo(false))
     end
   end
-
   local len = w:length()
   local chunk_size = qconsts.chunk_size
 
@@ -24,14 +23,13 @@ function sum_prod(X, w)
   for c = 1, nC do
     for i = 1, M do
       temp[i]:chunk(c-1) -- get this chunk evaluated
-      for j = i, M do
+      for j = 1, M do
         SA[i][j]:next() -- get this chunk evaluated
       end
     end
   end
-
   for i = 1, M do
-    for j = i, M do
+    for j = 1, M do
       A[i][j] = SA[i][j]:value() -- get the value evaluated
     end
   end
