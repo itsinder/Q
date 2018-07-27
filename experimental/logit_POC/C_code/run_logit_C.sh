ca674bc70c66d45dbaa007990bb8f79734097974
PragatiPailwan <pragati.pailwan@gmail.com>
2018-07-27 15:03:26 +0530

    minor changes in logit it test scripts

--- experimental/logit_POC/C_code/run_logit_C.sh
+++ experimental/logit_POC/C_code/run_logit_C.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc -O4 -fopenmp $QC_FLAGS $Q_SRC_ROOT/experimental/logit_POC/C_code/test_logit.c \
+gcc -O4 -fopenmp $QC_FLAGS $Q_SRC_ROOT/experimental/logit_POC/C_code/test_logit_C.c \
   $Q_SRC_ROOT/experimental/logit_POC/C_code/logit_I8.c \
   $Q_SRC_ROOT/UTILS/src/get_time_usec.c \
   -I$Q_SRC_ROOT/experimental/logit_POC/C_code/ \
--- experimental/logit_POC/Lua/Lua_C/run_logit_lua_C.sh
+++ experimental/logit_POC/Lua/Lua_C/run_logit_lua_C.sh
@@ -7,5 +7,5 @@ cd $W_DIR
 
 gcc -O4 -fopenmp -fPIC -std=gnu99 -shared  logit_I8.c -o liblogit_I8.so -lgomp
 
-luajit $SCRIPT_DIR/test_logit_C.lua
+luajit $SCRIPT_DIR/test_logit_lua_C.lua
 
