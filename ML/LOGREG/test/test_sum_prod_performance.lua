7556bb1310073460de866b5614f227bcd7daff62
mardikark-gslab <krushnakant.mardikar@gmail.com>
2018-08-01 12:43:44 +0530

    Added the timers in Reducer.lua, updated the test_sum_prod_performance test with minor modification

--- ML/LOGREG/test/test_sum_prod_performance.lua
+++ ML/LOGREG/test/test_sum_prod_performance.lua
@@ -6,7 +6,7 @@ local test = {}
 test.t1 = function ()
   local sum_prod = require 'Q/ML/LOGREG/lua/sum_prod_eval'
   local N = 256 * 65536
-  local M = 32
+  local M = 16
   local X = {}
 
   for i = 1, M do
@@ -19,13 +19,25 @@ test.t1 = function ()
   local A = sum_prod(X, w)
   local stop_time = qc.RDTSC()
   print("sum_prod eval = ", stop_time-start_time)
-
+  --[[
   for i = 1, 3 do
     for j = 1, 3 do
       print(A[i][j])
     end
     print("================")
   end
+  ]]
+
+  print("==============================================")
+  if _G['g_time'] then
+    for k, v in pairs(_G['g_time']) do
+      local niters  = _G['g_ctr'][k] or "unknown"
+      local ncycles = tonumber(v)
+      print("0," .. k .. "," .. niters .. "," .. ncycles)
+    end
+  end
+
+
   print("SUCCESS")
   os.exit()
 end
@@ -33,7 +45,7 @@ end
 test.t2 = function ()
   local sum_prod = require 'Q/ML/LOGREG/lua/sum_prod_chunk'
   local N = 256 * 65536
-  local M = 32
+  local M = 16
   local X = {}
 
   for i = 1, M do
@@ -46,13 +58,24 @@ test.t2 = function ()
   local A = sum_prod(X, w)
   local stop_time = qc.RDTSC()
   print("sum_prod chunk = ", stop_time-start_time)
-
+  --[[
   for i = 1, 3 do
     for j = 1, 3 do
       print(A[i][j])
     end
     print("================")
   end
+  ]]
+
+  print("==============================================")
+  if _G['g_time'] then
+    for k, v in pairs(_G['g_time']) do
+      local niters  = _G['g_ctr'][k] or "unknown"
+      local ncycles = tonumber(v)
+      print("0," .. k .. "," .. niters .. "," .. ncycles)
+    end
+  end
+
   print("SUCCESS")
   os.exit()
 end
