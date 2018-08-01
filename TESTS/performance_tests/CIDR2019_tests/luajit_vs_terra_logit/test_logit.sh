86c12ff6b04ec95dbc5c737f2145858f1cb85204
PragatiPailwan <pragati.pailwan@gmail.com>
2018-08-01 12:16:17 +0530

    minor modification in test_logit.sh script to assume default cmd line values for q_testrunner run.

--- TESTS/performance_tests/CIDR2019_tests/luajit_vs_terra_logit/test_logit.sh
+++ TESTS/performance_tests/CIDR2019_tests/luajit_vs_terra_logit/test_logit.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 set -e
-if [ ${#} -ne 2 ]
+if [ ${#} -eq 2 ]
 then
+
+  compiler=$(echo $1 | tr '[:upper:]' '[:lower:]')
+
+  if [ ! \( \( $compiler == "luajit-2.1.0-beta3" \) -o \( $compiler == "luajit-2.0.4" \) \) ]
+  then
+    echo "First argument is incorrect, available options are : {luajit-2.1.0-beta3, luajit-2.0.4}"
+    exit 1
+  fi
+
+  use_terra=$2
+  #if [ \( $compiler == "luajit-2.1.0-beta3" \) -a \( $use_terra == "true" \) ]
+  #then
+    #as we are facing issue with luajit-2.1.0-beta3 compiler when introduced the require 'terra' statement
+    # Issue is: luajit-2.1.0-beta3: strict.lua: cannot load incompatible bytecode
+  #  echo "Running test without terra: As luajit-2.1.0-beta3 version is facing issue with terra"
+  #  use_terra=false
+  #fi
+
+  $compiler test_logit_terra.lua $use_terra
+
+else
   echo "Usage: bash run_performance_test.sh <luajit-2.1.0-beta3/luajit-2.0.4> <use_terra(true/false)>"
-  exit 1
+  # Note: q_testrunner currently does not support cmd line argument for .sh scripts
+  # Assuming default values for q_testrunner
+  echo "Running logit with default value assumed for cmd line arguments<luajit-2.1.0-beta3><false>"
+  luajit-2.1.0-beta3 $Q_SRC_ROOT/TESTS/performance_tests/CIDR2019_tests/luajit_vs_terra_logit/test_logit_terra.lua false
 fi
-
-compiler=$(echo $1 | tr '[:upper:]' '[:lower:]')
-
-if [ ! \( \( $compiler == "luajit-2.1.0-beta3" \) -o \( $compiler == "luajit-2.0.4" \) \) ]
-then
-  echo "First argument is incorrect, available options are : {luajit-2.1.0-beta3, luajit-2.0.4}"
-  exit 1
-fi
-
-use_terra=$2
-#if [ \( $compiler == "luajit-2.1.0-beta3" \) -a \( $use_terra == "true" \) ]
-#then
-  #as we are facing issue with luajit-2.1.0-beta3 compiler when introduced the require 'terra' statement
-  # Issue is: luajit-2.1.0-beta3: strict.lua: cannot load incompatible bytecode
-#  echo "Running test without terra: As luajit-2.1.0-beta3 version is facing issue with terra"
-#  use_terra=false
-#fi
-
-$compiler test_logit_terra.lua $use_terra
 
 echo "DONE"
-
-
