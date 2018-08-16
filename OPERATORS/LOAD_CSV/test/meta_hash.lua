f7c43592134876df255b3e34008c20db5b8c1070
PragatiPailwan <pragati.pailwan@gmail.com>
2018-08-16 13:51:44 +0530

    modifications in load_csv operator to return an extra column(hash value for SC) for each respective SC column. Also added a testcase for it.

--- OPERATORS/LOAD_CSV/test/meta_hash.lua
+++ OPERATORS/LOAD_CSV/test/meta_hash.lua
@@ -1,4 +1,5 @@
 local metadata = {
- { name = "empid", has_nulls = true, qtype = "SC", is_load = true, width = 10 }
+ { name = "empid", has_nulls = true, qtype = "SC", is_load = true, width = 10 },
+ { name = "empsal", has_nulls = true, qtype = "I2", is_load = true},
 }
 return metadata
