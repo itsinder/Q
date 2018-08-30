dedb9c667773d2d323d0702de44479a9126ccf00
krushna <krushnakant.mardikar@gslab.com>
2018-08-30 03:06:34 -0700

    Updated the test_vvadd from q_performing_testing directory

--- experimental/q_performance_testing/test_min_max_sum.lua
+++ experimental/q_performance_testing/test_min_max_sum.lua
@@ -4,39 +4,33 @@ local Scalar = require 'libsclr'
 local qconsts = require 'Q/UTILS/lua/q_consts'
 local qc = require 'Q/UTILS/lua/q_core'
 
-local file_name = "profile_result.txt"
+local len = qconsts.chunk_size*5
 
-local len = qconsts.chunk_size*2
+--[[
 local in_table = {}
 for i = 1, len do
   in_table[i] = i
 end
 
 local col1 = Q.mk_col(in_table, "I4", nil):set_name("col1")
-local col2 = Q.mk_col(in_table, "I4", nil):set_name("col2")
-local vec_meta = col1:meta()
-
+]]
+local col1 = Q.rand( { lb = 100, ub = len, qtype = "I4", len = len } )
 local start_time, stop_time, time
 
 Vector.reset_timers()
-local dbg = false
-start_time = qc.RDTSC()
+start_time = qc.get_time_usec()
 for i = 1, 10000 do
-  local x = Q.vvadd(col1, col2):memo(false):set_name("vvadd_out")
-  if ( dbg ) then 
-    local n1, n2 = Q.sum(x):eval()
-    assert(n1:to_num() == 2 * len * (len+1) / 2)
-  else
-    x:eval()
-  end
+  local x = Q.min(col1)
+  x:eval()
+  min, total, index = x:value()
+  -- print(min:to_num())
 end
-stop_time = qc.RDTSC()
+stop_time = qc.get_time_usec()
 Vector.print_timers()
 
-print("vvadd total execution time : " .. tostring(tonumber(stop_time-start_time)))
+print("min total execution time : " .. tostring(tonumber(stop_time-start_time)/1000000))
 
 print("=========================")
-
 if _G['g_time'] then
   for k, v in pairs(_G['g_time']) do
     local niters  = _G['g_ctr'][k] or "unknown"
@@ -45,3 +39,4 @@ if _G['g_time'] then
   end
 end
 
+os.exit()
