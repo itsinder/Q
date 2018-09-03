35c2dc003f84bfb75563e8b2d5b9336343f60395
Ramesh Subramonian <subramonian@gmail.com>
2018-09-03 13:25:30 -0700

    ok

--- GRAPH/CONN_COMP/src/run.sh
+++ GRAPH/CONN_COMP/src/run.sh
@@ -1,4 +1,9 @@
 gcc -g $QC_FLAGS proc_uk_graph.c \
   $HOME/WORK/Q/UTILS/src/mmap.c \
   -I$HOME/WORK/Q/UTILS/inc/ \
-  -I$HOME/WORK/Q/UTILS/gen_inc/
+  -I$HOME/WORK/Q/UTILS/gen_inc/ 
+
+gcc -O4 $QC_FLAGS x.c \
+  $HOME/WORK/Q/UTILS/src/mmap.c \
+  -I$HOME/WORK/Q/UTILS/inc/ \
+  -I$HOME/WORK/Q/UTILS/gen_inc/ 
