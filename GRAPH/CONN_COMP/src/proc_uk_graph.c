29971927909cba9ca71e184954271ce2bc4b38e8
rsubramonian <rsubramonian@nerdwallet.com>
2018-09-03 13:13:31 -0700

    some graph computations

--- GRAPH/CONN_COMP/src/proc_uk_graph.c
+++ GRAPH/CONN_COMP/src/proc_uk_graph.c
@@ -15,16 +15,16 @@ main(
   FILE *fp = NULL;
   FILE *lbfp = NULL;
   FILE *ubfp = NULL;
-  FILE *connfp = NULL;
+  FILE *tofp = NULL;
   char *buf = NULL;
   char line[MAXLINE+1];
   NODE_TYPE *lbl = NULL;
   NODE_TYPE *lb = NULL;
   NODE_TYPE *ub = NULL;
-  NODE_TYPE *conn = NULL;
+  NODE_TYPE *to = NULL;
   char *lb_X = NULL; size_t lb_nX = 0;
   char *ub_X = NULL; size_t ub_nX = 0;
-  char *conn_X = NULL; size_t conn_nX = 0;
+  char *to_X = NULL; size_t to_nX = 0;
 
   NODE_TYPE idx = 0;
   if ( argc != 4 ) { go_BYE(-1); }
@@ -41,9 +41,9 @@ main(
     ubfp = fopen(buf, "wb");
     return_if_fopen_failed(ubfp, buf, "wb");
 
-    sprintf(buf, "%s_conn.bin", prefix);
-    connfp = fopen(buf, "wb");
-    return_if_fopen_failed(connfp, buf, "wb");
+    sprintf(buf, "%s_to.bin", prefix);
+    tofp = fopen(buf, "wb");
+    return_if_fopen_failed(tofp, buf, "wb");
 
 
     fp = fopen(infile, "r");
@@ -62,7 +62,7 @@ main(
         xptr = strtok(NULL, ",");
         if ( xptr == NULL ) { break; }
         node_id = atoll(xptr);
-        fwrite(&node_id, sizeof(NODE_TYPE), 1, connfp);
+        fwrite(&node_id, sizeof(NODE_TYPE), 1, tofp);
         idx++;
       }
       fwrite(&idx, sizeof(NODE_TYPE), 1, ubfp); 
@@ -79,8 +79,8 @@ main(
   sprintf(buf, "%s_ub.bin", prefix);
   status = rs_mmap(buf, &ub_X, &ub_nX, 0); cBYE(status);
 
-  sprintf(buf, "%s_conn.bin", prefix);
-  status = rs_mmap(buf, &conn_X, &conn_nX, 0); cBYE(status);
+  sprintf(buf, "%s_to.bin", prefix);
+  status = rs_mmap(buf, &to_X, &to_nX, 0); cBYE(status);
 
   uint64_t n = lb_nX / sizeof(NODE_TYPE);  
   fprintf(stderr, "Working on  %ld nodes \n", n);
@@ -93,40 +93,39 @@ main(
 
   lb = (NODE_TYPE *)lb_X;
   ub = (NODE_TYPE *)ub_X;
-  conn = (NODE_TYPE *)conn_X;
+  to = (NODE_TYPE *)to_X;
 
-  int num_changed = -1; // just to get in the first tome
-  for ( int iter = 0; num_changed != 0; iter++ ) { 
-    num_changed = 0;
+  bool is_any_change = true; // just to get in the first tome
+  for ( int iter = 0; is_any_change == true; iter++ ) { 
+    is_any_change = false;
 #pragma omp parallel for schedule(static)
     for ( uint64_t i = 0; i < n; i++ ) {
-      int l_num_changed = 0;
+      bool l_is_any_change = false;
       if ( ub[i] <= lb[i] ) { continue; }
       NODE_TYPE minval = lbl[i];
       for ( uint64_t j = lb[i]; j < ub[i]; j++ ) {
-        minval = mcr_min(minval, lbl[conn[j]]);
+        minval = mcr_min(minval, lbl[to[j]]);
       }
       if ( lbl[i] != minval ) { 
-        l_num_changed++;
+        l_is_any_change = true;
         lbl[i] = minval;
       }
-#pragma omp critical (_some_uq_lbl)
-      {
-        num_changed += l_num_changed;
+      if ( ( l_is_any_change ) && ( is_any_change == false ) ) {
+        is_any_change = true;
       }
     }
-    fprintf(stderr, "Pass %d, num_changed = %d \n", iter, num_changed);
+    fprintf(stderr, "Pass %d \n", iter);
   }
 
 BYE:
   if ( lb_X != NULL ) { munmap(lb_X, lb_nX); }
   if ( ub_X != NULL ) { munmap(ub_X, lb_nX); }
-  if ( conn_X != NULL ) { munmap(conn_X, lb_nX); }
+  if ( to_X != NULL ) { munmap(to_X, lb_nX); }
   free_if_non_null(buf);
   fclose_if_non_null(fp);
   fclose_if_non_null(lbfp);
   fclose_if_non_null(ubfp);
-  fclose_if_non_null(connfp);
+  fclose_if_non_null(tofp);
   return_if_malloc_failed(lbl);
   return status;
 }
--- GRAPH/CONN_COMP/src/run.sh
+++ GRAPH/CONN_COMP/src/run.sh
@@ -2,3 +2,13 @@ gcc -g $QC_FLAGS proc_uk_graph.c \
   $HOME/WORK/Q/UTILS/src/mmap.c \
   -I$HOME/WORK/Q/UTILS/inc/ \
   -I$HOME/WORK/Q/UTILS/gen_inc/
+
+gcc -g $QC_FLAGS conn_comp.c \
+  $HOME/WORK/Q/UTILS/src/mmap.c \
+  -I$HOME/WORK/Q/UTILS/inc/ \
+  -I$HOME/WORK/Q/UTILS/gen_inc/ -o conn_comp
+
+datafile=$HOME/ukgraph-5
+test -f $datafile
+./a.out $datafile out y
+echo "Successfully completed $0 in $PWD"
