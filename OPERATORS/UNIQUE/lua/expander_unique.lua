ed8b627b3a8be4a784b35af590c1bcffc230a473
PragatiPailwan <pragati.pailwan@gmail.com>
2018-09-03 23:37:53 +0530

    moving pre-processing of sort_order for Q.unique to a separate file instead of having it in expander file.

--- OPERATORS/UNIQUE/lua/expander_unique.lua
+++ OPERATORS/UNIQUE/lua/expander_unique.lua
@@ -4,8 +4,7 @@ local qconsts = require 'Q/UTILS/lua/q_consts'
 local qc      = require 'Q/UTILS/lua/q_core'
 local cmem    = require 'libcmem'
 local get_ptr = require 'Q/UTILS/lua/get_ptr'
-local qtils = require 'Q/QTILS/lua/is_sorted'
-local sort = require 'Q/OPERATORS/SORT/lua/sort'
+local sort_utility = require 'Q/OPERATORS/UNIQUE/lua/unique_sort_utility'
 
 local function expander_unique(op, a, b)
   -- Verification
@@ -38,28 +37,8 @@ local function expander_unique(op, a, b)
   local last_unq_element = 0
   local brk_n_write
   
-  -- NOTE: For unique operator, input vector needs to be sorted(asc/dsc)  
-  local sort_order = a:get_meta("sort_order")
-  -- if sort_order field is nil then check the input vector for sort order
-  if ( sort_order == nil ) then
-    -- calling an utility called is_sorted(vec)
-    local status, order = qtils.is_sorted(a)
-    -- if input vector is not sorted, cloning and sorting that cloned vector
-    if status == false and order == nil then
-      local a_clone = a:clone()
-      sort(a_clone, "asc")
-      a = a_clone
-    else
-      assert( status == true, "is_sorted utility failed")
-      assert( order, "input vector not sorted")
-      a:set_meta( "sort_order", order)
-    end
-  end
-  -- getting updated sort_order meta value if sort_order was nil
-  sort_order = a:get_meta( "sort_order" )
-  assert( (sort_order == "asc") or ( sort_order == "dsc" ),
-      "input vector not sorted")
-
+  local a = sort_utility(a)
+  
   local unique_vec = lVector( { gen = true, has_nulls = false, qtype = a:qtype() } )
   local cnt_vec    = lVector( { gen = true, has_nulls = false, qtype = "I8" } )
   local sum_vec    = nil
