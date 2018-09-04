ca55cd5dc8e04ad8b6e368a237773a318859421a
krushna <krushnakant.mardikar@gslab.com>
2018-09-04 06:57:38 -0700

    Added a test for KNN which works on breast cancer data

--- ML/KNN/test/test_knn.lua
+++ ML/KNN/test/test_knn.lua
@@ -20,7 +20,7 @@ tests.t1 = function()
   local stop_time = qc.RDTSC()
   print("KNN = ", stop_time-start_time)
   
-  print(average_acr)
+  print("Accuracy = " .. tostring(average_acr))
   
   print("==============================================")
   if _G['g_time'] then
@@ -33,5 +33,34 @@ tests.t1 = function()
   print("==============================================")
 end
 
+tests.t2 = function()
+  local data_file = Q_SRC_ROOT .. "/ML/KNN/data/cancer/b_cancer/cancer_data.csv"
+  local metadata_file = Q_SRC_ROOT .. "/ML/KNN/data/cancer/b_cancer/cancer_meta.lua"
+
+  local args = {}
+  args.meta_data_file = metadata_file
+  args.data_file = data_file
+  args.goal = "diagnosis"
+
+  local start_time = qc.RDTSC()
+  -- Vector.reset_timers()
+  local average_acr, accuracy_table = run_knn(args)
+  -- Vector.print_timers()
+  local stop_time = qc.RDTSC()
+  print("KNN = ", stop_time-start_time)
+
+  print("Accuracy = " .. tostring(average_acr))
+  --[[
+  print("==============================================")
+  if _G['g_time'] then
+    for k, v in pairs(_G['g_time']) do
+      local niters  = _G['g_ctr'][k] or "unknown"
+      local ncycles = tonumber(v)
+      print("0," .. k .. "," .. niters .. "," .. ncycles)
+    end
+  end
+  print("==============================================")
+  ]]
+end
 
 return tests
