4aea8b6fe31fbdc7fbf1ee94a332eea8f9b7001a
mardikark-gslab <krushnakant.mardikar@gslab.com>
2018-09-07 12:41:39 +0530

    Minor updates in conn_comp_cuda.cu

--- GRAPH/CONN_COMP/src/conn_comp_cuda.cu
+++ GRAPH/CONN_COMP/src/conn_comp_cuda.cu
@@ -47,9 +47,6 @@ main(
     )
 {
   int status = 0;
-  FILE *lbfp = NULL;
-  FILE *ubfp = NULL;
-  FILE *tofp = NULL;
   NODE_TYPE *lbl = NULL;
   NODE_TYPE *lb = NULL;
   NODE_TYPE *ub = NULL;
@@ -61,67 +58,51 @@ main(
   if ( argc != 1 ) { status = -1; return status; }
 
   status = rs_mmap("lb.bin", &lb_X, &lb_nX, 0);
-  lb = (NODE_TYPE *)lb_X;
 
   status = rs_mmap("ub.bin", &ub_X, &ub_nX, 0);
-  ub = (NODE_TYPE *)ub_X;
 
   status = rs_mmap("to.bin", &to_X, &to_nX, 0);
-  to = (NODE_TYPE *)to_X;
 
   uint64_t n_nodes = lb_nX / sizeof(NODE_TYPE);
   fprintf(stderr, "Working on  %ld nodes \n", n_nodes);
 
-  // lbl = (NODE_TYPE *)malloc(n_nodes * sizeof(NODE_TYPE));
-  cudaMallocManaged(&lbl, n_nodes * sizeof(NODE_TYPE));
-  // return_if_malloc_failed(lbl);
-  if ( lbl == NULL ) { return -1; }
+  // Allocate memory for lb, ub, to & lbl using cudaMallocManaged
+  cudaMallocManaged(&lbl, lb_nX);
+  cudaMallocManaged(&lb, lb_nX);
+  cudaMallocManaged(&ub, ub_nX);
+  cudaMallocManaged(&to, to_nX);
+
+  if ( lbl == NULL ) { printf("Failed cuda malloc lbl\n"); return -1; }
+  if ( lb == NULL ) { printf("Failed cuda malloc lb\n"); return -1; }
+  if ( ub == NULL ) { printf("Failed cuda malloc ub\n"); return -1; }
+  if ( to == NULL ) { printf("Failed cuda malloc to\n"); return -1; }
+
+  printf("Memory allocation done\n");
+
+  // Initialize lbl, lb, ub, to
   for ( unsigned int i = 0; i < n_nodes; i++ ) {
     lbl[i] = i;
   }
+  memcpy(lb, lb_X, lb_nX);
+  memcpy(ub, ub_X, ub_nX);
+  memcpy(to, to_X, to_nX);
 
   uint64_t blockSize = 256;
   uint64_t numBlocks = (n_nodes + 256 - 1) / blockSize;
 
   bool is_any_change = true; // just to get in the first tome
   for ( int iter = 0; is_any_change == true; iter++ ) {
-    // any_change(lb, ub, to, lbl, n_nodes, &is_any_change);
+    printf("######################\n");
     any_change<<<numBlocks, blockSize>>>(lb, ub, to, lbl, n_nodes, &is_any_change);
     cudaDeviceSynchronize();
-
-/*
-#pragma omp parallel for schedule(static)
-    for ( uint64_t i = 0; i < n_nodes; i++ ) {
-      bool l_is_any_change = false;
-      if ( ub[i] <= lb[i] ) { continue; }
-      NODE_TYPE minval = lbl[i];
-      for ( int64_t j = lb[i]; j < ub[i]; j++ ) {
-        minval = mcr_min(minval, lbl[to[j]]);
-      }
-      if ( lbl[i] != minval ) {
-        l_is_any_change = true;
-        lbl[i] = minval;
-      }
-      if ( ( l_is_any_change ) && ( is_any_change == false ) ) {
-        is_any_change = true;
-      }
-    }
-*/
     fprintf(stderr, "Pass %d \n", iter);
   }
+
   cudaFree(lbl);
+  cudaFree(lb);
+  cudaFree(ub);
+  cudaFree(to);
   return status;
 
-//BYE:
-  /*
-  if ( lb_X != NULL ) { munmap(lb_X, lb_nX); }
-  if ( ub_X != NULL ) { munmap(ub_X, lb_nX); }
-  if ( to_X != NULL ) { munmap(to_X, lb_nX); }
-  fclose_if_non_null(lbfp);
-  fclose_if_non_null(ubfp);
-  fclose_if_non_null(tofp);
-  */
-  //cudaFree(lbl);
-  //return status;
 }
 
