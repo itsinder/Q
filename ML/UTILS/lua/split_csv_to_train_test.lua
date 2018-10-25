2d3035bb5e3e3fd1d1876e9419f8b398e25fee3c
PragatiPailwan <pragati.pailwan@gmail.com>
2018-10-25 22:15:30 +0530

    adding a temporary function called write_headers to train and test data files after splitting csv datafile.

--- ML/UTILS/lua/split_csv_to_train_test.lua
+++ ML/UTILS/lua/split_csv_to_train_test.lua
@@ -5,7 +5,33 @@ local Q_SRC_ROOT = os.getenv("Q_SRC_ROOT")
 local path_to_here = Q_SRC_ROOT .. "/ML/DT/lua/"
 assert(plpath.isdir(path_to_here))
 
-local function split_csv(data_file, meta_data_file, args)
+-- temporary function for writing headers
+local function write_hdrs_to_train_test(train_file, test_file, hdr_info)
+  local lines_train = {}
+  local lines_test = {}
+  for line in io.lines(train_file) do
+    lines_train[#lines_train + 1] = line
+  end
+  for line in io.lines(test_file) do
+    lines_test[#lines_test + 1] = line
+  end
+
+  local fp_train = io.open(train_file, "w")
+  local fp_test  = io.open(test_file, "w")
+  fp_train:write(hdr_info .. "\n")
+  fp_test:write(hdr_info .. "\n")
+  for i=1, #lines_train do
+    fp_train:write(lines_train[i] .. "\n")
+  end
+  for i=1, #lines_test do
+    fp_test:write(lines_test[i] .. "\n")
+  end
+  fp_train:close()
+  fp_test:close()
+end
+
+-- hdr_info: a temporary arg to this split_csv --TODO
+local function split_csv(data_file, meta_data_file, args, hdr_info)
   
   assert(data_file, "csv file not provided")
   assert(meta_data_file, "metadata file not provided")
@@ -40,8 +66,13 @@ local function split_csv(data_file, meta_data_file, args)
   end
   local dest_path, file_name = plpath.splitpath(data_file)
   local file_n = plpath.splitext(file_name)
-  Q.print_csv(Train, {opfile = dest_path .. "/" .. file_n .. "_train.csv", print_order = print_order })
-  Q.print_csv(Test, {opfile = dest_path .. "/" .. file_n .. "_test.csv", print_order = print_order })
+  local train_filename = dest_path .. "/" .. file_n .. "_train.csv"
+  local test_filename  = dest_path .. "/" .. file_n .. "_test.csv"
+  Q.print_csv(Train, {opfile = train_filename, print_order = print_order })
+  Q.print_csv(Test,  {opfile = test_filename, print_order = print_order })
+  -- TODO: temporary function to write headers in train and test datafiles
+  write_hdrs_to_train_test(train_filename, test_filename, hdr_info)
+
 end
 
 return split_csv
