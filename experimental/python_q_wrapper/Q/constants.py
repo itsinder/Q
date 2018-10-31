913ea9a69cc4ebb60a2cd02366cc0893662342ab
krushna <krushnakant.mardikar@gslab.com>
2018-10-31 04:38:15 -0700

    Added comments in the test files, also added constants which resembles with numpy

--- experimental/python_q_wrapper/Q/constants.py
+++ experimental/python_q_wrapper/Q/constants.py
@@ -1,6 +1,15 @@
+# Q data types
 I1 = "I1"
 I2 = "I2"
 I4 = "I4"
 I8 = "I8"
 F4 = "F4"
 F8 = "F8"
+
+# data types that resembles with numpy
+int8 = "I1"
+int16 = "I2"
+int32 = "I4"
+int64 = "I8"
+float32 = "F4"
+float64 = "F8"
--- experimental/python_q_wrapper/test_q.py
+++ experimental/python_q_wrapper/test_q.py
@@ -5,31 +5,53 @@ from Q.p_vector import PVector
 
 
 def test_mk_col():
+    """Test the Q.mk_col() functionality
+    Input is list, output is PVector"""
+
     in_vals = [1, 2, 3, 4]
     vec = Q.mk_col(in_vals, Q.I1)
     assert(isinstance(vec, PVector))
     assert(vec.length() == len(in_vals))
     assert(vec.qtype() == Q.I1)
     Q.print_csv(vec)
-    print("Successfully exexuted Q.mk_col test")
+    print("Successfully executed Q.mk_col test")
 
 
 def test_print_csv():
+    """Test Q.print_csv() with opfile=None (default)
+    This will print the vector values to stdout"""
+
     in_vals = [1, 2, 3, 4]
     vec = Q.mk_col(in_vals, Q.I1)
     Q.print_csv(vec)
-    print("Successfully exexuted Q.print_csv test")
+    print("Successfully executed Q.print_csv test")
+
+
+def test_print_csv_str():
+    """Test Q.print_csv() with opfile=""
+    This will return the string representation of vector"""
+
+    in_vals = [1, 2, 3, 4]
+    vec = Q.mk_col(in_vals, Q.I1)
+    result = Q.print_csv(vec, opfile="")
+    sys.stdout.write(result)
+    print("Successfully executed Q.print_csv test")
 
 
 def test_print_csv_list():
+    """Test Q.print_csv() with list of vectors as input"""
+
     in_vals = [1, 2, 3, 4]
     vec1 = Q.mk_col(in_vals, Q.I1)
     vec2 = Q.mk_col(in_vals, Q.I1)
     Q.print_csv([vec1, vec2])
-    print("Successfully exexuted Q.print_csv with list as input")
+    print("Successfully executed Q.print_csv with list as input")
 
 
 def test_print_csv_to_file():
+    """Test Q.print_csv() with opfile=file
+    This will write the print_csv output to file"""
+
     in_vals = [1, 2, 3, 4]
     file_name = "result.txt"
     vec1 = Q.mk_col(in_vals, Q.I1)
@@ -39,10 +61,13 @@ def test_print_csv_to_file():
     os.system("cat %s" % file_name)
     os.remove(file_name)
     assert(not os.path.exists(file_name))
-    print("Successfully exexuted Q.print_csv with opfile as file")
+    print("Successfully executed Q.print_csv with opfile as file")
 
 
 def test_const():
+    """Test the Q.const() functionality
+    It creates a constant vector with specified value and length"""
+
     length = 6
     val = 5
     qtype = Q.I1
@@ -52,10 +77,13 @@ def test_const():
     vec.eval()
     assert(vec.num_elements() == length)
     Q.print_csv(vec)
-    print("Successfully exexuted Q.const test")
+    print("Successfully executed Q.const test")
 
 
 def test_vec_add():
+    """Test Q.vvadd() functionality
+    It performs sum of two vectors"""
+
     in_vals = [1, 2, 3, 4, 5]
     vec1 = Q.mk_col(in_vals, Q.I1)
     vec2 = Q.const(val=5, length=len(in_vals), qtype=Q.I1)
@@ -64,48 +92,57 @@ def test_vec_add():
     out.eval()
     assert(out.num_elements() == len(in_vals))
     Q.print_csv(out)
-    print("Successfully exexuted vec_add test")
+    print("Successfully executed vec_add test")
 
 
 def test_vec_concat():
+    """Test the vector methods concatenation"""
+
     vec1 = Q.const(val=5, length=5, qtype=Q.I1).set_name("new_vec").eval()
     assert(isinstance(vec1, PVector))
     Q.print_csv(vec1)
-    print(vec1.get_name())
-    print("Successfully exexuted vector method concat test")
+    assert(vec1.get_name() == "new_vec")
+    print("Successfully executed vector method concat test")
 
 
 def test_op_concat():
+    """Test the Q operator concatenation"""
+
     vec1 = Q.const(val=5, length=5, qtype=Q.I1)
     vec2 = Q.const(val=25, length=5, qtype=Q.I1)
     result = Q.vveq(Q.vvsub(Q.vvadd(vec1, vec2), vec2), vec1).eval()
     assert(isinstance(result, PVector))
     Q.print_csv(result)
-    print("Successfully exexuted Q operator concat test")
+    print("Successfully executed Q operator concat test")
 
 
 def test_op_concat_memo():
+    """Test the Q operator concatenation with setting memo to false"""
     vec1 = Q.const(val=5, length=5, qtype=Q.I1).memo(False)
     vec2 = Q.const(val=25, length=5, qtype=Q.I1).memo(False)
     result = Q.vveq(Q.vvsub(Q.vvadd(vec1, vec2).memo(False), vec2).memo(False), vec1).memo(False).eval()
     assert(isinstance(result, PVector))
     Q.print_csv(result)
-    print("Successfully exexuted Q operator concat test with setting memo false")
+    print("Successfully executed Q operator concat test with setting memo false")
 
 
-def test_op_concat_persist():
+def test_vec_persist():
+    """Test the vec persist method"""
+
     vec1 = Q.const(val=5, length=5, qtype=Q.I1)
     vec2 = Q.const(val=25, length=5, qtype=Q.I1)
     result = Q.vveq(Q.vvsub(Q.vvadd(vec1, vec2), vec2), vec1).persist(True).eval()
     assert(isinstance(result, PVector))
     Q.print_csv(result)
-    print("Successfully exexuted Q operator concat test with persist flag to true")
+    print("Successfully executed Q operator concat test with persist flag to true")
 
 
 if __name__ == "__main__":
-    test_mk_col()
+    # test_mk_col()
     print("==========================")
     test_print_csv()
+    print("==========================")
+    test_print_csv_str()
     print("==========================")
     test_print_csv_list()
     print("==========================")
@@ -121,4 +158,4 @@ if __name__ == "__main__":
     print("==========================")
     test_op_concat_memo()
     print("==========================")
-    test_op_concat_persist()
+    test_vec_persist()
