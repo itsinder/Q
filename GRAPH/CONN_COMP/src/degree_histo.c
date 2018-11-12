a8f5092821e0c96887d595b20ff1c67d57b48ba3
rsubramonian <rsubramonian@nerdwallet.com>
2018-11-12 12:34:59 -0800

    ok

--- GRAPH/CONN_COMP/src/degree_histo.c
+++ GRAPH/CONN_COMP/src/degree_histo.c
@@ -3,6 +3,7 @@
 #include "q_incs.h"
 #include "_mmap.h"
 #include "_rdtsc.h"
+#include "qsort_asc_I4.h"
 
 int 
 main(
@@ -31,7 +32,6 @@ main(
   int nV = max_id + 1;
   degree = malloc(nV * sizeof(int));
   return_if_malloc_failed(degree);
-#pragma omp parallel for
   for ( int i = 0; i < nV; i++ ) { 
     degree[i] = 0;
   }
@@ -40,7 +40,6 @@ main(
   uint64_t n = nX / sizeof(int);
   int *E = (int *)X;
   uint64_t t_start = RDTSC();
-// #pragma omp parallel for schedule(static, 1024)
   for ( uint64_t i = 0; i < n; i++ ) {
     int node_id = E[i];
 #ifdef DEBUG
@@ -63,13 +62,27 @@ main(
   ofp2 = fopen(opfile2, "wb");
   return_if_fopen_failed(ofp2, opfile2, "wb");
 
+  int real_nV = 0;
   for ( int i = 0; i < nV; i++ ) { 
     if ( degree[i] > 0 ) {
       fwrite(&i,       sizeof(int), 1,  ofp1);
       fwrite(degree+i, sizeof(int), 1,  ofp2);
+      real_nV++;
     }
   }
-  fprintf(stderr, "Wrote answer \n");
+  fclose_if_non_null(ofp1);
+  fclose_if_non_null(ofp2);
+  mcr_rs_munmap(X, nX);
+  fprintf(stderr, "Wrote %d degrees \n", real_nV);
+
+  status = rs_mmap(opfile2, &X, &nX, 1);
+  int *x_degree = (int *)X;
+  t_start = RDTSC();
+  qsort_asc_I4(x_degree, real_nV, sizeof(int), NULL);
+  t_stop = RDTSC();
+  mcr_rs_munmap(X, nX);
+  fprintf(stderr, "Sorted in %lf  \n", 
+      (t_stop - t_start)/(2800.0*1000000.0));
 BYE:
   mcr_rs_munmap(X, nX);
   free_if_non_null(degree);
