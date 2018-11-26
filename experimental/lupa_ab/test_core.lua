7f49df8616287955cd0f25cddfda429415528696
krushna <krushnakant.mardikar@gslab.com>
2018-11-26 05:28:26 -0800

    Made changes in lua layer and python code to reflect the C changes

--- experimental/lupa_ab/test_core.lua
+++ experimental/lupa_ab/test_core.lua
@@ -12,16 +12,4 @@ for i, v in pairs(out_tbl) do
 end
 ab.print_ab(ab_struct)
 ab.free_ab(ab_struct)
-
-print("==========================================")
-
-ab_struct = ab.init_ab_copy("My_Config")
-local ab_tbl = {}
-ab_tbl['factor'] = 2
-out_json = ab.sum_ab(ab_struct, JSON:encode(ab_tbl))
-out_tbl = JSON:decode(out_json)
-for i, v in pairs(out_tbl) do
-  print(i, v)
-end
-ab.print_ab(ab_struct)
-ab.free_ab(ab_struct)
+print("=============================================")
