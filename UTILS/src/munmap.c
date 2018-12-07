d5c420ffc773782fc08aebf1637dc25d6d78d05a
PragatiPailwan <pragati.pailwan@gmail.com>
2018-12-07 12:05:58 +0530

    Updated the load_csv to use qc.rs_mmap and qc.rs_munmap, also added test for mmaping

--- UTILS/src/munmap.c
+++ UTILS/src/munmap.c
@@ -1,5 +1,4 @@
 #include "q_incs.h"
-#include "q_macros.h"
 //STOP_INCLUDES
 #include "_munmap.h"
 //START_FUNC_DECL
@@ -10,13 +9,15 @@ rs_munmap(
 	)
 //STOP_FUNC_DECL
 { 
-   int status = 0;
-   if ( ( X != NULL ) && ( nX != 0 ) ) {
-     munmap(X, nX);
-   }
-   else {
-     go_BYE(-1);
-   }
- BYE:
-   return status;
+  int status = 0;
+
+  if ( ( X == NULL ) && ( nX != 0 ) ) { go_BYE(-1); }
+  if ( ( X != NULL ) && ( nX == 0 ) )  { go_BYE(-1); }
+  if ( X != NULL ) {
+    status = munmap(X, nX); if ( status != 0 ) { go_BYE(-1); }
+  }
+  X = NULL; nX = 0;
+
+BYE:
+  return status;
 }
