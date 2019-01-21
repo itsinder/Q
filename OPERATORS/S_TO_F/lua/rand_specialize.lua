16178d976667fa88dc215c6c5e72f899bb0e41b0
rsubramonian <rsubramonian@nerdwallet.com>
2019-01-21 14:20:46 -0800

    ok

--- OPERATORS/S_TO_F/lua/rand_specialize.lua
+++ OPERATORS/S_TO_F/lua/rand_specialize.lua
@@ -69,18 +69,6 @@ return function (
   c_mem_ptr.ub = ffi.cast(ctype .. " *", get_ptr(ub:to_cmem()))[0]
   c_mem_ptr.seed = seed
   --==============================
-  if ( qconsts.iorf[qtype] == "fixed" ) then 
-    subs.generator = "mrand48_r"
-    subs.gen_type = "int64_t"
-    subs.scaling_code = "floor( (( (double) (x - INT_MIN) ) / ( (double) (INT_MAX) - (double)(INT_MIN) ) ) * (range + 1) )"
-  elseif ( qconsts.iorf[qtype] == "floating_point" ) then 
-    subs.generator = "drand48_r"
-    subs.gen_type = "double"
-    subs.scaling_code = "range * x"
-  else
-    assert(nil, "Unknown type " .. qtype)
-  end
-  --=========================
   tmpl = qconsts.q_src_root .. "/OPERATORS/S_TO_F/lua/rand.tmpl"
   subs.fn = "rand_" .. qtype
   subs.c_mem = c_mem
