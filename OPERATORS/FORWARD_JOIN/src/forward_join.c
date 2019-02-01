982c887c99e86b3e3c9b0579439ebd7c9d44adeb
rsubramonian <rsubramonian@nerdwallet.com>
2019-01-31 20:04:42 -0800

    ok

--- OPERATORS/FORWARD_JOIN/src/forward_join.c
+++ OPERATORS/FORWARD_JOIN/src/forward_join.c
@@ -1,28 +1,65 @@
 
+#include "q_incs.h"
+#include "fj_state_rec_type.h"
+
 int
 forward_join(
-    int32_t *from_t, //  link field 
-    int n1,
-    int32_t *from_v. // value field 
-    int n1,
-    int32_t *to_t, // link field
-    int n2,
-    int32_t *to_v, // value field 
-    int n2,
+    int64_t *from_uuid, // [n_from] link field 
+    int32_t *from_time, // [n_from] link field 
+    int32_t *from_valu, // [n_from] value field 
+    int n_from,
+
+    int64_t *to_uuid, // [n_to] link field
+    int32_t *to_time, // [n_to] link field 
+    int32_t *to_valu, // [n_to] value field 
+    int n_to,
+
     uint32_t delta
     )
 {
   int status = 0;
-  uint64_t fidx = 0; // from index
-  uint64_t tidx = 0; // to index
+
+  uint64_t tidx = 0; //
+  uint64_t fidx = 0; // 
+
+  for ( uint64_t i = 0; i < n_to; i++ ) {
+    to_valu[i] = -1;
+  }
   for ( ; ; ) {
-    int32_t s_from_t = *from_t;
-    int32_t s_from_v = *from_v;
-    int32_t s_to_t   = *to_t;
-    if ( s_from_t + delta >= s_to_t ) {
-      *to_v = s_from_v;
+    int64_t s_from_uuid = from_uuid[fidx];
+    int64_t s_to_uuid   = to_uuid[tidx];
+    if ( s_from_uuid < s_to_uuid )  {
+      fidx++; continue;
+    }
+    if ( s_from_uuid > s_to_uuid )  {
+      tidx++; continue;
+    }
+    // Now, s_from_uuid == s_to_uuid 
+    int32_t s_from_time = from_time[fidx];
+    int32_t s_to_time   = to_time[tidx];
+    if ( s_from_time + delta < s_to_time ) {
+      fidx++; continue;
+    }
+    int32_t s_from_valu = from_valu[fidx];
+    if ( s_from_time > s_to_time ) {
+      fidx++; continue;
+    }
+    to_valu[tidx] = s_from_valu;
+    tidx++;
+    if ( tidx == n_to ) {
+      break;
+    }
+    fidx++;
+    if ( fidx == n_from ) {
+      break;
     }
   }
 BYE:
+/* Who updates
+  ptr_state->from_index = g_fidx;
+  ptr_state->to_index   = g_tidx;
+  uint32_t from_chunk_num;
+  uint32_t to_chunk_num;
+*/
   return status;
 }
