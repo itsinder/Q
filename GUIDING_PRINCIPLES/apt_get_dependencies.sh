90fbc1281c1d4d5d52820ee462fa5cec613c7d82
PragatiPailwan <pragati.pailwan@gmail.com>
2019-02-05 20:11:26 +0530

    changes in apt_get dependencies shell file

--- GUIDING_PRINCIPLES/apt_get_dependencies.sh
+++ GUIDING_PRINCIPLES/apt_get_dependencies.sh
@@ -20,21 +20,10 @@ else
   bash my_print.sh "dependency unzip is already installed"
 fi
 
-apt-cache policy libncurses5-dev &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install libncurses5-dev -y # for lua-5.1.5
-else
-  bash my_print.sh "dependency libncurses5-dev is already installed"
-fi
+sudo apt-get install libncurses5-dev -y # for lua-5.1.5
 
-apt-cache policy libssl-dev &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install libssl-dev -y # for QLI
-else
-  bash my_print.sh "dependency libssl-dev is already installed"
-fi
+sudo apt-get install libssl-dev -y # for QLI
+
 
 which m4 &> /dev/null || true
 RES=$?
@@ -44,21 +33,11 @@ else
   bash my_print.sh "dependency m4 is already installed"
 fi
 
-apt-cache policy libreadline-dev &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install libreadline-dev -y
-else
-  bash my_print.sh "dependency libreadline-dev is already installed"
-fi
+
+sudo apt-get install libreadline-dev -y
 
 #installing LAPACK stuff
-apt-cache policy liblapacke-dev &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install liblapacke-dev -y
-else
-  bash my_print.sh "dependency liblapacke-dev is already installed"
-fi
+sudo apt-get install liblapacke-dev -y
+
 
 bash my_print.sh "COMPLETED: Installing dependencies from apt-get"
