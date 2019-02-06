115edf619e1e30cf8e5934f4e77685743bd6b1b8
PragatiPailwan <pragati.pailwan@gmail.com>
2019-02-06 19:59:40 +0530

    minor changes in apt_get_dependencies script

--- GUIDING_PRINCIPLES/apt_get_dependencies.sh
+++ GUIDING_PRINCIPLES/apt_get_dependencies.sh
@@ -4,33 +4,15 @@ bash my_print.sh "STARTING: Installing dependencies from apt-get"
 
 sudo apt-get update -y || true
 
-which cmake &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install make cmake -y
-else
-  bash my_print.sh "dependency cmake is already installed"
-fi
+sudo apt-get install make cmake -y
 
-which unzip &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install unzip -y # for luarocks
-else
-  bash my_print.sh "dependency unzip is already installed"
-fi
+sudo apt-get install unzip -y # for luarocks
 
 sudo apt-get install libncurses5-dev -y # for lua-5.1.5
 
 sudo apt-get install libssl-dev -y # for QLI
 
-which m4 &> /dev/null || true
-RES=$?
-if [[ $RES -ne 0 ]] ; then
-  sudo apt-get install m4 -y         # for QLI
-else
-  bash my_print.sh "dependency m4 is already installed"
-fi
+sudo apt-get install m4 -y         # for QLI
 
 sudo apt-get install libreadline-dev -y
 
