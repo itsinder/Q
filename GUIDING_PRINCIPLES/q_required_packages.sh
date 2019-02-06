1681c01e317249fa29b111ee3aa74ce48676fc31
PragatiPailwan <pragati.pailwan@gmail.com>
2019-02-06 14:16:08 +0530

    adding penlight to Q required packages for now as our Q build process is using penlight functions

--- GUIDING_PRINCIPLES/q_required_packages.sh
+++ GUIDING_PRINCIPLES/q_required_packages.sh
@@ -1,3 +1,5 @@
 bash my_print.sh "STARTING: Installing Q-required packages"
 sudo luarocks install luaposix
+#for now included pkg "penlight" as our Q build is using it
+sudo luarocks install penlight
 bash my_print.sh "COMPLETED: Installing Q-required packages"
