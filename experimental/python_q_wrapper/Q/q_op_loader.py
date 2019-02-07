b3d6b3746550e9e541fae63201167ed453c53b3d
krushna <krushnakant.mardikar@gslab.com>
2019-02-07 00:35:00 -0800

    Deleted python_q_wrapper from this branch, will add this code after review

--- experimental/python_q_wrapper/Q/q_op_loader.py
+++
@@ -1,9 +0,0 @@
-import Q.lua_executor as executor
-from Q.q_helper import call_lua_op
-from Q import constants as q_consts
-
-
-q_operators = list(executor.execute_lua(q_consts.q_op_str).values())
-
-def op_wrapper(op_name):
-    return lambda *args: call_lua_op(op_name, *args)
