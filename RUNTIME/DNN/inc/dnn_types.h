0e5161f6fce432760ec1b2e8caf8a7b623ff6b23
rsubramonian <rsubramonian@nerdwallet.com>
2019-02-09 07:45:19 -0800

    ok

--- RUNTIME/DNN/inc/dnn_types.h
+++ RUNTIME/DNN/inc/dnn_types.h
@@ -8,16 +8,19 @@ typedef enum act_fn_type {
 typedef struct _dnn_rec_type {
   int bsz; // batch size
   int nl; // num layers
-  int *npl;  // neurons per layer 
+  int *npl;  // neurons per layer  [num_layers]
   ACT_FN_TYPE  *A; // activation_function[num_layers] 
-  float **X; // TODO NOT SURE  [neurons_in_layer[0]][num_instances]
-  int num_instances; // TODO NOT SURE 
-  float ***W; // [num_layers] 
-  float **b; // [num_layers] [neurons_per_layer[l]]
+  /* A[0] = undef_act_fn */
+  float **in; // Input data, [neurons_in_layer[0]][num_instances]
+  int num_instances; // 
+  float **out; // Output data, [neurons_in_layer[nl-1]][num_instances]
+  float ***W; // weights, [num_layers][][] 
+  float **b; // bias, [num_layers] [neurons_per_layer[l]]
   /* W[0] = NULL
    * W[i] = [neurons_in_layer[i-1]][neurons_in_layer[i]]
+   * b[0] = NULL
+   * W[i] = [neurons_in_layer[i]]
    * */
-  float **B; // [num_layers] B[0] = 0 --> unused
 //------------------------------------------------------------
 } DNN_REC_TYPE;
 #endif // _DNN_TYPES_H
