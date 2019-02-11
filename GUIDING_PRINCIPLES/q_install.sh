5a19a23f8f78a20f6e65c3474809bc560ce13a57
PragatiPailwan <pragati.pailwan@gmail.com>
2019-02-11 15:42:19 +0530

    Pull Request 320: Changes done according to Ramesh's comment. Using test cmd when expecting directory to exists. Added Indrajeet's comment to the cmds in aio.sh script

--- GUIDING_PRINCIPLES/q_install.sh
+++ GUIDING_PRINCIPLES/q_install.sh
@@ -55,7 +55,9 @@ else
   bash luajit_installation.sh
 fi
 
-# TODO: what's this for?
+# This modifies (increases dramatically) the number of files the particular user can have
+# open (concurrently). The user for whom we are increasing the limits is the current user.
+# The understanding is that Q will be run by this user.
 echo "`whoami` hard nofile 102400" | sudo tee --append /etc/security/limits.conf
 echo "`whoami` soft nofile 102400" | sudo tee --append /etc/security/limits.conf
 
