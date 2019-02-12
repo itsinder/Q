7d58b1559a0208228ff97b79d189560618ee46d3
PragatiPailwan <pragati.pailwan@gmail.com>
2019-02-12 18:02:37 +0530

    adding luacov pkg installation in test mode of q_install.sh. Mentioned the reason for the same. Need to check with Ramesh.

--- GUIDING_PRINCIPLES/q_test_dependencies.sh
+++ GUIDING_PRINCIPLES/q_test_dependencies.sh
@@ -1,4 +1,5 @@
 bash my_print.sh "STARTING: Installing Q-test dependencies using luarocks"
 sudo luarocks install luaposix #posix.time lib has been used for capturing timers
 sudo luarocks install penlight #pl lib has been used for Q unit test
+sudo luarocks install luacov #luacov is used in Q/TEST_RUNNER/q_testrunner.lua :line 70
 bash my_print.sh "COMPLETED: Installing Q-test dependencies using luarocks"
