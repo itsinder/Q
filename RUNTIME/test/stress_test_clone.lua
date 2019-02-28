7e893f3c53f67b371c61e1d0020bb9d45ea3669b
krushna <krushnakant.mardikar@gslab.com>
2019-02-28 02:52:15 -0800

    Updated the stress_test_clone.lu

--- RUNTIME/test/stress_test_clone.lua
+++ RUNTIME/test/stress_test_clone.lua
@@ -1,8 +1,15 @@
+local Q		= require 'Q'
 local lVector	= require 'Q/RUNTIME/lua/lVector'
+local qconsts   = require 'Q/UTILS/lua/q_consts'
+local cmem      = require 'libcmem'
+local ffi       = require 'Q/UTILS/lua/q_ffi'
+local get_ptr	= require 'Q/UTILS/lua/get_ptr'
+
 local tests = {}
-local niters = 1000000
+local niters = 100
 --====== Testing with more than chunk size
 tests.t1 = function()
+  local num = 10
   local len = qconsts.chunk_size * 2 + 1
   local x = Q.const( {val = num, qtype = "I4", len = len })
   for i = 1, niters do y = Q.clone(x) end
@@ -11,16 +18,16 @@ tests.t1 = function()
   print("Successfully completed test t1")
 end
 --====== Testing with less than chunk size 
-tests.t1 = function()
+tests.t2 = function()
   local x = lVector( { qtype = "I4", gen = true, has_nulls = false})
-  local len = qconsts.chunk_size - 1 
+  local len = qconsts.chunk_size - 1
   local field_size = 4
-  local base_data = cmem.new(num_elements * field_size)
-  local iptr = ffi.cast("int32_t *", base_data)
-  for i = 1, num_elements do
+  local base_data = cmem.new(len * field_size)
+  local iptr = get_ptr(base_data, "I4")
+  for i = 1, len do
     iptr[i-1] = i*10
   end
-  x:put_chunk(base_data, nil, num_elements)
+  x:put_chunk(base_data, nil, len)
   for i = 1, niters do y = Q.clone(x) end
   x:eov()
   for i = 1, niters do y = Q.clone(x) end
