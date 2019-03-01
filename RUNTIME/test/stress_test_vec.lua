8c8a528d40fc801fbaa0a6b8cb4cb1934684a04c
PragatiPailwan <pragati.pailwan@gslab.com>
2019-03-01 15:59:12 +0530

    modified the count, num_iters,  num_elements variable value in stress test to get the stress-tests results in 24 hrs

--- RUNTIME/test/stress_test_vec.lua
+++ RUNTIME/test/stress_test_vec.lua
@@ -3,13 +3,17 @@ local Vector = require 'libvec' ;
 local Scalar = require 'libsclr' ; 
 local cmem = require 'libcmem' ; 
 local qconsts = require 'Q/UTILS/lua/q_consts'
+local qc = require 'Q/UTILS/lua/q_core'
+local utils = require 'Q/UTILS/lua/utils'
+
 require 'Q/UTILS/lua/strict'
 
 -- for k, v in pairs(vec) do print(k, v) end 
 local tests = {} 
 
 tests.t1 = function()
-  local num_trials = 1000000
+  local num_trials = 100000
+  local start_time = qc.RDTSC()
   for i = 1, num_trials do 
     -- create a nascent vector many times
     local y = Vector.new('I4', qconsts.Q_DATA_DIR)
@@ -24,7 +28,9 @@ tests.t1 = function()
     assert(y:check())
     if ( ( i % 10 ) == 0 ) then print("Iter ", i)  end
   end
-  print("Successfully completed test t1")
+  local stop_time = qc.RDTSC()
+  print("stress_test_vec time(seconds): ", utils["RDTSC"](stop_time-start_time))
+  print("Successfully completed stress test: RUNTIME/test/stress_test_vec.lua")
 end
 
 return tests
