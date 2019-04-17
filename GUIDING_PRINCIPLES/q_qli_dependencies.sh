7b049c1434602a040e97f762b38e04c753560cea
Ramesh Subramonian <subramonian@gmail.com>
2019-04-17 20:56:42 +0000

    ok

--- GUIDING_PRINCIPLES/q_qli_dependencies.sh
+++ GUIDING_PRINCIPLES/q_qli_dependencies.sh
@@ -7,5 +7,5 @@ bash my_print.sh "STARTING: Installing Q-qli dependencies using luarocks"
 #sudo luarocks install cluacov
 sudo luarocks install http      # for QLI
 sudo luarocks install linenoise # for QLI
-sudo apt-get install libcurl4-openssl-dev # for CSERVER
+sudo apt-get install libcurl4-openssl-dev -y # for CSERVER
 bash my_print.sh "COMPLETED: Installing Q-qli dependencies using luarocks"
